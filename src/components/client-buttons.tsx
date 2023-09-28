531bedd811523f1b69b69ee2e39d65804d99bceb
Srdjan <milicevic.srdjan0@gmail.com>
2023-09-28 23:40:05 +0200

    Initialize project, set themeContext for dark mode

---
+++ src/components/client-buttons.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { DarkModeIcon, LightModeIcon } from "./svgs";
+
+//Buttons that require any kind of client side action (hooks, libs based on custom hooks - state, effect, context)
+
+export const ModeButton = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  function toggleMode() {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  }
+
+  if (!mounted) return <></>;
+
+  return (
+    <button onClick={toggleMode}>
+      {resolvedTheme === "dark" ? (
+        <LightModeIcon className='h-7 w-7 text-slate-50' />
+      ) : (
+        <DarkModeIcon className='h-7 w-7 text-slate-600' />
+      )}
+    </button>
+  );
+};
