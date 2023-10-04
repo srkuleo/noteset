1dc542b06a83fb957cc60b47201ee931e69ddc10
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:46:26 +0200

    ADDS: login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,74 @@
+import { SubmitFormBtn } from "@/components/form-button";
+import { OAuth } from "@/components/oauth";
+import { Separator } from "@/components/separator";
 import Link from "next/link";
 
-export default function LoginPage() {
+export default async function LoginPage() {
   return (
-    <div>
-      <p>Hey, login now!</p>
-      <Link href='/' className='bg-slate-600 dark:bg-slate-500'>
-        Return to Home page
-      </Link>
+    <div className="px-12 pt-32">
+      <p className="mb-8 text-lg font-bold italic text-slate-500 dark:text-slate-300/90">
+        Login, set new records!
+      </p>
+
+      <OAuth />
+
+      <Separator />
+
+      <form action="" className="flex flex-col gap-2">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          autoFocus
+          required
+          className="input-field"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          required
+          className="input-field"
+        />
+        <div className="flex items-center gap-2 py-4">
+          <input
+            type="checkbox"
+            id="remember-me"
+            name="remember-me"
+            className="h-4 w-4 accent-green-500 dark:accent-green-600"
+          />
+          <label
+            htmlFor="remember-me"
+            className="text-xs text-slate-600 dark:text-slate-300"
+          >
+            Remember me
+          </label>
+        </div>
+        <SubmitFormBtn>Login</SubmitFormBtn>
+      </form>
+
+      <div className="space-y-2 pb-4 text-xs text-slate-500 dark:text-slate-300/90">
+        <p>
+          Forgot your{" "}
+          <Link
+            href="/"
+            className="border-b border-green-500 text-sm font-semibold text-green-500"
+          >
+            password?
+          </Link>
+        </p>
+        <p>
+          New to Noteset? Sign up{" "}
+          <Link
+            href="/sign-up"
+            className="border-b border-green-500 text-sm font-semibold text-green-500"
+          >
+            here.
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
