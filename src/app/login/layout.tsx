f242f74fad0e48e22809eae090046c0e01324873
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:44:40 +0200

    ADDS: shared navbar in layout

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/navbar";
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main>
+      <Navbar />
+      {children}
+    </main>
+  );
+}
