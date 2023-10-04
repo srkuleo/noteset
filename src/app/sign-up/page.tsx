de1e8dbc40fe4068530c6fbde080ea6df39f5e61
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:47:52 +0200

    ADDS: sign up form - WIP

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,12 +1,66 @@
+import { SubmitFormBtn } from "@/components/form-button";
+import { OAuth } from "@/components/oauth";
+import { Separator } from "@/components/separator";
 import Link from "next/link";
 
 export default function SignUpPage() {
   return (
-    <div>
-      <p>Hey, sign in now!</p>
-      <Link href='/' className='bg-slate-600 dark:bg-slate-500'>
-        Return to Home page
-      </Link>
+    <div className="px-12 pt-32">
+      <p className="mb-8 text-lg font-bold italic text-slate-500 dark:text-slate-300/90">
+        Sign up, start your journey today!
+      </p>
+
+      <OAuth />
+
+      <Separator />
+
+      <form action="" className="flex flex-col gap-2">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+          required
+          className="input-field"
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          autoFocus
+          required
+          className="input-field"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          required
+          className="input-field"
+        />
+        <input
+          type="password"
+          name="cpassword"
+          id="cpassowrd"
+          placeholder="Confirm password"
+          required
+          className="input-field"
+        />
+        <div className="mb-4"></div>
+        <SubmitFormBtn>Sign up</SubmitFormBtn>
+      </form>
+
+      <p className="pb-4 text-xs text-slate-500 dark:text-slate-300/90">
+        Already have an account? Login{" "}
+        <Link
+          href="/login"
+          className="border-b border-green-500 text-sm font-semibold text-green-500"
+        >
+          here.
+        </Link>
+      </p>
     </div>
   );
 }
