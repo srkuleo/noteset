b502876f90f74a37c1aa724d9a2b36ac5b1a6f82
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:49:24 +0200

    REFACTORED: removes logo, add link to home

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { LogoSvg } from "./svgs";
 import { ModeButton } from "./client-buttons";
 import { GitHubButton } from "./server-buttons";
+import Link from "next/link";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -27,15 +27,15 @@ export const Navbar = () => {
   }
 
   return (
-    <div className='sticky top-0'>
+    <div className="sticky top-0">
       <nav
         className={`${
           isScrolled &&
           "bg-opacity-80 backdrop-blur-sm transition duration-300 ease-out dark:bg-opacity-80"
-        } bg-green-600 dark:bg-green-800 flex justify-between px-4 py-2 shadow-sm`}
+        } flex justify-between bg-green-600 px-4 py-2 shadow-md dark:bg-green-800`}
       >
-        <Logo />
-        <div className="flex gap-2 items-center">
+        <Label />
+        <div className="flex items-center gap-2">
           <ModeButton />
           <GitHubButton />
         </div>
@@ -44,14 +44,15 @@ export const Navbar = () => {
   );
 };
 
-export const Logo = () => {
+const Label = () => {
   return (
-    <div className='flex items-center gap-1'>
-      <div className=''>{LogoSvg}</div>
-      <span className='text-2xl font-bold text-slate-light50 dark:text-green-light400'>
+    <Link href="/">
+      <p className="text-2xl font-bold text-slate-50 dark:text-green-400">
         Note
-        <span className='font-semibold dark:text-slate-light400'>Set</span>
-      </span>
-    </div>
+        <span className="font-semibold text-slate-200 dark:text-slate-400">
+          Set
+        </span>
+      </p>
+    </Link>
   );
 };
