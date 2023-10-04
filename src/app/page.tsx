5c133952966703717517247f651850f77badc1bf
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:44:20 +0200

    REFACTORS: extract into few small components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,32 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='min-h-screen p-4'>
-      <div className='flex gap-4 justify-end mb-56 border-b pb-4 dark:border-slate-700'>
+    <main className="p-4">
+      <div className="mb-56 flex justify-end gap-4 border-b border-slate-400/60 pb-4 dark:border-slate-700">
         <ModeButton />
         <GitHubButton />
       </div>
-      <div className='space-y-8 rounded-lg shadow-sm'>
-        <h1 className='text-5xl pl-8 text-green-600 font-bold'>
-          Note<span className='text-slate-500 font-semibold'>Set</span>
-        </h1>
-        <h2 className='pl-4 font-semibold text-slate-400 dark:text-slate-500/90'>
-          Application aimed to replace conventional use of a notebook in a gym.
-        </h2>
-        <div className='flex justify-evenly'>
-          <Link href='/login'>Login</Link>
-          <Link href='/sign-up'>Sign Up</Link>
-        </div>
+
+      <h1 className="mb-8 pl-8 text-5xl font-bold text-green-500 dark:text-green-600">
+        Note<span className="font-semibold text-slate-500">Set</span>
+      </h1>
+      <h2 className="mb-12 pl-4 font-semibold text-slate-400 dark:text-slate-500/90">
+        Application aimed to replace conventional use of a notebook in a gym.
+      </h2>
+
+      <div className="mb-8 flex justify-evenly">
+        <Link
+          href="/login"
+          className="rounded-3xl bg-gradient-to-r from-white to-slate-100 px-6  py-2 font-semibold text-slate-600 shadow-md ring-1 ring-inset ring-slate-400/60"
+        >
+          Login
+        </Link>
+        <Link
+          href="/sign-up"
+          className="rounded-3xl bg-gradient-to-r from-green-500 to-green-600 px-6 py-2 text-slate-50 shadow-md dark:from-green-600 dark:to-green-700"
+        >
+          Sign Up
+        </Link>
       </div>
     </main>
   );
