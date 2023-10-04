172646e26f95aae379def23aca70f5ee9b3a71b1
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:50:38 +0200

    ADDS: oAuth svgs

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -2,15 +2,15 @@ export const DarkModeIcon = ({ className }: { className: string }) => {
   return (
     <svg
       className={className}
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 20 20'
-      fill='currentColor'
-      aria-hidden='true'
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
     >
       <path
-        fillRule='evenodd'
-        d='M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z'
-        clipRule='evenodd'
+        fillRule="evenodd"
+        d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
+        clipRule="evenodd"
       />
     </svg>
   );
@@ -20,17 +20,17 @@ export const LightModeIcon = ({ className }: { className: string }) => {
   return (
     <svg
       className={className}
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
       strokeWidth={2.2}
-      aria-hidden='true'
+      aria-hidden="true"
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
       />
     </svg>
   );
@@ -38,48 +38,153 @@ export const LightModeIcon = ({ className }: { className: string }) => {
 
 export const LogoSvg = (
   <svg
-    className='h-10 w-10'
-    viewBox='-2.4 -2.4 28.80 28.80'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-    transform='rotate(45)'
-    aria-hidden='true'
+    className="h-10 w-10"
+    viewBox="-2.4 -2.4 28.80 28.80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    transform="rotate(45)"
+    aria-hidden="true"
   >
     <g
-      id='SVGRepo_bgCarrier'
-      strokeWidth='0'
-      transform='translate(0.7200000000000006,0.7200000000000006), scale(0.94)'
+      id="SVGRepo_bgCarrier"
+      strokeWidth="0"
+      transform="translate(0.7200000000000006,0.7200000000000006), scale(0.94)"
     >
       <path
-        transform='translate(-2.4, -2.4), scale(1.7999999999999998)'
-        d='M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z'
-        strokeWidth='0'
-        fill='#64748b'
+        transform="translate(-2.4, -2.4), scale(1.7999999999999998)"
+        d="M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z"
+        strokeWidth="0"
+        fill="#64748b"
       ></path>
     </g>
     <g
-      id='SVGRepo_tracerCarrier'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      stroke='#CCCCCC'
-      strokeWidth='0.048'
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke="#CCCCCC"
+      strokeWidth="0.048"
     ></g>
-    <g id='SVGRepo_iconCarrier'>
+    <g id="SVGRepo_iconCarrier">
       <rect
-        x='6'
-        y='4'
-        width='13'
-        height='17'
-        rx='2'
-        fill='#000000'
-        fillOpacity='0.24'
-        stroke='#4ade80'
-        strokeWidth='1.2'
+        x="6"
+        y="4"
+        width="13"
+        height="17"
+        rx="2"
+        fill="#000000"
+        fillOpacity="0.24"
+        stroke="#4ade80"
+        strokeWidth="1.2"
       ></rect>
-      <path d='M15 10V8' stroke='#4ade80' strokeWidth='1.2' strokeLinecap='round'></path>
-      <path d='M4 9H8' stroke='#4ade80' strokeWidth='1.2' strokeLinecap='round'></path>
-      <path d='M4 13H8' stroke='#4ade80' strokeWidth='1.2' strokeLinecap='round'></path>
-      <path d='M4 17H8' stroke='#4ade80' strokeWidth='1.2' strokeLinecap='round'></path>
+      <path
+        d="M15 10V8"
+        stroke="#4ade80"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      ></path>
+      <path
+        d="M4 9H8"
+        stroke="#4ade80"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      ></path>
+      <path
+        d="M4 13H8"
+        stroke="#4ade80"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      ></path>
+      <path
+        d="M4 17H8"
+        stroke="#4ade80"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      ></path>
+    </g>
+  </svg>
+);
+
+export const GoogleSvg = (
+  <svg
+    className="h-5 w-5"
+    viewBox="-0.5 0 48 48"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    fill="none"
+    aria-hidden="true"
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <title>Google-color</title> <desc>Created with Sketch.</desc>
+      <defs> </defs>
+      <g
+        id="Icons"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g id="Color-" transform="translate(-401.000000, -860.000000)">
+          <g id="Google" transform="translate(401.000000, 860.000000)">
+            <path
+              d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+              id="Fill-1"
+              fill="#FBBC05"
+            ></path>
+            <path
+              d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+              id="Fill-2"
+              fill="#EB4335"
+            ></path>
+            <path
+              d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+              id="Fill-3"
+              fill="#34A853"
+            ></path>
+            <path
+              d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+              id="Fill-4"
+              fill="#4285F4"
+            ></path>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export const AppleSvg = (
+  <svg
+    className="h-5 w-5"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    aria-hidden="true"
+  >
+    <g
+      fill="#94a3b8"
+      fillRule="nonzero"
+      stroke="none"
+      strokeWidth="1"
+      strokeLinecap="butt"
+      strokeLinejoin="miter"
+      strokeMiterlimit="10"
+      strokeDasharray=""
+      strokeDashoffset="0"
+      fontFamily="none"
+      fontWeight="none"
+      fontSize="none"
+      textAnchor="none"
+    >
+      <g transform="scale(5.12,5.12)">
+        <path d="M44.52734,34.75c-1.07812,2.39453 -1.59766,3.46484 -2.98437,5.57813c-1.94141,2.95313 -4.67969,6.64063 -8.0625,6.66406c-3.01172,0.02734 -3.78906,-1.96484 -7.87891,-1.92969c-4.08594,0.01953 -4.9375,1.96875 -7.95312,1.9375c-3.38672,-0.03125 -5.97656,-3.35156 -7.91797,-6.30078c-5.42969,-8.26953 -6.00391,-17.96484 -2.64844,-23.12109c2.375,-3.65625 6.12891,-5.80469 9.65625,-5.80469c3.59375,0 5.85156,1.97266 8.82031,1.97266c2.88281,0 4.63672,-1.97656 8.79297,-1.97656c3.14063,0 6.46094,1.71094 8.83594,4.66406c-7.76562,4.25781 -6.50391,15.34766 1.33984,18.31641zM31.19531,8.46875c1.51172,-1.94141 2.66016,-4.67969 2.24219,-7.46875c-2.46484,0.16797 -5.34766,1.74219 -7.03125,3.78125c-1.52734,1.85938 -2.79297,4.61719 -2.30078,7.28516c2.69141,0.08594 5.47656,-1.51953 7.08984,-3.59766z"></path>
+      </g>
     </g>
   </svg>
 );
