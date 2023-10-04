5111a07efdc2eec75edab8504431f2ad2a5845d0
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-04 05:50:01 +0200

    ABSTRACTION: reusable separator in form

---
+++ src/components/separator.tsx
@@ -0,0 +1,11 @@
+export const Separator = () => {
+  return (
+    <div className="mb-4 flex items-center gap-2">
+      <div className="w-32 border-b border-green-500/70"></div>
+      <p className="text-slate-main600 text-sm  italic dark:text-slate-300/90">
+        or
+      </p>
+      <div className="w-32 border-b border-green-500/70"></div>
+    </div>
+  );
+};
