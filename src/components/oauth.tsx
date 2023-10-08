b237e57b4ea48c0a4fd048389b3e78d71cd34f66
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-08 11:39:01 +0200

    CHANGES: refactors look, remove nameplate

--- src/components/oauth.tsx
+++ src/components/oauth.tsx
@@ -2,14 +2,12 @@ import { AppleSvg, GoogleSvg } from "./svgs";
 
 export const OAuth = () => {
   return (
-    <div className="mb-4 flex justify-between px-2">
-      <button className="flex items-center gap-2 rounded-xl bg-slate-50 px-4 py-2.5 text-xs shadow-sm ring-2 ring-inset ring-green-500 dark:bg-slate-700">
+    <div className="mb-4 flex justify-between">
+      <button className="flex items-center gap-2 rounded-xl bg-slate-50 px-12 py-2 text-xs shadow-sm ring-1 ring-slate-300 dark:bg-slate-200 dark:ring-0">
         {GoogleSvg}
-        Google
       </button>
-      <button className="flex items-center text-xs gap-2 rounded-xl bg-slate-50 px-4 py-2.5 shadow-sm ring-2 ring-inset ring-green-500 dark:bg-slate-700">
+      <button className="flex items-center gap-2 rounded-xl bg-slate-950 px-12 py-2 text-xs shadow-sm dark:bg-slate-500">
         {AppleSvg}
-        Apple ID
       </button>
     </div>
   );
