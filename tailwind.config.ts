830e49ca3663952f1e305350d9ae0f3312b1bb66
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-11 17:17:52 +0200

    ADDS: custom font size for navbar button's text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,16 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontSize: {
+        icon: ["0.625rem", "1.125rem"],
+      },
+    },
+  },
   plugins: [],
-  darkMode: "class"
+  darkMode: "class",
 };
 export default config;
