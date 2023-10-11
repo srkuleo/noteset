2d2891b7a1ff2711f1f3509aa8c8fa244c309b4a
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-11 17:24:09 +0200

    RENAMES: more suited name, doesn't navigate app

---
+++ src/components/headerbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ModeButton } from "./client-buttons";
+import { GitHubButton } from "./server-buttons";
+import Link from "next/link";
+
+export const HeaderBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("load", scrolled);
+    window.addEventListener("scroll", scrolled);
+
+    return () => {
+      window.removeEventListener("load", scrolled);
+      window.removeEventListener("scroll", scrolled);
+    };
+  }, []);
+
+  function scrolled() {
+    if (window.scrollY >= 1) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  }
+
+  return (
+    <div className="sticky top-0">
+      <nav
+        className={`${
+          isScrolled &&
+          "bg-opacity-80 backdrop-blur-sm transition duration-300 ease-out dark:bg-opacity-80"
+        } flex justify-between bg-green-600 px-4 py-2 shadow-md dark:bg-green-800`}
+      >
+        <Label />
+        <div className="flex items-center gap-2">
+          <ModeButton />
+          <GitHubButton />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+const Label = () => {
+  return (
+    <Link href="/">
+      <p className="text-2xl font-bold text-slate-50 dark:text-green-400">
+        Note
+        <span className="font-semibold text-slate-200 dark:text-slate-400">
+          Set
+        </span>
+      </p>
+    </Link>
+  );
+};
