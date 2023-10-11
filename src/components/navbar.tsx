80374eac0484f3366538af9cc8c042c22d356895
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-11 17:24:40 +0200

    ADDS: new cmpt, navigation inside profile page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,58 +1,38 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { ModeButton } from "./client-buttons";
-import { GitHubButton } from "./server-buttons";
 import Link from "next/link";
+import {
+  HomeIcon,
+  ProfileIcon,
+  LogsIcon,
+  EditIcon,
+  LogoutIcon,
+} from "@/components/svgs";
 
 export const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("load", scrolled);
-    window.addEventListener("scroll", scrolled);
-
-    return () => {
-      window.removeEventListener("load", scrolled);
-      window.removeEventListener("scroll", scrolled);
-    };
-  }, []);
-
-  function scrolled() {
-    if (window.scrollY >= 1) {
-      setIsScrolled(true);
-    } else {
-      setIsScrolled(false);
-    }
-  }
-
   return (
-    <div className="sticky top-0">
-      <nav
-        className={`${
-          isScrolled &&
-          "bg-opacity-80 backdrop-blur-sm transition duration-300 ease-out dark:bg-opacity-80"
-        } flex justify-between bg-green-600 px-4 py-2 shadow-md dark:bg-green-800`}
+    <div className="fixed inset-x-0 bottom-0 flex justify-between bg-green-600 px-4 pb-6 pt-2 dark:bg-green-800">
+      <button className="text-icon flex flex-col items-center uppercase text-white">
+        {HomeIcon}
+        home
+      </button>
+      <button className="text-icon flex flex-col items-center uppercase text-white">
+        {ProfileIcon}
+        profile
+      </button>
+      <button className="text-icon flex flex-col items-center uppercase text-white">
+        {LogsIcon}
+        logs
+      </button>
+      <button className="text-icon flex flex-col items-center uppercase text-white">
+        {EditIcon}
+        edit
+      </button>
+      <Link
+        href="/login"
+        className="text-icon flex flex-col items-center uppercase text-white"
       >
-        <Label />
-        <div className="flex items-center gap-2">
-          <ModeButton />
-          <GitHubButton />
-        </div>
-      </nav>
+        {LogoutIcon}
+        logout
+      </Link>
     </div>
-  );
-};
-
-const Label = () => {
-  return (
-    <Link href="/">
-      <p className="text-2xl font-bold text-slate-50 dark:text-green-400">
-        Note
-        <span className="font-semibold text-slate-200 dark:text-slate-400">
-          Set
-        </span>
-      </p>
-    </Link>
   );
 };
