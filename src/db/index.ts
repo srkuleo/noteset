2ebad7653d97ccb2c00f6dd7945e7706ba4d0fe0
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-12 22:52:15 +0200

    REMOVES: unnecessary comment

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,15 +2,8 @@ import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import * as schema from "./schema";
 
-/*  Potentially needed for reading env file  */
-
-// import { cwd } from "process";
-// import { loadEnvConfig } from "@next/env";
-
-// loadEnvConfig(cwd());
-
 const connection = connect({
-  url: process.env.DB_URL,
+  url: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(connection, { schema });
