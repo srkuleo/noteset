eb5cfb8d0fb7347f8db8a7cb1f9e8226819fe4d8
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-12 22:50:42 +0200

    ADDS: session provider to whole app

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute='class'>{children}</ThemeProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>;
+    </SessionProvider>
+  );
 }
