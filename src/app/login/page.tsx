5f0189a598585f76f81621ce1ed59e6c7b327cf4
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-12 22:49:39 +0200

    ADDS: abstraction to both login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,79 +1,15 @@
-import { OAuth } from "@/components/oauth";
+import { LoginForm } from "@/components/login-form";
+import { GoogleAuth } from "@/components/oauth";
 import { Separator } from "@/components/separator";
-import Link from "next/link";
+import { LoginLinks } from "@/components/login-links";
 
 export default function LoginPage() {
   return (
-    <div className="px-12 pt-32">
-      <p className="mb-8 text-lg font-bold italic text-slate-500 dark:text-slate-300/90">
-        Login, set new records!
-      </p>
-
-      <OAuth />
-
+    <div className="px-12 pt-48">
+      <GoogleAuth />
       <Separator />
-
-      <form action="" className="flex flex-col gap-2">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          autoFocus
-          required
-          className="input-field"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          required
-          className="input-field"
-        />
-        <div className="flex items-center gap-2 py-4">
-          <input
-            type="checkbox"
-            id="remember-me"
-            name="remember-me"
-            className="h-5 w-5 accent-green-500 dark:accent-green-600"
-          />
-          <label
-            htmlFor="remember-me"
-            className="text-xs text-slate-600 dark:text-slate-300"
-          >
-            Stay logged in
-          </label>
-        </div>
-        <Link
-          href="/profile"
-          type="submit"
-          className="mb-4 rounded-3xl bg-green-500 p-3 text-center font-semibold text-slate-50 shadow-md dark:bg-green-700 dark:ring-2 dark:ring-inset dark:ring-green-500"
-        >
-          Login
-        </Link>
-      </form>
-
-      <div className="space-y-2 pb-4 text-xs text-slate-500 dark:text-slate-300/90">
-        <p>
-          Forgot your{" "}
-          <Link
-            href="/"
-            className="border-b border-green-500 text-sm font-semibold text-green-500"
-          >
-            password?
-          </Link>
-        </p>
-        <p>
-          New to Noteset? Sign up{" "}
-          <Link
-            href="/sign-up"
-            className="border-b border-green-500 text-sm font-semibold text-green-500"
-          >
-            here.
-          </Link>
-        </p>
-      </div>
+      <LoginForm />
+      <LoginLinks />
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,70 +1,15 @@
-import { OAuth } from "@/components/oauth";
+import { GoogleAuth } from "@/components/oauth";
 import { Separator } from "@/components/separator";
-import Link from "next/link";
+import { SignUpForm } from "@/components/signup-form";
+import { SignUpLink } from "@/components/signup-link";
 
 export default function SignUpPage() {
   return (
-    <div className="px-12 pt-32">
-      <p className="mb-8 text-lg font-bold italic text-slate-500 dark:text-slate-300/90">
-        Sign up, start your journey today!
-      </p>
-
-      <OAuth />
-
+    <div className="px-12 pt-40">
+      <GoogleAuth />
       <Separator />
-
-      <form action="" className="flex flex-col gap-2">
-        <input
-          type="text"
-          name="username"
-          id="username"
-          placeholder="Username"
-          required
-          className="input-field"
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          autoFocus
-          required
-          className="input-field"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          required
-          className="input-field"
-        />
-        <input
-          type="password"
-          name="cpassword"
-          id="cpassowrd"
-          placeholder="Confirm password"
-          required
-          className="input-field"
-        />
-        <div className="mb-4"></div>
-        <button
-          type="submit"
-          className="mb-4 rounded-3xl bg-green-500 p-3 text-center font-semibold text-slate-50 shadow-md dark:bg-green-700 dark:ring-2 dark:ring-inset dark:ring-green-500"
-        >
-          Sign up
-        </button>
-      </form>
-
-      <p className="pb-4 text-xs text-slate-500 dark:text-slate-300/90">
-        Already have an account? Login{" "}
-        <Link
-          href="/login"
-          className="border-b border-green-500 text-sm font-semibold text-green-500"
-        >
-          here.
-        </Link>
-      </p>
+      <SignUpForm />
+      <SignUpLink />
     </div>
   );
 }
