f97c36c91fc870b8ea49fa49352b9622a014afdb
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-15 23:57:07 +0200

    MODIFY: imports

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,30 +5,30 @@ import {
   LogsIcon,
   EditIcon,
   LogoutIcon,
-} from "@/components/svgs";
+} from "@/components/Svgs";
 
 export const Navbar = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 flex justify-between bg-green-600 px-4 pb-6 pt-2 dark:bg-green-800">
-      <button className="text-icon flex flex-col items-center uppercase text-white">
+      <button className="flex flex-col items-center text-icon uppercase text-white">
         {HomeIcon}
         home
       </button>
-      <button className="text-icon flex flex-col items-center uppercase text-white">
+      <button className="flex flex-col items-center text-icon uppercase text-white">
         {ProfileIcon}
         profile
       </button>
-      <button className="text-icon flex flex-col items-center uppercase text-white">
+      <button className="flex flex-col items-center text-icon uppercase text-white">
         {LogsIcon}
         logs
       </button>
-      <button className="text-icon flex flex-col items-center uppercase text-white">
+      <button className="flex flex-col items-center text-icon uppercase text-white">
         {EditIcon}
         edit
       </button>
       <Link
         href="/login"
-        className="text-icon flex flex-col items-center uppercase text-white"
+        className="flex flex-col items-center text-icon uppercase text-white"
       >
         {LogoutIcon}
         logout
