bbc0312be952c602b399411ab33a9e5564149dd3
Srdjan <milicevic.srdjan0@gmail.com>
2023-10-15 23:55:59 +0200

    MODIFY: removes accidental semicolon

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from "next-auth/react";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider attribute="class">{children}</ThemeProvider>;
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
     </SessionProvider>
   );
 }
