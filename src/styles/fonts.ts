2216e38ed07560e5757d71c4d4727a92c7849b8e
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-01 20:45:08 +0100

    ADD:  new ts file for google fonts

---
+++ src/styles/fonts.ts
@@ -0,0 +1,21 @@
+import { Inter, Noto_Sans, Nunito, Manrope } from "next/font/google";
+
+export const inter = Inter({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const notoSans = Noto_Sans({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const nunito = Nunito({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const manrope = Manrope({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
