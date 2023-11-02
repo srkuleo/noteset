e5fc1094945bda7454b94c2871a3a225e09fb5be
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 00:51:03 +0100

    ADD: landing page

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function LandingPage() {
+  return (
+    <div className="flex grow flex-col justify-center">
+      <div className="mb-12 pl-4">
+        <h1 className="mb-8 pl-4 text-5xl font-bold text-green-500 dark:text-green-600">
+          Note<span className="font-semibold text-slate-500">Set</span>
+        </h1>
+        <h2 className="font-semibold text-slate-400 dark:text-slate-500/90">
+          Application aimed to replace conventional use of a notebook in a gym.
+        </h2>
+      </div>
+
+      <div className="mb-8 flex justify-evenly">
+        <Link
+          href="/login"
+          className="rounded-3xl bg-gradient-to-r from-white to-slate-100 px-6  py-2 font-semibold text-slate-600 shadow-md ring-1 ring-inset ring-slate-400/60"
+        >
+          Login
+        </Link>
+        <Link
+          href="/register"
+          className="rounded-3xl bg-gradient-to-r from-green-500 to-green-600 px-6 py-2 text-slate-50 shadow-md dark:from-green-600 dark:to-green-700"
+        >
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+}
