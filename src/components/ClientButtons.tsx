56572e67da4d3bc9c8f2c2bb22303bf9ebb87599
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 00:58:18 +0100

    ADD: client buttons for highlighting current path

--- src/components/ClientButtons.tsx
+++ src/components/ClientButtons.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { links } from "@/util/data";
 import { useTheme } from "next-themes";
-import { DarkModeIcon, LightModeIcon } from "./Svgs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { DarkModeIcon, LightModeIcon } from "./Icons";
 
-//Buttons that require any kind of client side action (hooks, libs based on custom hooks - state, effect, context)
+//Buttons that require any kind of client side action (hooks, libraries based on custom hooks)
 
-export const ModeButton = () => {
+export const ThemeButton = () => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
 
@@ -18,15 +21,38 @@ export const ModeButton = () => {
     setTheme(resolvedTheme === "dark" ? "light" : "dark");
   }
 
-  if (!mounted) return <></>;
+  if (!mounted) {
+    return <div className="h-7 w-7 rounded-full bg-slate-500/10" />;
+  }
 
   return (
     <button onClick={toggleMode}>
-      {resolvedTheme === "dark" ? (
-        <LightModeIcon className="h-7 w-7 text-slate-50" />
-      ) : (
-        <DarkModeIcon className="h-7 w-7 text-slate-300" />
-      )}
+      {resolvedTheme === "dark" ? LightModeIcon : DarkModeIcon}
     </button>
+  );
+};
+
+export const LinkButtons = () => {
+  const path = usePathname();
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          className={`flex grow flex-col items-center justify-center rounded-xl p-2 shadow-md ${
+            link.href === path
+              ? "bg-green-500 text-white dark:bg-green-700"
+              : "bg-white dark:bg-slate-800"
+          }`}
+          href={link.href}
+        >
+          {link.icon}
+          {link.href === path && (
+            <div className="mt-[2px] h-[1px] w-5 bg-white" />
+          )}
+        </Link>
+      ))}
+    </>
   );
 };
