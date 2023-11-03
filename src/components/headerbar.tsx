bc70a696c88841e0841ce21ae0c78042e6e4aaeb
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 01:05:29 +0100

    MODIFY: complete UI refactoring

--- src/components/headerbar.tsx
+++ src/components/headerbar.tsx
@@ -1,30 +1,15 @@
-import { ModeButton } from "./ClientButtons";
+import { ThemeButton } from "./ClientButtons";
+import { Logo } from "./Icons";
 import { GitHubButton } from "./ServerButtons";
-import Link from "next/link";
 
 export const HeaderBar = () => {
   return (
-    <div className="sticky top-0">
-      <nav className="flex justify-between bg-green-600 px-4 py-2 shadow-sm dark:bg-green-800">
-        <Label />
-        <div className="flex items-center gap-2">
-          <ModeButton />
-          <GitHubButton />
-        </div>
-      </nav>
+    <div className="flex justify-between rounded-[28px] bg-white shadow-md px-4 py-2 dark:bg-slate-800 mb-4">
+      <Logo />
+      <div className="flex items-center gap-4">
+        <ThemeButton />
+        <GitHubButton />
+      </div>
     </div>
-  );
-};
-
-const Label = () => {
-  return (
-    <Link href="/">
-      <p className="text-2xl font-bold text-slate-50 dark:text-green-400">
-        Note
-        <span className="font-semibold text-slate-300 dark:text-slate-50">
-          Set
-        </span>
-      </p>
-    </Link>
   );
 };
