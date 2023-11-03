829748dcd9800f7e1746e7e44bf615c9847a207c
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 01:25:08 +0100

    ADD: links array, easier to add future routes

---
+++ src/util/data.ts
@@ -0,0 +1,8 @@
+import { HomeIcon, ProfileIcon, LogsIcon } from "@/components/Icons";
+import type { Links } from "./types";
+
+export const links: Links = [
+  { href: "/user", icon: HomeIcon },
+  { href: "/user/profile", icon: ProfileIcon },
+  { href: "/user/logs", icon: LogsIcon },
+];
