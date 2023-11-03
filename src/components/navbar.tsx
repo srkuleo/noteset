a3f612c208478f1a929656276e98397a9b61f7e1
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 01:05:51 +0100

    MODIFY: complete UI refactoring

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,38 +1,11 @@
-import Link from "next/link";
-import {
-  HomeIcon,
-  ProfileIcon,
-  LogsIcon,
-  EditIcon,
-  LogoutIcon,
-} from "@/components/Svgs";
+import { LinkButtons } from "./ClientButtons";
+import { LogoutButton } from "./ServerButtons";
 
 export const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 bottom-0 flex justify-between bg-green-600 px-4 pb-6 pt-2 dark:bg-green-800">
-      <button className="flex flex-col items-center text-icon uppercase text-white">
-        {HomeIcon}
-        home
-      </button>
-      <button className="flex flex-col items-center text-icon uppercase text-white">
-        {ProfileIcon}
-        profile
-      </button>
-      <button className="flex flex-col items-center text-icon uppercase text-white">
-        {LogsIcon}
-        logs
-      </button>
-      <button className="flex flex-col items-center text-icon uppercase text-white">
-        {EditIcon}
-        edit
-      </button>
-      <Link
-        href="/login"
-        className="flex flex-col items-center text-icon uppercase text-white"
-      >
-        {LogoutIcon}
-        logout
-      </Link>
+    <div className="mb-8 flex gap-2">
+      <LinkButtons />
+      <LogoutButton />
     </div>
   );
 };
