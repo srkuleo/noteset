f15cdcee82b5b25e61fed432a38be3bf67bb8425
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 01:08:02 +0100

    MODIFY: small changes in coding, for clarity

--- src/components/separator.tsx
+++ src/components/separator.tsx
@@ -1,11 +1,11 @@
 export const Separator = () => {
   return (
-    <div className="mb-4 flex items-center gap-2">
-      <div className="w-32 border-b border-green-500/70"></div>
+    <div className="mb-4 flex items-center gap-2 p-2">
+      <div className="h-[1px] grow bg-green-500/70"></div>
       <p className="text-slate-main600 text-sm  italic dark:text-slate-300/90">
         or
       </p>
-      <div className="w-32 border-b border-green-500/70"></div>
+      <div className="h-[1px] grow bg-green-500/70"></div>
     </div>
   );
 };
