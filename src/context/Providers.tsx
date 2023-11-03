7d5bacb215637518c8470bcbecfd959622aaef7a
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-03 01:09:43 +0100

    MODIFY: moves to context folder

---
+++ src/context/Providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </SessionProvider>
+  );
+}
