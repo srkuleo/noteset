92449fe401209cf66cb9dd23fb56981002477085
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-06 23:15:27 +0100

    ADD: button to switch between dark and ligjht mode

---
+++ src/components/global/ThemeButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { LightModeIcon, DarkModeIcon } from "@/icons/theme";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
+export const ThemeButton = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    console.log("ThemeButton mounted.");
+    setMounted(true);
+  }, []);
+
+  function toggleMode() {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  }
+
+  if (mounted) {
+    return (
+      <button onClick={toggleMode}>
+        {resolvedTheme === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
+      </button>
+    );
+  }
+
+  return (
+    <div className="h-6 w-6 rounded-full bg-slate-500/50 dark:bg-slate-800" />
+  );
+};
