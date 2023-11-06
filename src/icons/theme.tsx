cc88e048fd3366f077defb9198021db6a0fc6ebf
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-06 23:10:57 +0100

    ADD: color theme icons

---
+++ src/icons/theme.tsx
@@ -0,0 +1,37 @@
+export const DarkModeIcon = () => {
+  return (
+    <svg
+      className="h-7 w-7"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export const LightModeIcon = () => {
+  return (
+    <svg
+      className="h-7 w-7"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth={2}
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </svg>
+  );
+};
