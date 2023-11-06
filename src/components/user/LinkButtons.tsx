9a9565bc000b770f4f3b01676f67da65fe48cf06
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-06 23:12:52 +0100

    ADD: link buttons in navbar, for pages navigation

---
+++ src/components/user/LinkButtons.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { HomeIcon, ProfileIcon, LogsIcon } from "@/icons/user/link-icons";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export type Links = {
+  href: string;
+  icon: JSX.Element;
+}[];
+
+const links: Links = [
+  { href: "/user", icon: HomeIcon },
+  { href: "/user/profile", icon: ProfileIcon },
+  { href: "/user/logs", icon: LogsIcon },
+];
+
+const highlightPath = (
+  <div className="absolute bottom-[6px] h-[1px] w-5 bg-white" />
+);
+
+export const LinkButtons = () => {
+  const path = usePathname();
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={`relative flex grow items-center justify-center rounded-xl shadow-md transition active:scale-95 ${
+            link.href === path ? "active-page" : "idle-page"
+          }`}
+        >
+          {link.icon}
+          {link.href === path && highlightPath}
+        </Link>
+      ))}
+    </>
+  );
+};
