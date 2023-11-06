e4adcce6c62985d316d0e67a8ae4584a904e36ee
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-06 23:15:49 +0100

    ADD: simple logo button to go to landing page

---
+++ src/components/global/LandingPageButton.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "@/icons/logo";
+import Link from "next/link";
+
+export const LandingPageButton = () => {
+  return (
+    <Link href="/">
+      <Logo />
+    </Link>
+  );
+};
