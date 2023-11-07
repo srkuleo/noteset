b167657002caf99819fee2310a1d32709623e22a
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:34:58 +0100

    MODIFY: remove margine bottom, gap added to main

--- src/components/user/UserPageHeader.tsx
+++ src/components/user/UserPageHeader.tsx
@@ -1,6 +1,6 @@
 import { Logo } from "@/icons/logo";
-import { ThemeButton } from "../global/ThemeButton";
 import { GitHubButton } from "../global/GitHubButton";
+import { ThemeButton } from "../global/ThemeButton";
 import { LinkButtons } from "./LinkButtons";
 import { LogoutButton } from "./LogoutButton";
 
@@ -15,7 +15,7 @@ export const UserPageHeader = () => {
 
 const HeaderBar = () => {
   return (
-    <div className="mb-4 flex justify-between rounded-[28px] bg-white px-4 py-2 shadow-md dark:bg-slate-800">
+    <div className="flex justify-between rounded-[28px] bg-white px-4 py-2 shadow-md dark:bg-slate-800">
       <Logo />
       <div className="flex items-center gap-4">
         <ThemeButton />
@@ -27,7 +27,7 @@ const HeaderBar = () => {
 
 const Navbar = () => {
   return (
-    <div className="mb-8 flex h-10 gap-2">
+    <div className="flex gap-1">
       <LinkButtons />
       <LogoutButton />
     </div>
