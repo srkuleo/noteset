95ac186a129e29ad9d923268646997894cc907e1
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:33:19 +0100

    ADD: abstracts from landing layout

---
+++ src/components/global/LandingPageHeader.tsx
@@ -0,0 +1,15 @@
+import { LandingPageButton } from "./LandingPageButton";
+import { ThemeButton } from "./ThemeButton";
+import { GitHubButton } from "./GitHubButton";
+
+export const LandingPageHeader = () => {
+  return (
+    <div className="flex justify-between border-b border-slate-400/60 pb-2 dark:border-slate-700">
+      <LandingPageButton />
+      <div className="flex items-center gap-4">
+        <ThemeButton />
+        <GitHubButton />
+      </div>
+    </div>
+  );
+};
