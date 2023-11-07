95213cdffa786f3c820ece780452a3c5fecc3781
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:32:37 +0100

    ADD: sceleton for main component of the page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,7 @@
 export default function UserHomePage() {
   return (
-    <div className="flex grow flex-col items-center">
-      This is user`s Home Page!
+    <div className="grow rounded-xl bg-white p-2 shadow-sm dark:bg-slate-700/70">
+      <p>This is user`s Home page.</p>
     </div>
   );
 }
