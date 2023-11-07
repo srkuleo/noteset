3610239d39aaf58cbfea3234dc730d65d5de9627
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:31:38 +0100

    MODIFY: small changes to UI, WIP

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <div className="flex grow flex-col justify-center">
-      <div className="mb-12 pl-4">
-        <h1 className="mb-8 pl-4 text-5xl font-bold text-green-500 dark:text-green-600">
-          Note<span className="font-semibold text-slate-500">Set</span>
+      <div className="mb-16 pl-4">
+        <h1 className="mb-8 pl-4 text-5xl font-extrabold text-green-500 dark:text-green-600">
+          Note<span className="font-semibold italic text-slate-500">Set</span>
         </h1>
-        <h2 className="font-semibold text-slate-400 dark:text-slate-500/90">
+        <h2 className="font-semibold text-slate-400 dark:text-slate-500">
           Application aimed to replace conventional use of a notebook in a gym.
         </h2>
       </div>
 
-      <div className="mb-8 flex justify-evenly">
+      <div className="mb-8 flex justify-center gap-12">
         <Link
           href="/login"
           className="rounded-3xl bg-gradient-to-r from-white to-slate-100 px-6  py-2 font-semibold text-slate-600 shadow-md ring-1 ring-inset ring-slate-400/60"
