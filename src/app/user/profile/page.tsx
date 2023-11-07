5b9cc44ab3a6312bdb2ac84be6acc880d3128b34
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:32:50 +0100

    ADD: sceleton for main component of the page

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,3 +1,7 @@
 export default function UserLogsPage() {
-  return <div>This is user`s Profile Page!</div>;
+  return (
+    <div className="grow rounded-xl bg-white dark:bg-slate-700/70 p-2 text-center shadow-sm">
+      <p>This is user`s Profile page.</p>
+    </div>
+  );
 }
