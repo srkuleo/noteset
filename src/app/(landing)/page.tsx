be702f68a099e2ca52edd753769a0d3077ef36fd
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 03:01:08 +0100

    MODIFY: small UI changes, WIP

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -12,16 +12,19 @@ export default function LandingPage() {
         </h2>
       </div>
 
-      <div className="mb-8 flex justify-center gap-12">
+      <div className="mb-8 flex flex-col items-center justify-center gap-2 text-center">
         <Link
           href="/login"
-          className="rounded-3xl bg-gradient-to-r from-white to-slate-100 px-6  py-2 font-semibold text-slate-600 shadow-md ring-1 ring-inset ring-slate-400/60"
+          className="w-[120px] rounded-2xl bg-gradient-to-r from-white to-slate-100 px-6 py-2 font-semibold text-slate-600 shadow-md ring-1 ring-inset ring-slate-400/60 transition active:scale-95"
         >
           Login
         </Link>
+        <p className="text-sm italic text-slate-400/70 dark:text-slate-500/90">
+          or
+        </p>
         <Link
           href="/register"
-          className="rounded-3xl bg-gradient-to-r from-green-500 to-green-600 px-6 py-2 text-slate-50 shadow-md dark:from-green-600 dark:to-green-700"
+          className="w-[120px] rounded-2xl bg-gradient-to-r from-green-500 to-green-600 px-6 py-2 text-slate-50 shadow-md transition active:scale-95 dark:from-green-600 dark:to-green-700"
         >
           Register
         </Link>
