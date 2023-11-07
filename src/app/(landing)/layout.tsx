b6eaf9fe043e99b8181d811710eb18a46e06d8c0
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-07 02:29:16 +0100

    MODIFY: abstraction to header for landing page

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,6 +1,4 @@
-import { LandingPageButton } from "@/components/global/LandingPageButton";
-import { ThemeButton } from "@/components/global/ThemeButton"; 
-import { GitHubButton } from "@/components/global/GitHubButton";
+import { LandingPageHeader } from "@/components/global/LandingPageHeader";
 
 export default function LandingPageLayout({
   children,
@@ -9,14 +7,7 @@ export default function LandingPageLayout({
 }) {
   return (
     <main className="flex h-screen flex-col p-4">
-      <div className="flex justify-between border-b border-slate-400/60 pb-2 dark:border-slate-700">
-        <LandingPageButton />
-        <div className="flex items-center gap-4">
-          <ThemeButton />
-          <GitHubButton />
-        </div>
-      </div>
-
+      <LandingPageHeader />
       {children}
     </main>
   );
