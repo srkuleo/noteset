3a25588b07e9f4ae604d3d98f0c45a5a6658efb7
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-08 14:44:12 +0100

    MODIFY: small UI tweaks

--- src/components/login/GoogleAuthButton.tsx
+++ src/components/login/GoogleAuthButton.tsx
@@ -4,9 +4,9 @@ import { GoogleIcon } from "@/icons/google";
 
 export const GoogleAuthButton = () => {
   return (
-    <button className="mb-4 flex w-full items-center justify-center gap-4 rounded-xl bg-slate-50 py-2 shadow-md ring-1 ring-slate-300 dark:bg-slate-200 dark:ring-0">
+    <button className="flex items-center justify-center gap-4 rounded-xl bg-white py-3 shadow-md text-sm ring-1 ring-slate-400/50">
       <GoogleIcon />
-      <p className="text-sm font-semibold italic dark:text-slate-600">
+      <p className="font-semibold italic dark:text-slate-500">
         Continue with Google
       </p>
     </button>
