0015b073b3b60bea9272a74a3a0c5adad732dc13
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-08 14:45:17 +0100

    MODIFY: removes margine, adds padding on y axis

--- src/components/login/Separator.tsx
+++ src/components/login/Separator.tsx
@@ -1,6 +1,6 @@
 export const Separator = () => {
   return (
-    <div className="mb-4 flex items-center gap-2 p-2">
+    <div className="flex items-center gap-2 px-2 py-6">
       <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-700" />
       <p className="text-sm italic dark:text-slate-400">or</p>
       <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-700" />
