d07ae15f1a1db2d66960c26205133fe8f99cff75
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-11 22:23:44 +0100

    ADD: workout page, WIP

--- src/app/user/workout/page.tsx
+++ src/app/user/workout/page.tsx
@@ -6,7 +6,7 @@ export default function WorkoutPage() {
       <p className="py-10 text-xl font-semibold">
         This is user`s Workout page!
       </p>
-      <Link href="/user" className="text-slate-300">Go back to profile</Link>
+      <Link href="/user">Go back to profile</Link>
     </div>
   );
 }
