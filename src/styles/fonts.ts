d28d14ed8f85815f1ac999ed16eb71fbc59ec300
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-13 23:44:33 +0100

    REMOVE: inter and noto_sans fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,17 +1,4 @@
-import { Inter, Noto_Sans, Nunito, Manrope } from "next/font/google";
-
-export const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  // variable: "--font-inter"
-});
-
-export const noto_sans = Noto_Sans({
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
-  display: "swap",
-  // variable: "--font-noto-sans"
-});
+import { Nunito, Manrope } from "next/font/google";
 
 export const nunito = Nunito({
   subsets: ["latin"],
