cff86cbd50fac7dd91c881abc1b1f4b354c6f5f1
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:12:58 +0100

    ADD: custom metadata

--- src/app/user/(homepage)/profile/page.tsx
+++ src/app/user/(homepage)/profile/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Profile",
+};
+
 export default function UserLogsPage() {
   return (
     <div className="grow pb-4 pt-2 text-center">
