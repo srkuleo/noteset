1cfde5cf46594950d2c52159f480eef851178a1e
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:12:47 +0100

    ADD: custom metadata

--- src/app/user/(homepage)/logs/page.tsx
+++ src/app/user/(homepage)/logs/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Logs",
+};
+
 export default function LogsPage() {
   return (
     <div className="grow pb-4 pt-2 text-right">
