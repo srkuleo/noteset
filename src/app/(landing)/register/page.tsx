5bd4c66bf0a94b2f17699db3d56ad7b493935e3f
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:12:08 +0100

    ADD: custom metadata

--- src/app/(landing)/register/page.tsx
+++ src/app/(landing)/register/page.tsx
@@ -1,8 +1,13 @@
-import { PageWrapper } from "@/components/login/PageWrapper";
-import { GoogleAuthButton } from "@/components/login/GoogleAuthButton";
-import { Separator } from "@/components/login/Separator";
-import { RegisterForm } from "@/components/register/RegisterForm";
-import { RegisterLink } from "@/components/register/RegisterLink";
+import { PageWrapper } from "../../../components/login/PageWrapper";
+import { GoogleAuthButton } from "../../../components/login/GoogleAuthButton";
+import { Separator } from "../../../components/login/Separator";
+import { RegisterForm } from "../../../components/register/RegisterForm";
+import { RegisterLink } from "../../../components/register/RegisterLink";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
 
 export default function RegisterPage() {
   return (
