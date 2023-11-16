dc8f7ba50a7e023aad1ad16504317d0b419d24a4
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:11:28 +0100

    ADD: custom helper function for checking env

--- src/envs.ts
+++ src/envs.ts
@@ -2,15 +2,23 @@ import { z } from "zod";
 
 const envVariablesSchema = z.object({
   DATABASE_URL: z.string().url(),
-  NEXTAUTH_SECRET: z.string(),
+  AUTH_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
 });
-
-envVariablesSchema.parse(process.env);
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv extends z.infer<typeof envVariablesSchema> {}
   }
 }
+
+const checkEvn = () => {
+  const envCheck = envVariablesSchema.safeParse(process.env);
+
+  if (!envCheck.success) {
+    throw new Error("Env variable is missing.");
+  }
+};
+
+export default checkEvn;
