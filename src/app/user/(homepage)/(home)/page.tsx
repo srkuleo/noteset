1b74e7dbaa4e89705da22f320d3e036a302d5c55
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:12:40 +0100

    ADD: custom metadata

--- src/app/user/(homepage)/(home)/page.tsx
+++ src/app/user/(homepage)/(home)/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
 export default function UserHomePage() {
   return (
-    <div className="grow pt-2 pb-4">
+    <div className="grow pb-4 pt-2">
       <p>This is user`s Home page.</p>
     </div>
   );
