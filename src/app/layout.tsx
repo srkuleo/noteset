555a167573f0606f248cc620286d72cf694ee769
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-16 01:11:45 +0100

    ADD: custom metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import { nunito } from "../styles/fonts";
 import "../styles/global.css";
 import { Providers } from "../context/Providers";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Noteset",
+    default: "Noteset",
+  },
+  description:
+    "A minimalistic workout tracking PWA aimed to replace conventional usage of a notebook in the gym.",
+  metadataBase: new URL("https://noteset.vercel.app"),
+};
 
 export default function RootLayout({
   children,
