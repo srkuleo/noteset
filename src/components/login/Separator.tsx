bd6188332c8ebc5e90fcecf76341406c071f61c0
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-19 00:30:50 +0100

    MODIFY: color change

--- src/components/login/Separator.tsx
+++ src/components/login/Separator.tsx
@@ -1,9 +1,9 @@
 export const Separator = () => {
   return (
     <div className="flex items-center gap-2 px-2 py-6">
-      <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-700" />
-      <p className="text-sm italic dark:text-slate-400">or</p>
-      <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-700" />
+      <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-600" />
+      <p className="text-sm italic">or</p>
+      <div className="h-[1px] grow bg-slate-400/60 dark:bg-slate-600" />
     </div>
   );
 };
