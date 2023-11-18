c14df4ae8558bb34a112f5238c695b5e775a4d34
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-19 00:27:52 +0100

    MODIFY: form button

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -15,10 +15,7 @@ export const LoginForm = () => {
         placeholder="Password"
         className="input-field"
       />
-      <button
-        type="submit"
-        className="mb-4 rounded-3xl bg-green-500 p-3 text-center font-semibold text-slate-50 shadow-md transition active:scale-95 dark:bg-green-700 dark:ring-2 dark:ring-inset dark:ring-green-500"
-      >
+      <button className="my-4 rounded-3xl bg-green-500 p-3 text-center font-semibold text-slate-50 shadow-md transition active:scale-95 dark:bg-green-700 dark:shadow-slate-950">
         Login
       </button>
     </form>
