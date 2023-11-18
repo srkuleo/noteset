10090b3645bd9465f7fbd9d4e1663304c1900719
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-19 00:31:40 +0100

    ADD: position sticky to userpage header

--- src/components/user/UserPageHeader.tsx
+++ src/components/user/UserPageHeader.tsx
@@ -6,10 +6,10 @@ import { LogoutButton } from "./LogoutButton";
 
 export const UserPageHeader = () => {
   return (
-    <>
+    <div className="sticky top-4 space-y-4">
       <HeaderBar />
       <Navbar />
-    </>
+    </div>
   );
 };
 
