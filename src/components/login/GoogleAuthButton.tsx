366cc11c28c0d0e6b241f17b7b0a1ee904f7762d
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-20 00:10:58 +0100

    MODIFY: active scale on button, color scheme

--- src/components/login/GoogleAuthButton.tsx
+++ src/components/login/GoogleAuthButton.tsx
@@ -4,7 +4,7 @@ import { GoogleIcon } from "../../icons/google";
 
 export const GoogleAuthButton = () => {
   return (
-    <button className="flex items-center justify-center gap-4 rounded-xl bg-white py-2.5 text-sm shadow-md ring-1 ring-slate-400/50 dark:ring-0 dark:shadow-slate-950">
+    <button className="flex items-center justify-center gap-4 rounded-xl bg-white py-2.5 text-sm shadow-md ring-1 ring-slate-400/50 active:scale-95 dark:shadow-slate-950 dark:ring-0">
       <GoogleIcon />
       <p className="font-semibold italic dark:text-slate-500">
         Continue with Google
