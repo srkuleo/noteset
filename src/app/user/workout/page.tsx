fe07453e37a9d68763a8a07a4ba6036ba000469b
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-19 01:17:46 +0100

    MODIFY: Link to home page

--- src/app/user/workout/page.tsx
+++ src/app/user/workout/page.tsx
@@ -6,7 +6,7 @@ export default function WorkoutPage() {
       <p className="py-10 text-xl font-semibold">
         This is user`s Workout page!
       </p>
-      <Link href="/user">Go back to profile</Link>
+      <Link href="/user/home">Go back to Home page</Link>
     </div>
   );
 }
