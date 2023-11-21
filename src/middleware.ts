3348f4f57ae86908b9343fcd88834bf4d6c5ed9c
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-21 02:43:25 +0100

    WIP

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,8 @@
-// import NextAuth from "next-auth";
-// import { authConfig } from "./auth.config";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-function middleware() {
-  console.log("Mwr");
-}
+export default NextAuth(authConfig).auth;
 
-export default middleware;
-// export default NextAuth(authConfig).auth;
-
-// export const config = {
-//   matcher: ["/user/:path*"],
-// };
+export const config = {
+  matcher: [],
+};
