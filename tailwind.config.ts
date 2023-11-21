591f9fb79863e1693d3f3ad06b15267e5acf4f1e
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-21 02:41:17 +0100

    ADD: padding for notch on apple

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ const config: Config = {
       fontSize: {
         icon: ["0.625rem", "1.125rem"],
       },
+      spacing: {
+        "safe-top": "env(safe-area-inset-top)",
+        "safe-bottom": "env(safe-area-inset-bottom)",
+        "safe-left": "env(safe-area-inset-left)",
+        "safe-right": "env(safe-area-inset-right)",
+      },
     },
   },
   plugins: [],
