bd279ba8b2bc34b33b36a97f4ebded3485b4a45a
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-21 04:19:55 +0100

    ADD: margine top for both register and login page

--- src/components/login/PageWrapper.tsx
+++ src/components/login/PageWrapper.tsx
@@ -1,6 +1,6 @@
 export const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex grow items-center justify-center py-4">
+    <div className="mt-8 flex grow items-center justify-center py-4">
       <div className="flex max-w-[260px] grow flex-col">{children}</div>
     </div>
   );
