4d86594b597b239bc312fa8cd5f12b828ddc3ab2
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-22 01:22:02 +0100

    MODIFY: updates header UI to be free of hooks

--- src/components/global/LandingPageHeader.tsx
+++ src/components/global/LandingPageHeader.tsx
@@ -1,36 +1,10 @@
-"use client";
-
 import { LandingPageButton } from "./LandingPageButton";
 import { ThemeButton } from "./ThemeButton";
 import { GitHubButton } from "./GitHubButton";
-import { useEffect, useState } from "react";
 
 export const LandingPageHeader = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    function handleScroll() {
-      if (window.scrollY > 0 && isScrolled !== true) {
-        console.log(window.scrollY);
-        setIsScrolled(true);
-      }
-      if (window.scrollY <= 0) {
-        setIsScrolled(false);
-      }
-    }
-
-    window.addEventListener("load", handleScroll);
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("load", handleScroll);
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isScrolled]);
-
   return (
-    <div
-      className={`pt-safe-top fixed w-full px-4 ${isScrolled && "navbar-blur"}`}
-    >
+    <div className="fixed w-full bg-slate-300/80 px-4 pt-safe-top backdrop-blur-sm dark:bg-slate-950/90">
       <div className="flex justify-between border-b border-slate-400/60 py-2 dark:border-slate-800">
         <LandingPageButton />
         <div className="flex items-center gap-4">
