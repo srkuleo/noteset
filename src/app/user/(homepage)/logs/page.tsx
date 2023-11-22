8f80679ee27d53de19058fcaa1836311485c78e7
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-22 01:21:02 +0100

    MODIFY: padding top, WIP

--- src/app/user/(homepage)/logs/page.tsx
+++ src/app/user/(homepage)/logs/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
 
 export default function LogsPage() {
   return (
-    <div className="grow px-4 text-right">
+    <div className="grow px-4 pt-40 text-right">
       <p>This is user`s Logs page.</p>
     </div>
   );
