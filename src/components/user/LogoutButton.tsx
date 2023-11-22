1ce50d8b8f2dfd7c72bc6d05bf358f25c9ea4e6d
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-22 17:14:51 +0100

    MODIFY: renders JSX in Link component

--- src/components/user/LogoutButton.tsx
+++ src/components/user/LogoutButton.tsx
@@ -7,7 +7,7 @@ export const LogoutButton = () => {
       href="/login"
       className="flex items-center rounded-xl bg-white px-3 py-2 shadow-md active:scale-95 dark:bg-slate-800"
     >
-      <LogoutIcon />
+      {LogoutIcon}
     </Link>
   );
 };
