8d46e935977fff486953299d9a8d965f87972639
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-22 19:25:29 +0100

    MODIFY: padding top

--- src/components/navbars/LandingPageHeader.tsx
+++ src/components/navbars/LandingPageHeader.tsx
@@ -6,7 +6,7 @@ import { GitHubButton } from "./GitHubButton";
 export const LandingPageHeader = () => {
   return (
     <div className="fixed w-full bg-slate-300/80 px-4 pt-safe-top backdrop-blur-sm dark:bg-slate-950/90">
-      <div className="flex justify-between border-b border-slate-400/60 py-2 dark:border-slate-800">
+      <div className="flex justify-between border-b border-slate-400/60 pt-4 pb-2 dark:border-slate-800">
         <LandingPageButton />
         <div className="flex items-center gap-4">
           <ThemeButton />
