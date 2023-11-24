3957dcb37f2a5c21776246706d4a6a8a686ecad9
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-24 22:17:05 +0100

    ADD: providers, needs NodeJS for bcrypt

---
+++ src/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth({
+  ...authConfig,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({}),
+  ],
+});
