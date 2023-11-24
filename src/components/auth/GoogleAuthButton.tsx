cb3dba78eb2ee750114363b5b03235e3163d5508
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-24 22:29:20 +0100

    MODIFY: server component with signin function

--- src/components/auth/GoogleAuthButton.tsx
+++ src/components/auth/GoogleAuthButton.tsx
@@ -1,14 +1,21 @@
-"use client";
-
 import { GoogleIcon } from "../../icons/auth/google";
+import { signIn } from "@/auth";
 
 export const GoogleAuthButton = () => {
   return (
-    <button className="flex items-center justify-center gap-4 rounded-xl bg-white py-2.5 text-sm shadow-md ring-1 ring-slate-400/50 transition active:scale-95 dark:shadow-slate-950 dark:ring-0">
-      <GoogleIcon />
-      <p className="font-semibold italic dark:text-slate-500">
-        Continue with Google
-      </p>
-    </button>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google", { redirectTo: "/user/home" });
+      }}
+      className="flex"
+    >
+      <button className="flex grow items-center justify-center gap-4 rounded-xl bg-white py-2.5 text-sm shadow-md ring-1 ring-slate-400/50 transition active:scale-95 dark:shadow-slate-950 dark:ring-0">
+        <GoogleIcon />
+        <p className="font-semibold italic dark:text-slate-500">
+          Continue with Google
+        </p>
+      </button>
+    </form>
   );
 };
