c86a25672a548c1679d3cc1aaad9760da9f14a79
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-24 22:25:50 +0100

    MODIFY: changes matcher from test version

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: "/register",
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
