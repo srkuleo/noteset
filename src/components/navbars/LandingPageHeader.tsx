b8eefd1d50aec3eaa92c905750d86fc5da0d9e07
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-29 00:23:56 +0100

    MODIFY: backdrop blur and opacity of navbar

--- src/components/navbars/LandingPageHeader.tsx
+++ src/components/navbars/LandingPageHeader.tsx
@@ -5,7 +5,7 @@ import { GitHubButton } from "./GitHubButton";
 
 export const LandingPageHeader = () => {
   return (
-    <div className="fixed w-full border-b border-slate-400/50 bg-slate-300/80 px-4 pt-safe-top backdrop-blur-sm dark:border-slate-800 dark:bg-slate-950/80">
+    <div className="fixed w-full border-b border-slate-400/50 bg-slate-300/50 px-4 pt-safe-top backdrop-blur-md dark:backdrop-blur-xl dark:border-slate-800 dark:bg-slate-950/50">
       <div className="flex justify-between py-2">
         <LandingPageButton />
         <div className="flex items-center gap-4">
