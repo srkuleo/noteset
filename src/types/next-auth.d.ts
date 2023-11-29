5f1787d378acf7bfecfa300e6555e01b2df15747
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-30 00:03:22 +0100

    MODIFY: session interface accept id

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,12 @@
 import type { DefaultSession } from "next-auth";
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  interface User {
-    username?: string | undefined;
-  }
-
   interface Session {
     user: {
       id: string;
-      username?: string | undefined;
     } & DefaultSession["user"];
   }
 }
