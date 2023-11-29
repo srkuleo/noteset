0bf3ec0976d0c84c68fc8df2742f82c5879329b8
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-30 00:29:14 +0100

    MODIFY: removes custom route protection, WIP

--- src/app/user/(userpage)/home/page.tsx
+++ src/app/user/(userpage)/home/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { auth } from "@/auth";
 import { db } from "@/db";
-import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -10,12 +9,8 @@ export const metadata: Metadata = {
 export default async function UserHomePage() {
   const session = await auth();
 
-  if (!session?.user.id) {
-    redirect("/api/auth/signin");
-  }
-
   const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.id, session.user.id),
+    where: (users, { eq }) => eq(users.id, session!.user.id),
   });
 
   return (
