d34ababcd45d58182cc6fb4aa8621089426e4f8f
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-30 00:02:05 +0100

    MODIFY: add signout function from authjs

--- src/components/user/LogoutButton.tsx
+++ src/components/user/LogoutButton.tsx
@@ -1,13 +1,17 @@
+import { signOut } from "@/auth";
 import { LogoutIcon } from "../../icons/user/logout";
-import Link from "next/link";
 
 export const LogoutButton = () => {
   return (
-    <Link
-      href="/login"
-      className="flex items-center rounded-xl bg-white px-3 py-2 shadow-md active:scale-95 dark:bg-slate-800"
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
     >
-      {LogoutIcon}
-    </Link>
+      <button className="flex items-center rounded-xl bg-white px-3 py-2 shadow-md active:scale-95 dark:bg-slate-800">
+        {LogoutIcon}
+      </button>
+    </form>
   );
 };
