59fd9db333afbfe416c144490f250d61e5220b8f
Srdjan <milicevic.srdjan0@gmail.com>
2023-11-30 00:00:47 +0100

    ADD: custom protection for the route

--- src/app/user/(userpage)/home/page.tsx
+++ src/app/user/(userpage)/home/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { auth } from "@/auth";
-
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -8,11 +9,18 @@ export const metadata: Metadata = {
 
 export default async function UserHomePage() {
   const session = await auth();
-  console.log(session?.user);
+
+  if (!session?.user.id) {
+    redirect("/api/auth/signin");
+  }
+
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.id, session.user.id),
+  });
 
   return (
     <div className="grow px-4 pt-40">
-      <p>This is ${session?.user.username || session?.user.name} Home page.</p>
+      <p>Welcome {user?.username} to your Homepage.</p>
     </div>
   );
 }
