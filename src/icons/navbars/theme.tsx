9aca31b494db05df0212a570b96e2882d44fd6fe
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-06 19:45:07 +0100

    MODIFY: bug with h-7 w-7, uses width and height

--- src/icons/navbars/theme.tsx
+++ src/icons/navbars/theme.tsx
@@ -1,16 +1,18 @@
 export const MoonIcon = () => {
   return (
     <svg
-      className="h-7 w-7"
+      width={28}
+      height={28}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
       fill="currentColor"
-      aria-hidden="true"
+      viewBox="0 0 24 24"
+      strokeWidth={1.7}
+      stroke="currentColor"
     >
       <path
-        fillRule="evenodd"
-        d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
-        clipRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
       />
     </svg>
   );
@@ -19,13 +21,13 @@ export const MoonIcon = () => {
 export const SunIcon = () => {
   return (
     <svg
-      className="h-7 w-7"
+      width={28}
+      height={28}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
       fill="currentColor"
+      viewBox="0 0 24 24"
+      strokeWidth={1.7}
       stroke="currentColor"
-      strokeWidth={2}
-      aria-hidden="true"
     >
       <path
         strokeLinecap="round"
