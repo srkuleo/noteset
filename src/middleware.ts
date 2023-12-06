0b579d39b9e4bbfee414e0d6f212b40f8b74a6b2
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-06 19:24:39 +0100

    MODIFY: now works with kinde api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import type { NextRequest } from "next/server";
 
-export default NextAuth(authConfig).auth;
-
+export default function middleware(req: NextRequest) {
+  return withAuth(req);
+}
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|site|shortcut-icon.png).*)",
-  ],
+  matcher: ["/user/:path*"],
 };
