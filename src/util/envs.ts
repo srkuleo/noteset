776400209de9c97672cea4f1231df1964e23efc2
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-06 19:46:15 +0100

    MODIFY: envVariablesSchema changes

--- src/util/envs.ts
+++ src/util/envs.ts
@@ -2,11 +2,6 @@ import { z } from "zod";
 
 const envVariablesSchema = z.object({
   DATABASE_URL: z.string().url(),
-  AUTH_SECRET: z.string(),
-  AUTH_GITHUB_ID: z.string(),
-  AUTH_GITHUB_SECRET: z.string(),
-  AUTH_GOOGLE_ID: z.string(),
-  AUTH_GOOGLE_SECRET: z.string(),
 });
 
 declare global {
