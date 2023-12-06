20cf04e8da03756f975434a616a64476b08b98b9
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-06 19:35:31 +0100

    REMOVE: unused login and register components

--- src/components/auth/PageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-export const PageWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex justify-center pb-4 px-4 pt-40">
-      <div className="flex max-w-[260px] grow flex-col">{children}</div>
-    </div>
-  );
-};
