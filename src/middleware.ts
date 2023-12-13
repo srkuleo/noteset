d41915b094f32740de2e7afaee2aef3721647b2c
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-13 23:29:41 +0100

    MODIFY: adds matcher and redirect to /landing from base URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  return withAuth(req);
+  if (
+    req.url === "http://localhost:3000/" ||
+    req.url === "https://noteset.vercel.app"
+  ) {
+    return NextResponse.redirect(new URL("/landing", req.url));
+  } else {
+    return withAuth(req);
+  }
 }
 export const config = {
-  matcher: ["/user/:path*"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|site|shortcut-icon|landing).*)",
+  ],
 };
