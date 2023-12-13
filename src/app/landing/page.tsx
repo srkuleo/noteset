19aa1d0011f1b4a6b9ce6293284d683957c86e6a
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-13 23:38:00 +0100

    MODIFY: now on /landing route instead of / - needed for middleware

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,48 @@
+import { manrope } from "@/styles/fonts";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function LandingPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  console.log({ user });
+  if (user?.given_name) {
+    const username = user.given_name.toLowerCase();
+    redirect(`/${username}`);
+  } else if (user?.family_name) {
+    const lastname = user.family_name.toLowerCase();
+    redirect(`/${lastname}`);
+  }
+
+  return (
+    <div className="flex grow flex-col justify-center px-4 pb-8 pt-18">
+      <div className="space-y-8 pb-16 pl-4">
+        <h1 className="w-fit bg-gradient-to-r from-green-400 to-violet-500 bg-clip-text pl-2 text-6xl font-extrabold text-transparent dark:from-green-500 dark:to-violet-600">
+          Note<span className="font-bold">Set</span>
+        </h1>
+        <h2
+          className={`${manrope.className} font-semibold dark:text-slate-400`}
+        >
+          Personalized workout tracking PWA, aimed to replace conventional use
+          of a notebook in a gym. Made by someone who used to bring one to the
+          gym.
+        </h2>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-2 text-center">
+        <LoginLink className="w-[120px] rounded-2xl bg-violet-500/90 py-2 font-semibold text-white shadow-md transition active:scale-95">
+          Login
+        </LoginLink>
+        <p className="text-sm italic dark:text-slate-400">or</p>
+        <RegisterLink className="w-[120px] rounded-2xl bg-green-500 py-2 font-semibold text-white shadow-md transition active:scale-95 dark:bg-green-600">
+          Register
+        </RegisterLink>
+      </div>
+    </div>
+  );
+}
