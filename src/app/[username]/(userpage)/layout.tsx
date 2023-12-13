2a7dc9b24d37ecdf438074489bb9df8eb2f97787
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-13 23:31:59 +0100

    MODIFY: now reflects users username inside dynamic route

--- src/app/[username]/(userpage)/layout.tsx
+++ src/app/[username]/(userpage)/layout.tsx
@@ -2,12 +2,14 @@ import { UserPageHeader } from "@/components/user/UserPageHeader";
 
 export default function UserPageLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { username: string };
 }) {
   return (
     <main className="flex min-h-screen flex-col">
-      <UserPageHeader />
+      <UserPageHeader username={params.username} />
       {children}
     </main>
   );
