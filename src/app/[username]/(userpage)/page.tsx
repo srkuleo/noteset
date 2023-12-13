a8fd897a8e384537064cfa6d36a77d58f94a7815
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-13 23:33:05 +0100

    MODIFY: reflect username inside dynamic route, removes home param

---
+++ src/app/[username]/(userpage)/page.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { getUserWorkouts } from "@/db/query";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
+import { EmptyPage } from "@/components/user/EmptyPage";
+import { Workouts } from "@/components/user/Workouts";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default async function UserHomePage({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.given_name?.toLowerCase() !== params.username) notFound();
+
+  const workouts = await getUserWorkouts(user.id);
+
+  if (workouts.length < 1) {
+    return <EmptyPage username={params.username} />;
+  }
+
+  return <Workouts username={params.username} workouts={workouts} />;
+}
