06b329ef28110fe88f8f17449a4d680b2017c756
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 00:39:29 +0100

    MODIFY: metadata with minimal fields, everything else from root layer

--- src/app/landing/layout.tsx
+++ src/app/landing/layout.tsx
@@ -1,63 +1,9 @@
 import { LandingPageHeader } from "@/components/navbars/LandingPageHeader";
-import type { Viewport, Metadata } from "next";
-
-export const viewport: Viewport = {
-  userScalable: false,
-  initialScale: 1,
-  viewportFit: "cover",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#cbd5e1" },
-    { media: "(prefers-color-scheme: dark)", color: "#020617" },
-  ],
-};
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Landing",
-  description:
-    "Landing page for a minimalistic workout tracking PWA",
-  metadataBase: new URL("https://noteset.vercel.app/"),
-  openGraph: {
-    title: "Noteset - workout app",
-    description: "Landing page for a minimalistic workout tracking PWA",
-    url: "https://noteset.vercel.app/",
-    siteName: "Noteset",
-    images: [
-      {
-        url: "/noteset-og-image.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  manifest: "/site.webmanifest",
-  appleWebApp: {
-    statusBarStyle: "black-translucent",
-    capable: true,
-    startupImage: [
-      {
-        url: "/apple-splash-1170-2532.jpeg",
-        media:
-          "(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/apple-splash-dark-1170-2532.jpg",
-        media:
-          "(prefers-color-scheme: dark) and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/apple-splash-1290-2796.jpeg",
-        media:
-          "(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/apple-splash-dark-1290-2796.jpg",
-        media:
-          "(prefers-color-scheme: dark) and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-    ],
-  },
+  description: "Landing page for a minimalistic workout tracking PWA",
 };
 
 export default function LandingPageLayout({
