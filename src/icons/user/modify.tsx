0bc09be6d6a5ebcb71eb2ad7de83eca39e624dd3
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 19:52:12 +0100

    MODIFY:add new edit icon for edit workout button

--- src/icons/user/modify.tsx
+++ src/icons/user/modify.tsx
@@ -20,10 +20,9 @@ export const EditIcon = (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.2}
+    strokeWidth={1.5}
     stroke="currentColor"
-    width={16}
-    height={16}
+    className="h-5 w-5"
   >
     <path
       strokeLinecap="round"
@@ -33,14 +32,32 @@ export const EditIcon = (
   </svg>
 );
 
-export const DeleteIcon = (
+export const EditWorkoutIcon = (
   <svg
-    width={16}
-    height={16}
+    width={18}
+    height={18}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.2}
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+    />
+  </svg>
+);
+
+export const DeleteIcon = (
+  <svg
+    width={18}
+    height={18}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
     stroke="currentColor"
   >
     <path
