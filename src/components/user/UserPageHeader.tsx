dd7361ebd55e7e96ef4df5501e87e438f0bcf3b5
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 21:57:47 +0100

    MODIFY: reverting changes regarding padding top on UserPageHeader

--- src/components/user/UserPageHeader.tsx
+++ src/components/user/UserPageHeader.tsx
@@ -7,7 +7,7 @@ import { LogoutButton } from "./LogoutButton";
 export const UserPageHeader = ({ username }: { username: string }) => {
   return (
     <div className="fixed w-full border-b border-slate-300 bg-slate-300/50 pt-safe-top backdrop-blur-xl dark:border-slate-950 dark:bg-slate-950/50">
-      <div className="space-y-4 px-4 pb-2 pt-1">
+      <div className="space-y-4 px-4 pb-2 pt-2">
         <div className="flex justify-between rounded-[28px] bg-white px-4 py-2 shadow-md dark:bg-slate-800">
           <Logo />
           <div className="flex items-center gap-4">
