ee227b514612849f4fe52f37c69d9b4e66db3dd0
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 19:53:32 +0100

    MODIFY: logo doesn't lead to landing page anymore, since login and register page are done by kinde

--- src/components/navbars/LandingPageHeader.tsx
+++ src/components/navbars/LandingPageHeader.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { Logo } from "@/icons/navbars/logo";
 import { ThemeButton } from "./ThemeButton";
 import { GitHubButton } from "./GitHubButton";
@@ -7,20 +6,12 @@ export const LandingPageHeader = () => {
   return (
     <div className="fixed w-full border-b border-slate-400/50 bg-slate-300/60 px-4 pt-safe-top backdrop-blur-xl dark:border-slate-800/70 dark:bg-slate-950/60">
       <div className="flex justify-between py-2">
-        <LandingPageButton />
+        <Logo />
         <div className="flex items-center gap-4">
           <ThemeButton />
           <GitHubButton />
         </div>
       </div>
     </div>
-  );
-};
-
-const LandingPageButton = () => {
-  return (
-    <Link href="/">
-      <Logo />
-    </Link>
   );
 };
