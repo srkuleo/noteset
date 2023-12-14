d7faa32ed9bc74490865f376c992f930580c9e79
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 16:15:13 +0100

    MODIFY: removes shared parent div, shared gap class

--- src/components/user/Workouts.tsx
+++ src/components/user/Workouts.tsx
@@ -16,8 +16,8 @@ export const Workouts = async ({
   workouts: Workout[];
 }) => {
   return (
-    <div className="flex grow flex-col gap-2 px-6 pb-8 pt-52">
-      <div className="flex items-center justify-between">
+    <>
+      <div className="flex items-center justify-between pb-2">
         <h1 className="text-xl font-bold">Your current workouts</h1>
         <Link
           className="rounded-lg bg-white p-2 shadow-sm active:scale-95 dark:bg-slate-800"
@@ -29,7 +29,7 @@ export const Workouts = async ({
       {workouts.map((workout) => (
         <div
           key={workout.id}
-          className="flex w-full flex-col gap-4 rounded-xl bg-white/90 p-3 shadow-sm dark:bg-slate-800/90"
+          className="mb-4 flex w-full flex-col gap-4 rounded-xl bg-white/90 p-3 shadow-sm dark:bg-slate-800/90"
         >
           <div className="space-y-1 border-b border-slate-200 pb-4 pl-1 dark:border-slate-700">
             <p className="font-bold dark:text-slate-300">{workout.title}</p>
@@ -60,6 +60,6 @@ export const Workouts = async ({
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 };
