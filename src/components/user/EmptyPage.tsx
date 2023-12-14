34c12ad2d63c5adaf5e784997b73067d7ea4ea79
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-14 19:53:49 +0100

    MODIFY: small UI changes

--- src/components/user/EmptyPage.tsx
+++ src/components/user/EmptyPage.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const EmptyPage = ({ username }: { username: string }) => {
   return (
-    <div className="flex grow flex-col items-center justify-center gap-8 px-4 pb-8">
+    <div className="flex flex-col justify-center grow items-center gap-8 px-4">
       <div className=" text-slate-400/60 dark:text-slate-700/80">
         {EmptyIcon}
       </div>
@@ -12,14 +12,14 @@ export const EmptyPage = ({ username }: { username: string }) => {
           Seems like you don&apos;t have any workouts available
         </h2>
         <p className="text-sm text-slate-400 dark:text-slate-500">
-          Go to create page and start adding new ones
+          Go to create page and create your first workout
         </p>
       </div>
       <Link
         href={`/${username}/create`}
-        className="rounded-xl bg-violet-500 px-3 py-2 font-semibold text-white shadow-sm dark:bg-violet-600"
+        className="rounded-xl bg-violet-500 px-3 py-2 font-semibold text-white shadow-sm  dark:bg-violet-600"
       >
-        Create page
+        Create workout
       </Link>
     </div>
   );
