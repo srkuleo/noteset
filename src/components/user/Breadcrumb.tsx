d4dab23fea92bdfa1b43ebd13bf1c42db97887ee
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-25 23:48:33 +0100

    MODIFY: small UI changes, bigger font, color changes

--- src/components/user/Breadcrumb.tsx
+++ src/components/user/Breadcrumb.tsx
@@ -8,7 +8,7 @@ export type Breadcrumb = {
 
 export const Breadcrumbs = ({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) => {
   return (
-    <ul className="flex items-end">
+    <ul className="flex pb-2">
       {breadcrumbs.map((bc, index) => (
         <li key={bc.href} aria-current={bc.active}>
           <Link
@@ -16,13 +16,13 @@ export const Breadcrumbs = ({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) => {
             className={`${
               bc.active
                 ? "font-semibold uppercase"
-                : "text-lg text-slate-400/80 dark:text-slate-600"
-            }`}
+                : "text-slate-400 dark:text-slate-600"
+            } text-xl`}
           >
             {bc.label}
           </Link>
           {index < breadcrumbs.length - 1 && (
-            <span className="px-4 text-xl text-slate-400/80 dark:text-slate-600">
+            <span className="px-4 text-xl text-slate-400 dark:text-slate-600">
               /
             </span>
           )}
