485abd0f29353defe52fdc7381c01f5b9a61f5c4
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-27 18:23:29 +0100

    MODIFY: new protected routes, no more dynamic username based routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,15 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextResponse, type NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  if (
-    req.url === "http://localhost:3000/" ||
-    req.url === "https://noteset.vercel.app/"
-  ) {
-    return NextResponse.redirect(new URL("/landing", req.url));
-  } else {
-    return withAuth(req);
-  }
+  return withAuth(req);
 }
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|site|shortcut-icon|landing|noteset-og-image|apple|manifest).*)",
+    "/workouts/:path*",
+    "/logs/:paths*",
+    "/profile",
+    "/workout/:paths*",
   ],
 };
