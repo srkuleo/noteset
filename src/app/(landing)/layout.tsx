3a806edfbe14b2860e5c11ef97eb63153a43941e
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-27 18:26:17 +0100

    MODIFY: moves to "/" route instead of "/landing"

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+import { LandingPageHeader } from "@/components/navbars/LandingPageHeader";
+
+export default function LandingPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <LandingPageHeader />
+      {children}
+    </main>
+  );
+}
