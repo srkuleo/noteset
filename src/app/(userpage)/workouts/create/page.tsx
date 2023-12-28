46cc5296cf93e43333f9fc15d711909da3188465
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-28 01:34:50 +0100

    MODIFY: enables adding workouts to db, first steps - WIP

--- src/app/(userpage)/workouts/create/page.tsx
+++ src/app/(userpage)/workouts/create/page.tsx
@@ -1,8 +1,17 @@
 import { type Breadcrumb, Breadcrumbs } from "@/components/user/Breadcrumb";
 import { manrope } from "@/styles/fonts";
+import { createWorkout } from "@/util/actions";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-export default function CreateWorkoutPage() {
+export default async function CreateWorkoutPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const userId = user?.id as string;
+
+  const createWorkoutWithId = createWorkout.bind(null, userId);
+
   const breadcrumbs: Breadcrumb[] = [
     {
       label: "workouts",
@@ -22,7 +31,7 @@ export default function CreateWorkoutPage() {
         <p className={`pb-6 text-lg font-semibold ${manrope.className}`}>
           Create a new workout
         </p>
-        <form action="" className="space-y-4">
+        <form action={createWorkoutWithId} className="space-y-4">
           <label className="flex flex-col gap-1">
             <span className="pl-1 text-sm font-semibold uppercase text-slate-400 dark:text-slate-300">
               Title
@@ -45,7 +54,7 @@ export default function CreateWorkoutPage() {
               className="input-field"
             />
           </label>
-          <div className="flex justify-end gap-2 pt-4 items-center">
+          <div className="flex items-center justify-end gap-2 pt-4">
             <Link
               href="/workouts"
               className="rounded-lg px-3 py-2 text-sm font-semibold text-slate-500 active:scale-95 active:bg-slate-100 dark:text-slate-200 active:dark:bg-slate-900/60 "
