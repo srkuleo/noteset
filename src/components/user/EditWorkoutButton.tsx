5206d76b56bb78f6b810a79786061d7fbf4bd62f
Srdjan <milicevic.srdjan0@gmail.com>
2023-12-29 00:49:03 +0100

    ADD: button for editing selected workout, different UI smoother animation, minimalistc

---
+++ src/components/user/EditWorkoutButton.tsx
@@ -0,0 +1,39 @@
+import { EditWorkoutIcon } from "@/icons/user/modify";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import type { Workout } from "./Workouts";
+
+export const EditWorkoutButton = ({
+  isEditing,
+  workout,
+}: {
+  isEditing: boolean;
+  workout: Workout;
+}) => {
+  return (
+    <AnimatePresence>
+      {isEditing && (
+        <motion.div
+          initial={{ opacity: 0, y: -12 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.2, ease: "easeOut" },
+          }}
+          exit={{
+            opacity: 0,
+            y: -8,
+            transition: { duration: 0.1, ease: "easeIn" },
+          }}
+        >
+          <Link
+            href={`/workouts/edit?id=${workout.id}`}
+            className="text-green-500 transition active:scale-95"
+          >
+            {EditWorkoutIcon}
+          </Link>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
