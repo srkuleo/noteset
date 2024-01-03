d3f2ec7088abdb763c4fef7f4c7c96f22df37d77
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-03 18:21:08 +0100

    MODIFY: UI overhaul, made my own modal instead of using Vaul

--- src/components/user/RemoveWorkoutButton.tsx
+++ src/components/user/RemoveWorkoutButton.tsx
@@ -1,21 +1,35 @@
-import { RemoveWorkoutIcon } from "@/icons/user/modify";
+"use client";
+
+import { useEffect, useState } from "react";
 import type { Workout } from "./Workouts";
-import { Drawer } from "vaul";
-import { AnimatePresence, motion } from "framer-motion";
 import { removeWorkout } from "@/util/actions";
+import { DangerIcon } from "@/icons/user/danger";
+import { manrope } from "@/styles/fonts";
+import { AnimatePresence, motion } from "framer-motion";
+import { RemoveWorkoutIcon } from "@/icons/user/modify";
 
 export const RemoveWorkoutButton = ({
-  isEditing,
   workout,
+  isEditing,
 }: {
   isEditing: boolean;
   workout: Workout;
 }) => {
+  const [openRemoveModal, setOpenRemoveModal] = useState(false);
+
+  useEffect(() => {
+    if (openRemoveModal) {
+      document.body.classList.add("overflow-y-hidden");
+    } else {
+      document.body.classList.remove("overflow-y-hidden");
+    }
+  }, [openRemoveModal]);
+
   return (
-    <Drawer.Root>
+    <>
       <AnimatePresence>
         {isEditing && (
-          <motion.div className="flex"
+          <motion.div
             initial={{ opacity: 0, y: -12 }}
             animate={{
               opacity: 1,
@@ -24,63 +38,52 @@ export const RemoveWorkoutButton = ({
             }}
             exit={{
               opacity: 0,
-              y: -8,
-              transition: { duration: 0.1, ease: "easeIn" },
+              y: -12,
+              transition: { duration: 0.14, ease: "easeIn" },
             }}
           >
-            <Drawer.Trigger className="text-red-500 transition  active:scale-95 dark:text-red-400">
+            <button
+              className="text-red-500 transition  active:scale-95 dark:text-red-400"
+              onClick={() => setOpenRemoveModal(true)}
+            >
               {RemoveWorkoutIcon}
-            </Drawer.Trigger>
+            </button>
           </motion.div>
         )}
       </AnimatePresence>
-      <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-slate-950/70 dark:bg-slate-900/70" />
-        <Drawer.Content className="fixed inset-x-0 bottom-0 flex flex-col items-center rounded-t-[10px] bg-gradient-to-t from-slate-300 via-slate-100 to-slate-300 pt-2 dark:from-slate-950 dark:via-slate-900 dark:to-slate-950">
-          <div className="h-1 w-12 rounded-full bg-slate-400/50 dark:bg-slate-700" />
-          <div className="flex w-full items-center justify-between px-4 py-4">
-            <h1 className="font-semibold">Remove {workout.title}</h1>
-            <Drawer.Close className="text-sm text-slate-600 dark:text-slate-300">
-              Cancel
-            </Drawer.Close>
-          </div>
-          <div className="px-8 pb-16 pt-4">
-            <div className="flex flex-col items-center rounded-[10px] px-4">
-              <div className="rounded-full bg-red-50 p-2 text-red-600 shadow-md ring-1 ring-red-300 dark:bg-red-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-                  />
-                </svg>
-              </div>
-              <div className="space-y-2 pb-8 pt-4 text-center">
-                <p className="text-lg font-semibold">
-                  Do you want to continue?
-                </p>
-                <p className="text-sm text-slate-600 dark:text-slate-300">
-                  This action is irreversible and will result in permanent data
-                  loss.
+
+      {openRemoveModal && (
+        <>
+          <div
+            className="fixed inset-0 bg-slate-900/40 dark:bg-slate-900/60"
+            onClick={() => setOpenRemoveModal(false)}
+          />
+          <div className="fixed inset-x-0 bottom-4 space-y-4 px-4 py-4">
+            <div className="flex flex-col items-center gap-3 rounded-lg bg-white/90 pt-3 backdrop-blur-md dark:bg-slate-700/90">
+              <DangerIcon />
+
+              <div className="px-1">
+                <p className="text-center text-sm font-semibold leading-snug text-slate-600 dark:text-slate-400">
+                  This action is irreversible. Proceeding further will result in
+                  permanent data loss. Continue?
                 </p>
               </div>
-              <Drawer.Close
+              <button
                 onClick={() => removeWorkout(workout.id)}
-                className="rounded-lg bg-red-500 px-4 py-1.5 font-semibold text-white shadow-md dark:bg-red-600"
+                className={`w-full rounded-b-lg border-t border-slate-400/40 p-3 text-lg font-semibold text-red-500 active:bg-slate-200 dark:border-slate-600 active:dark:bg-slate-600/90 ${manrope.className}`}
               >
-                Remove
-              </Drawer.Close>
+                Remove {workout.title}
+              </button>
             </div>
+            <button
+              onClick={() => setOpenRemoveModal(false)}
+              className="w-full rounded-lg bg-white p-3 text-lg font-bold text-violet-500 active:bg-slate-200 dark:bg-slate-700/95 dark:text-violet-400 active:dark:bg-slate-600/90"
+            >
+              Cancel
+            </button>
           </div>
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+        </>
+      )}
+    </>
   );
 };
