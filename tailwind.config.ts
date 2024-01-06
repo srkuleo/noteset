e8151cc93d9afd93c0f5f1cd36cf9606da1e8039
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-06 23:43:54 +0100

    MODIFY: add custom backdropBlur xs and animations for radix dialog

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config: Config = {
   prefix: "",
   theme: {
     extend: {
+      backdropBlur: {
+        xs: "2px",
+      },
       spacing: {
         "safe-top": "env(safe-area-inset-top)",
         "safe-bottom": "env(safe-area-inset-bottom)",
@@ -17,6 +20,17 @@ const config: Config = {
         "safe-right": "env(safe-area-inset-right)",
         18: "4.5rem",
         38: "9.5rem",
+      },
+      animation: {
+        "overlay-show": "overlay-show 400ms cubic-bezier(0.36, 0.66, 0.04, 1)",
+
+        "overlay-hide":
+          "overlay-hide 300ms cubic-bezier(0.36, 0.66, 0.04, 1) 100ms",
+
+        "content-show": "content-show 400ms cubic-bezier(0.36, 0.66, 0.04, 1)",
+
+        "content-hide":
+          "content-hide 300ms cubic-bezier(0.36, 0.66, 0.04, 1) 100ms",
       },
     },
   },
