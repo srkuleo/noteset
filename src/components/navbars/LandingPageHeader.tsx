ff2f4735ef4ab7394746ee3ba6d26457fc313d3f
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-06 15:53:28 +0100

    MODIFY: return back pt-safe-top to account for notch space

--- src/components/navbars/LandingPageHeader.tsx
+++ src/components/navbars/LandingPageHeader.tsx
@@ -4,7 +4,7 @@ import { GitHubButton } from "./GitHubButton";
 
 export const LandingPageHeader = () => {
   return (
-    <div className="border-b border-slate-400/50 px-4  dark:border-slate-800/70 ">
+    <div className="border-b border-slate-400/50 px-4 pt-safe-top dark:border-slate-800/70">
       <div className="flex justify-between py-2">
         <Logo />
         <div className="flex items-center gap-4">
