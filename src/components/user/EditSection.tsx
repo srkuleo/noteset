7552e1ccda2d7e6f45a21285ae5ad463a3b7723c
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-07 00:30:26 +0100

    ADD: full abstraction for edit section, easier to navigate through workout component

---
+++ src/components/user/EditSection.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { manrope } from "@/styles/fonts";
+import { AddWorkoutIcon, EditIcon } from "@/icons/user/modify";
+
+export const EditSection = ({
+  toggleEditMode,
+}: {
+  toggleEditMode: () => void;
+}) => {
+  return (
+    <div className="flex items-center justify-between pb-4">
+      <h1
+        className={`text-xl font-bold text-slate-600 dark:text-white ${manrope.className}`}
+      >
+        Your current workouts
+      </h1>
+      <div className="flex gap-2">
+        <Link
+          href="/workouts/create"
+          className="rounded-xl bg-white p-2 shadow-sm transition active:scale-95 dark:bg-slate-800 dark:ring-1 dark:ring-slate-700"
+        >
+          {AddWorkoutIcon}
+        </Link>
+        <button
+          onClick={toggleEditMode}
+          className="rounded-xl bg-green-500 p-2 text-white shadow-sm transition active:scale-95 dark:bg-green-600"
+        >
+          {EditIcon}
+        </button>
+      </div>
+    </div>
+  );
+};
