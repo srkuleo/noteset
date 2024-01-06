c48d7d12ed367ec5cfd135ce7595aad70ffec441
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-06 15:34:00 +0100

    MODIFY: rearranged for better clarity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
     extend: {
       spacing: {
@@ -19,6 +21,6 @@ const config: Config = {
     },
   },
   plugins: [],
-  darkMode: "class",
 };
+
 export default config;
