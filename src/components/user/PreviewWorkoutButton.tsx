adfa08e52ae3b860f50b64dc6bd2db229b9b0cba
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-07 00:55:36 +0100

    MODIFY: visual improvements on close button, add paddint top safe to account for notch on iphones

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -12,12 +12,12 @@ export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-xs data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
 
-        <Dialog.Content className="fixed inset-0 mt-32 rounded-t-modal bg-white p-4 data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show dark:bg-slate-800">
-          <div className="flex items-center justify-between">
+        <Dialog.Content className="fixed inset-0 mt-36 rounded-t-modal bg-white pt-safe-top  data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show dark:bg-slate-800">
+          <div className="flex items-center justify-between p-4">
             <p className="text-lg font-bold">
               Current workout: {workout.title}
             </p>
-            <Dialog.Close className="font-extrabold text-violet-500 focus:outline-none dark:text-violet-400">
+            <Dialog.Close className="font-extrabold text-violet-500 focus:outline-none active:text-violet-300 dark:text-violet-400 active:dark:text-violet-600">
               Done
             </Dialog.Close>
           </div>
