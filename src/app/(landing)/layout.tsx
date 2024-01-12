7c738f54ec145163418a3aa51e2ba1545c520394
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-12 23:42:46 +0100

    MODIFY: disables scrollability of main

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -7,14 +7,16 @@ export default function LandingPageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex min-h-screen touch-none flex-col">
       <LandingPageHeader />
       <div className="hidden pt-2 md:block">
         <p
           className={`${manrope.className} bg-red-500 py-2 text-center text-lg text-white`}
         >
-          <span className="font-bold underline underline-offset-4">Important</span>
-           : UI is not suitable for large screens. Visit on mobile.
+          <span className="font-bold underline underline-offset-4">
+            Important
+          </span>
+          : UI is not suitable for large screens. Visit on mobile.
         </p>
       </div>
       {children}
