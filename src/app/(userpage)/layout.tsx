1d2b9694cd68af711671b19c536be07732c1afcc
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-13 00:48:47 +0100

    MODIFY: removes vaul's data attribute

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -6,7 +6,7 @@ export default function UserPagesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen flex-col" vaul-drawer-wrapper="">
+    <main className="flex min-h-screen flex-col">
       <UserPageHeader />
       <div className="mt-40 flex flex-col px-6 pb-8 pt-safe-top">
         {children}
