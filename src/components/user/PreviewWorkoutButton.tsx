e0e1fe7184177606b4c32da8cf5d1713b22bdc23
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-13 00:47:52 +0100

    MODIFY: final skeleton version with added drag motion

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -1,28 +1,27 @@
 import type { Workout } from "./Workouts";
+import { Drawer } from "vaul";
 import { PreviewIcon } from "@/icons/user/preview";
-import * as Dialog from "@radix-ui/react-dialog";
 
 export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger className="select-none rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
+    <Drawer.Root>
+      <Drawer.Trigger className="select-none rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
         {PreviewIcon}
-      </Dialog.Trigger>
+      </Drawer.Trigger>
 
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-xs data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-slate-900/40 dark:bg-slate-950/70" />
 
-        <Dialog.Content className="fixed inset-0 mt-32 rounded-t-modal bg-white data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show dark:bg-slate-800">
-          <div className="flex items-center justify-between p-4">
-            <p className="select-none text-lg font-bold">
-              Current workout: {workout.title}
-            </p>
-            <Dialog.Close className="select-none font-extrabold text-violet-500 focus:outline-none active:text-violet-300 dark:text-violet-400 active:dark:text-violet-600">
+        <Drawer.Content className="fixed inset-0 mt-32 select-none rounded-t-modal bg-white dark:bg-slate-800">
+          <div className="w-12 h-1.5 bg-slate-300 mx-auto mt-1.5 rounded-full dark:bg-slate-600" />
+          <div className="flex items-center justify-center px-2 pb-4 pt-6 bg-slate-100/65 dark:bg-slate-700/10 border-b border-slate-400 dark:border-slate-600 rounded-t-modal">
+            <p className="font-bold">Preview: {workout.title}</p>
+            <Drawer.Close className="absolute right-4 text-lg font-extrabold text-violet-500 focus:outline-none active:text-violet-300 dark:text-violet-400 active:dark:text-violet-600">
               Done
-            </Dialog.Close>
+            </Drawer.Close>
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
