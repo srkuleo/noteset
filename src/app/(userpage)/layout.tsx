559a0738d669cb2a7e72a0090060aa27d33cd61a
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-13 01:03:10 +0100

    MODIFY: small UI changes regarding colors

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -6,7 +6,7 @@ export default function UserPagesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex min-h-screen select-none flex-col">
       <UserPageHeader />
       <div className="mt-40 flex flex-col px-6 pb-8 pt-safe-top">
         {children}
