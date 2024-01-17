2ee30bcb93c5d5b14a62df783de27fd09db6dcf4
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-17 22:18:11 +0100

    MODIFY: adds server action for editting an existing workout in db

--- src/util/actions.ts
+++ src/util/actions.ts
@@ -37,10 +37,10 @@ export async function removeWorkout(workoutId: number) {
 
 const CreateWorkout = WorkoutSchema.omit({
   id: true,
+  status: true,
   userId: true,
   doneAt: true,
   timeElapsed: true,
-  status: true,
 });
 
 export async function createWorkout(userId: string, formData: FormData) {
@@ -56,7 +56,7 @@ export async function createWorkout(userId: string, formData: FormData) {
 
     if (existingWorkout) {
       throw new WorkoutAlreadyExistsError(
-        `Workout with ${title} tile already exists.`,
+        `Workout with ${title} title already exists.`,
       );
     }
 
@@ -75,4 +75,45 @@ export async function createWorkout(userId: string, formData: FormData) {
 
   revalidatePath("/workouts");
   redirect("/workouts?action=created");
+}
+
+interface Ids {
+  userId: string;
+  workoutId: number;
+}
+
+export async function editWorkout(
+  { userId, workoutId }: Ids,
+  formData: FormData,
+) {
+  const { title, description } = CreateWorkout.parse({
+    title: formData.get("title"),
+    description: formData.get("description"),
+  });
+
+  try {
+    const matchingTitleWorkout = await db.query.workouts.findFirst({
+      where: and(eq(workouts.title, title), eq(workouts.userId, userId)),
+    });
+
+    if (matchingTitleWorkout && matchingTitleWorkout.id !== workoutId) {
+      throw new WorkoutAlreadyExistsError(
+        `Workout with ${title} title already exists.`,
+      );
+    }
+
+    await db
+      .update(workouts)
+      .set({ title: title, description: description })
+      .where(eq(workouts.id, workoutId));
+  } catch (error) {
+    if (error instanceof WorkoutAlreadyExistsError) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("Database Error: Workout could not be edited.");
+    }
+  }
+
+  revalidatePath("/workouts");
+  redirect("/workouts?action=edited");
 }
