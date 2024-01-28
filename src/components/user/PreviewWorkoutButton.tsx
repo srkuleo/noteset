2121d4a9a75a56e399c0fabce31d1f643bd75b49
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-28 03:54:24 +0100

    MODIFY: QoL changes, heading now in different font

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -1,6 +1,7 @@
 import type { Workout } from "./Workouts";
 import { Drawer } from "vaul";
 import { PreviewIcon } from "@/icons/user/preview";
+import { manrope } from "@/styles/fonts";
 
 export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
   return (
@@ -16,7 +17,9 @@ export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
           <div className="rounded-t-modal border-b border-slate-200 bg-slate-100/65 px-2 pb-4 pt-2 dark:border-slate-600 dark:bg-slate-700/20">
             <div className="mx-auto mb-6 h-1.5 w-12 rounded-full bg-slate-200 dark:bg-slate-600" />
             <div className="flex items-center justify-center">
-              <p className="font-bold">Preview: {workout.title}</p>
+              <p className={`font-bold ${manrope.className}`}>
+                Preview: {workout.title}
+              </p>
               <Drawer.Close className="absolute right-4 text-lg font-extrabold text-violet-500 focus:outline-none active:text-violet-300 dark:text-violet-400 active:dark:text-violet-600">
                 Done
               </Drawer.Close>
