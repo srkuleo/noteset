2d453cf5f7baae998e7ed894a546cfd3df3503c0
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-28 03:57:52 +0100

    MODIFY: AddWorkoutIcon is now AddIcon (used by multiple buttons) and RFC instead of JSX svg, accepting 3 props - width, height and strokeWidth

--- src/icons/user/modify.tsx
+++ src/icons/user/modify.tsx
@@ -1,28 +1,40 @@
-export const AddWorkoutIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.7}
-    stroke="currentColor"
-    className="h-5 w-5"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 4.5v15m7.5-7.5h-15"
-    />
-  </svg>
-);
+export const AddIcon = ({
+  width,
+  height,
+  strokeWidth,
+}: {
+  width: number;
+  height: number;
+  strokeWidth: number;
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+};
 
 export const EditIcon = (
   <svg
+    height={18}
+    width={18}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="h-5 w-5"
   >
     <path
       strokeLinecap="round"
