f7106f17721b56cecc6a74175f5740abacb601a9
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-31 14:55:02 +0100

    ADD: leftovers from previous EditSection component, heading text with button to add a new workout, now renders outside of WorkoutCards component since it can be statically generated at build time

---
+++ src/components/user/WorkoutCardsHeader.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { manrope } from "@/styles/fonts";
+import { AddIcon } from "@/icons/user/modify";
+
+export const WorkoutCardsHeader = () => {
+  return (
+    <div className="flex select-none items-center justify-between pb-4">
+      <h1
+        className={`text-2xl font-extrabold text-slate-600 dark:text-white ${manrope.className}`}
+      >
+        Workouts
+      </h1>
+      <div className="flex items-center gap-3">
+        <Link
+          href="/workouts/create"
+          className="rounded-full bg-white p-2 shadow-sm transition active:scale-95 dark:bg-slate-800 dark:ring-1 dark:ring-slate-700"
+        >
+          <AddIcon height={22} width={22} strokeWidth={2} />
+        </Link>
+      </div>
+    </div>
+  );
+};
