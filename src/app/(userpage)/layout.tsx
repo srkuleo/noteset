0f3888457d6fd1018c521ef67b0f0aa87d97c875
Srdjan <milicevic.srdjan0@gmail.com>
2024-01-31 17:53:05 +0100

    MODIFY: removes flex and flex-col unnecessary classes

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -8,7 +8,7 @@ export default function UserPagesLayout({
   return (
     <main className="flex min-h-screen select-none flex-col">
       <UserPageHeader />
-      <div className="mt-40 flex flex-col px-6 pb-8 pt-safe-top">
+      <div className="mt-40 px-6 pb-8 pt-safe-top">
         {children}
       </div>
     </main>
