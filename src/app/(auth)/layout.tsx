71a23f2ab0ae60971214f7b4f90d4a10cf14cd44
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-07 23:53:58 +0100

    MODIFY: renames the group folder, add auth to the main page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import { LandingPageHeader } from "@/components/navbars/LandingPageHeader";
+import { manrope } from "@/styles/fonts";
+
+export default function LandingPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex min-h-screen touch-none select-none flex-col">
+      <LandingPageHeader />
+      <UiMessage />
+      {children}
+    </main>
+  );
+}
+
+const UiMessage = () => {
+  return (
+    <div className="hidden pt-2 md:block">
+      <p
+        className={`${manrope.className} bg-red-500 py-2 text-center text-lg text-white`}
+      >
+        <span className="font-bold underline underline-offset-4">
+          Important
+        </span>
+        : UI is not suitable for large screens. Visit on mobile.
+      </p>
+    </div>
+  );
+};
---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,34 @@
+import { LoginForm } from "@/components/login/LoginForm";
+import { manrope } from "@/styles/fonts";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
+
+  isLoggedIn && redirect("/workouts");
+
+  return (
+    <div className="flex grow flex-col px-8 pb-8 pt-16">
+      <HeroText />
+      <LoginForm />
+    </div>
+  );
+}
+
+const HeroText = () => {
+  return (
+    <div className="space-y-5 pb-20 text-center">
+      <h1 className="mx-auto w-fit bg-gradient-to-r from-green-500 to-violet-500 bg-clip-text text-6xl font-extrabold text-transparent dark:to-violet-600">
+        Note<span className="font-bold">Set</span>
+      </h1>
+      <h2
+        className={`${manrope.className} font-semibold dark:text-slate-400 md:text-balance`}
+      >
+        Personalized workout tracking PWA, aimed to replace conventional use of
+        a notebook in a gym.
+      </h2>
+    </div>
+  );
+};
