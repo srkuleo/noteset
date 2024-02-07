90160bcbd835795c0eee5017bd893857beb406f1
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-07 23:55:14 +0100

    ADD: new component to separate OAuth from Email authentication

---
+++ src/components/login/Separator.tsx
@@ -0,0 +1,9 @@
+export const Separator = () => {
+  return (
+    <div className="flex items-center gap-2 px-2 py-5">
+      <div className="h-[1px] w-full bg-slate-300 dark:bg-slate-700" />
+      <p className="italic font-semibold leading-none text-slate-400 dark:text-slate-300">or</p>
+      <div className="h-[1px] w-full bg-slate-300 dark:bg-slate-700" />
+    </div>
+  );
+};
