cb97ba6b32bf465c96fcbfac1f4119443e868eb7
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-08 01:10:33 +0100

    MODIFY: logo now redirect to login page, useful when on register page

--- src/components/navbars/LandingPageHeader.tsx
+++ src/components/navbars/LandingPageHeader.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Logo } from "@/icons/navbars/logo";
 import { ThemeButton } from "./ThemeButton";
 import { GitHubButton } from "./GitHubButton";
@@ -6,7 +7,9 @@ export const LandingPageHeader = () => {
   return (
     <div className="border-b border-slate-400/50 px-4 pt-safe-top dark:border-slate-800/70">
       <div className="flex justify-between py-2">
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
         <div className="flex items-center gap-4">
           <ThemeButton />
           <GitHubButton />
