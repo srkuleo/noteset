8a70ee0eb7f62b9cfe023db2ff23540b86f32ec8
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-08 01:08:46 +0100

    MODIFY: remove h1 - now part of the layout. Add HeroSubtitle reusable component shared by both login and register page.

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,7 +1,7 @@
-import { LoginForm } from "@/components/login/LoginForm";
-import { manrope } from "@/styles/fonts";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { HeroSubtitle } from "@/components/login/HeroSubtitle";
+import { LoginForm } from "@/components/login/LoginForm";
 
 export default async function LoginPage() {
   const { isAuthenticated } = getKindeServerSession();
@@ -10,25 +10,9 @@ export default async function LoginPage() {
   isLoggedIn && redirect("/workouts");
 
   return (
-    <div className="flex grow flex-col px-8 pb-8 pt-16">
-      <HeroText />
+    <>
+      <HeroSubtitle content="Personalized workout tracking PWA, aimed to replace a notebook in the gym." />
       <LoginForm />
-    </div>
+    </>
   );
 }
-
-const HeroText = () => {
-  return (
-    <div className="space-y-5 pb-16 text-center">
-      <h1 className="mx-auto w-fit bg-gradient-to-r from-green-500 to-violet-500 bg-clip-text text-6xl font-extrabold text-transparent dark:to-violet-600">
-        Note<span className="font-bold">Set</span>
-      </h1>
-      <h2
-        className={`${manrope.className} font-semibold dark:text-slate-400 md:text-balance`}
-      >
-        Personalized workout tracking PWA, aimed to replace conventional use of
-        a notebook in a gym.
-      </h2>
-    </div>
-  );
-};
