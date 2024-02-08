07838d2600246ea4699dce48bcd412fddeeb70b4
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-08 01:10:02 +0100

    MODIFY: small UI modification

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,25 +1,17 @@
 "use client";
 
-import { GoogleIcon } from "@/icons/login/google";
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
 import { useState } from "react";
-import { Separator } from "./Separator";
 import Link from "next/link";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+import { GoogleOAuthButton } from "./GoogleOAuthBtn";
+import { Separator } from "./Separator";
 
 export const LoginForm = () => {
   const [userEmail, setUserEmail] = useState("");
 
   return (
     <div className="flex flex-col px-8">
-      <LoginLink
-        authUrlParams={{
-          connection_id: "conn_ac6e434edebf42099dd4371003bca270",
-        }}
-        className="flex items-center justify-center gap-2 rounded-xl bg-white py-2 shadow-md ring-1 ring-slate-300 transition active:scale-95 dark:bg-slate-900 dark:ring-slate-700"
-      >
-        {GoogleIcon}
-        <p className="font-semibold">Login via Google</p>
-      </LoginLink>
+      <GoogleOAuthButton />
 
       <Separator />
 
@@ -31,9 +23,9 @@ export const LoginForm = () => {
           placeholder="Email"
           className="login-input-field"
           required
+          autoComplete="email"
           onChange={(e) => setUserEmail(e.target.value)}
         />
-
         <LoginLink
           authUrlParams={{
             connection_id: "conn_8366bf58a5c5457dbfbd80a0e643e04f",
@@ -43,7 +35,7 @@ export const LoginForm = () => {
         >
           Login
         </LoginLink>
-        <p className="font-semibold dark:text-slate-400 leading-none">
+        <p className="font-semibold leading-none dark:text-slate-400">
           Not a user?
           <Link className="pl-1 font-bold text-green-500" href="/register">
             Register here.
