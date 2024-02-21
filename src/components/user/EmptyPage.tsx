24a85bf9c8c3555b79edeb326c8cc31cdec11bfd
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-21 23:33:30 +0100

    MODIFY: removes button for create page, shared add button between both empty and workoutcards pages

--- src/components/user/EmptyPage.tsx
+++ src/components/user/EmptyPage.tsx
@@ -1,26 +1,16 @@
 import { EmptyIcon } from "@/icons/user/warning";
-import Link from "next/link";
 
 export const EmptyPage = () => {
   return (
-    <div className="flex select-none flex-col items-center gap-8 px-4 pt-28">
+    <div className="flex select-none flex-col items-center gap-8 px-4 pt-40">
       <div className="text-slate-400/60 dark:text-slate-700/80">
         {EmptyIcon}
       </div>
-      <div className="space-y-2 text-center">
+      <div className="space-y-4 text-center">
         <h2 className="text-xl font-semibold dark:text-slate-300">
           Seems like you don&apos;t have any available workouts
         </h2>
-        <p className="text-sm text-slate-400 dark:text-slate-500">
-          Go to create page and create your first workout
-        </p>
       </div>
-      <Link
-        href="/workouts/create"
-        className="rounded-xl bg-violet-500 px-3 py-2 font-semibold text-white shadow-sm  dark:bg-violet-600"
-      >
-        Create workout
-      </Link>
     </div>
   );
 };
