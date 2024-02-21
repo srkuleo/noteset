d33b31b8cc3a923e205615a96b57f40c14fb479f
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-21 23:39:20 +0100

    REMOVES: now shows error while on create page

--- src/util/exceptions.ts
+++
@@ -1,6 +0,0 @@
-export class WorkoutAlreadyExistsError extends Error {
-  constructor(message = "Workout already exists.") {
-    super(message);
-    this.name = "WorkoutAlreadyExistsError ";
-  }
-}
