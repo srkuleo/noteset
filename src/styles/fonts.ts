1ebba205fea52b8e8ab7f0a1e098e0c516e62611
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-23 18:15:19 +0100

    MODIFY: variables now in use

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,11 +3,11 @@ import { Nunito, Manrope } from "next/font/google";
 export const nunito = Nunito({
   subsets: ["latin"],
   display: "swap",
-  // variable: "--font-nunito"
+  variable: "--font-nunito"
 });
 
 export const manrope = Manrope({
   subsets: ["latin"],
   display: "swap",
-  // variable: "--font-manrope"
+  variable: "--font-manrope"
 });
