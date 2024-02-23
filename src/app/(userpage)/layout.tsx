66a7b56fbe69f078c146eb3053f198d9d56c0cae
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-23 18:08:39 +0100

    MODIFY: just a different approach to vh

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -6,7 +6,7 @@ export default function UserPagesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen select-none flex-col">
+    <main className="flex min-h-lvh select-none flex-col">
       <UserPageHeader />
       <div className="mt-40 px-6 pb-8 pt-safe-top">{children}</div>
     </main>
