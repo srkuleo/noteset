a08bdca91b0de872ab1f51705a9faba1d39903d7
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-25 00:52:12 +0100

    MODIFY: changes to navbar - WIP

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -1,3 +1,5 @@
+import { LinkButtons } from "@/components/user/LinkButtons";
+import { LogoutButton } from "@/components/user/LogoutButton";
 import { UserPageHeader } from "@/components/user/UserPageHeader";
 
 export default function UserPagesLayout({
@@ -8,7 +10,13 @@ export default function UserPagesLayout({
   return (
     <main className="flex min-h-lvh select-none flex-col">
       <UserPageHeader />
-      <div className="mt-40 px-6 pb-8 pt-safe-top">{children}</div>
+      <div className="mb-20 px-6 pb-safe-bottom">{children}</div>
+      <div className="fixed inset-x-0 bottom-0 border-t border-slate-400/20 bg-slate-300/60 px-3 pb-safe-bottom pt-3 backdrop-blur-lg dark:border-slate-800/80 dark:bg-slate-950/60">
+        <div className="mb-3 flex gap-2">
+          <LinkButtons />
+          <LogoutButton />
+        </div>
+      </div>
     </main>
   );
 }
