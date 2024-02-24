6264fa235f25bc03f8c5922267842b93f8022cd8
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-25 00:53:16 +0100

    MODIFY: moves navbar to the bottom of the page, not fixed anymore

--- src/components/user/UserPageHeader.tsx
+++ src/components/user/UserPageHeader.tsx
@@ -1,24 +1,15 @@
 import { Logo } from "@/icons/navbars/logo";
 import { GitHubButton } from "../navbars/GitHubButton";
 import { ThemeButton } from "../navbars/ThemeButton";
-import { LinkButtons } from "./LinkButtons";
-import { LogoutButton } from "./LogoutButton";
 
 export const UserPageHeader = () => {
   return (
-    <div className="fixed w-full bg-slate-300/50 pt-safe-top backdrop-blur-lg dark:bg-slate-950/50">
-      <div className="space-y-4 px-4 pb-3 pt-2">
-        <div className="flex justify-between rounded-[28px] bg-white px-4 py-2 shadow-md dark:bg-slate-800">
-          <Logo />
-          <div className="flex items-center gap-4">
-            <ThemeButton />
-            <GitHubButton />
-          </div>
-        </div>
-
-        <div className="flex gap-2">
-          <LinkButtons />
-          <LogoutButton />
+    <div className="px-4 pb-4 pt-2">
+      <div className="flex justify-between rounded-[28px] bg-white px-4 py-2 shadow-md dark:bg-slate-800">
+        <Logo />
+        <div className="flex items-center gap-4">
+          <ThemeButton />
+          <GitHubButton />
         </div>
       </div>
     </div>
