5d7bf8dbc081008be199e5fb99b5005772416e4e
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-25 02:15:36 +0100

    MODIFY: removes select none, already on the parent

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -8,7 +8,7 @@ import { PreviewIcon } from "@/icons/user/preview";
 export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
   return (
     <Drawer.Root>
-      <Drawer.Trigger className="select-none rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
+      <Drawer.Trigger className="rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
         {PreviewIcon}
       </Drawer.Trigger>
 
