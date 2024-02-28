3957722a4577b9de62ad4f415662fe04061fc3bb
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 01:41:04 +0100

    MODIFY: new landing page without login options with added tooltip with info regarding better UX

---
+++ src/app/(landingpage)/layout.tsx
@@ -0,0 +1,30 @@
+import { LandingPageBar } from "@/components/landing/LandingPageBar";
+
+export default function LandingPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex min-h-screen select-none flex-col">
+      <LandingPageBar />
+      <UiMessage />
+      {children}
+    </main>
+  );
+}
+
+const UiMessage = () => {
+  return (
+    <>
+      <div className="hidden md:block">
+        <p className="bg-red-500 py-2 text-center font-manrope text-lg text-white">
+          <span className="font-bold underline underline-offset-4">
+            Important
+          </span>
+          : UI is not suitable for large screens. Visit on mobile.
+        </p>
+      </div>
+    </>
+  );
+};
---
+++ src/app/(landingpage)/page.tsx
@@ -0,0 +1,35 @@
+import { redirect } from "next/navigation";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TooltipDrawer } from "@/components/landing/TooltipDrawer";
+import { ArrowIcon } from "@/components/icons/landing/arrow";
+
+export default async function LoginPage() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
+
+  isLoggedIn && redirect("/workouts");
+
+  return (
+    <div className="mt-40 flex grow flex-col px-8 pb-8 pt-safe-top">
+      <h1 className="mx-auto w-fit bg-gradient-to-r from-green-400 to-violet-500 bg-clip-text pb-8 text-7xl font-extrabold text-transparent dark:to-violet-600">
+        Note<span className="font-bold">Set</span>
+      </h1>
+
+      <h2 className="pb-16 text-center font-semibold leading-snug dark:text-slate-400 md:text-balance">
+        Personalized workout tracking PWA, aimed to replace a notebook in the
+        gym.
+      </h2>
+
+      <div className="flex justify-center gap-4">
+        <TooltipDrawer  />
+
+        <div className="w-[1px] bg-slate-300/50 dark:bg-slate-700/80" />
+
+        <LoginLink className="flex items-center gap-1 rounded-full px-4 py-2 font-manrope text-slate-600 active:bg-slate-200 dark:text-slate-300 active:dark:bg-slate-800">
+          Login {ArrowIcon}
+        </LoginLink>
+      </div>
+    </div>
+  );
+}
