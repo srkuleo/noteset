f5bc681e402fe456eb0fb75b13bdbf2a1da60c7c
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 17:09:06 +0100

    MODIFY: now properly sets system as default on the first load

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -4,12 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      enableSystem
-      enableColorScheme
-      themes={["system", "dark", "light"]}
-    >
+    <ThemeProvider attribute="class" enableSystem={true} defaultTheme="system">
       {children}
     </ThemeProvider>
   );
