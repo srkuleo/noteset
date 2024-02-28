66a5e7cddf9caf6408d696329557b175e86d4864
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 05:55:49 +0100

    MODIFY: adds few additional theme provider props

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -3,5 +3,14 @@
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      enableSystem
+      enableColorScheme
+      themes={["system", "dark", "light"]}
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
