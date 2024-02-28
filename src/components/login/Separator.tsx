acece8742774b8540be677eef8d662212fafd01e
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 01:50:21 +0100

    REMOVE: not needed anymore, since login page is removed

--- src/components/login/Separator.tsx
+++
@@ -1,9 +0,0 @@
-export const Separator = () => {
-  return (
-    <div className="flex items-center gap-2 px-2 py-8">
-      <div className="h-[1px] w-full bg-slate-300 dark:bg-slate-700" />
-      <p className="italic font-semibold leading-none text-slate-400 dark:text-slate-300">or</p>
-      <div className="h-[1px] w-full bg-slate-300 dark:bg-slate-700" />
-    </div>
-  );
-};
