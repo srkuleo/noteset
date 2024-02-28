182f16272edf534f950949fe8bb257715322d871
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 16:58:35 +0100

    MODIFY: trying to figure out is it better to put lvh or svh on the landing page

--- src/app/(landingpage)/layout.tsx
+++ src/app/(landingpage)/layout.tsx
@@ -6,7 +6,7 @@ export default function LandingPageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-svh touch-none select-none flex-col pt-safe-top">
+    <main className="flex min-h-lvh touch-none select-none flex-col pt-safe-top">
       <LandingPageBar />
       <UiMessage />
       {children}
