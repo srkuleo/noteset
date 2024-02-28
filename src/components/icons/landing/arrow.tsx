9b3d4d1a45668b7761b7aac738b1be87b007b0d4
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 01:45:45 +0100

    ADD: new svg for login button

---
+++ src/components/icons/landing/arrow.tsx
@@ -0,0 +1,16 @@
+export const ArrowIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-4"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+    />
+  </svg>
+);
