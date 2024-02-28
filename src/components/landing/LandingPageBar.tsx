87cdbc34865e1c0b02b00e58cda1f40591931f53
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 05:42:19 +0100

    MODIFY: changes element to not be fixed, moves safe area inset top to the main element

--- src/components/landing/LandingPageBar.tsx
+++ src/components/landing/LandingPageBar.tsx
@@ -5,7 +5,7 @@ import { GitHubButton } from "./GitHubButton";
 
 export const LandingPageBar = () => {
   return (
-    <div className="border-b border-slate-300 px-4 pt-safe-top dark:border-slate-800/90">
+    <div className="border-b border-slate-300 px-4 dark:border-slate-800/90">
       <div className="flex justify-between py-2">
         <Link href="/">
           <Logo />
