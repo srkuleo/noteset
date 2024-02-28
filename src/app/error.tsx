ddb50cbd5d8e8609f8e948c1fcbf8866bab21ca2
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 01:38:58 +0100

    MODIFY: changes import, adds 2 utility classes to prevent overscrolling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { LandingPageHeader } from "@/components/navbars/LandingPageHeader";
-import { SadIcon } from "@/icons/user/warning";
 import Link from "next/link";
 import { useEffect } from "react";
+import { LandingPageBar } from "@/components/landing/LandingPageBar";
+import { SadIcon } from "@/components/icons/user/warning";
 
 export default function Error({
   error,
@@ -17,12 +17,13 @@ export default function Error({
   }, [error]);
 
   return (
-    <main className="flex min-h-screen flex-col">
-      <LandingPageHeader />
+    <main className="flex min-h-screen touch-none select-none flex-col">
+      <LandingPageBar />
       <div className="flex grow flex-col items-center justify-center gap-8 px-4 pb-8">
         <div className="text-slate-400/60 dark:text-slate-700/80">
           {SadIcon}
         </div>
+
         <div className="space-y-2 text-center">
           <h2 className="text-xl font-semibold dark:text-slate-300">
             Sorry, something went wrong!
@@ -31,6 +32,7 @@ export default function Error({
             {error.message}
           </p>
         </div>
+
         <div className="flex flex-col items-center gap-2">
           <Link
             href="/"
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,16 @@
-import { LandingPageHeader } from "@/components/navbars/LandingPageHeader";
-import { SadIcon } from "@/icons/user/warning";
 import Link from "next/link";
+import { LandingPageBar } from "@/components/landing/LandingPageBar";
+import { SadIcon } from "@/components/icons/user/warning";
 
 export default function NotFound() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <LandingPageHeader />
+    <main className="flex min-h-screen touch-none select-none flex-col">
+      <LandingPageBar />
       <div className="flex grow flex-col items-center justify-center gap-8 px-4 pb-8">
         <div className="text-slate-400/60 dark:text-slate-700/80">
           {SadIcon}
         </div>
+
         <div className="space-y-2 text-center">
           <h2 className="text-xl font-semibold dark:text-slate-300">
             404 Page could not be found
@@ -18,6 +19,7 @@ export default function NotFound() {
             Invalid URL
           </p>
         </div>
+
         <Link
           href="/"
           className="rounded-xl bg-violet-500 px-3 py-2 font-semibold text-white shadow-sm dark:bg-violet-600"
