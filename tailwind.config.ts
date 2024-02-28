213af047fe3b848470cb0c9ff731c5195091c2f4
Srdjan <milicevic.srdjan0@gmail.com>
2024-02-28 01:38:07 +0100

    MODIFY: removes pages route support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import plugin from "tailwindcss/plugin";
 const config: Config = {
   darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
