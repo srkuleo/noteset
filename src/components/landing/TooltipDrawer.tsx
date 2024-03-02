3f0ea03f4a3f18e2b489f33c680f54bc3b83d01e
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-02 03:59:36 +0100

    MODIFY: now uses basic radix dialog instead of vaul drawer

--- src/components/landing/TooltipDrawer.tsx
+++ src/components/landing/TooltipDrawer.tsx
@@ -1,48 +1,52 @@
 "use client";
 
-import { Drawer } from "vaul";
-import { DrawerWrapper } from "../user/DrawerWrapper";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export const TooltipDrawer = () => {
   return (
-    <Drawer.Root>
-      <Drawer.Trigger className="rounded-full px-6 py-2 font-manrope font-semibold text-slate-600 shadow-md ring-1 ring-slate-300 dark:text-white dark:ring-slate-400">
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-full px-6 py-2 font-manrope font-semibold text-slate-600 shadow-md ring-1 ring-slate-300 dark:text-white dark:ring-slate-400">
         Tooltip
-      </Drawer.Trigger>
+      </Dialog.Trigger>
 
-      <DrawerWrapper modalType="tooltip">
-        <div className="flex flex-col font-manrope">
-          <p className="pt-8 text-xl font-bold text-slate-800 dark:text-white">
-            For a better user experience
-          </p>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
 
-          <div className="space-y-2 py-8 pl-4">
-            <p className="text-sm font-bold">
-              <span className="text-xs font-semibold uppercase text-slate-400">
-                android:
-              </span>{" "}
-              install PWA
-            </p>
-            <p className="text-sm font-bold">
-              <span className=" text-xs font-semibold uppercase text-slate-400">
-                iphone:
-              </span>{" "}
-              add website to Home Screen
-            </p>
+        <Dialog.Content className="fixed inset-x-0 bottom-0 select-none px-4 pb-12 data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show">
+          <div className="rounded-modal bg-white ring-1 ring-inset ring-slate-200 dark:bg-slate-900 dark:ring-slate-800">
+            <Dialog.Title className="px-6 py-4 text-xl font-bold leading-snug text-slate-800 dark:text-white">
+              For a better user experience
+            </Dialog.Title>
+
+            <div className="flex flex-col px-6 pb-4 font-manrope">
+              <div className="space-y-2 py-2 pl-4">
+                <p className="font-bold text-slate-500 dark:text-slate-100">
+                  <span className="text-xs font-semibold uppercase text-slate-400">
+                    android:
+                  </span>{" "}
+                  install PWA
+                </p>
+                <p className="font-bold text-slate-500 dark:text-slate-100">
+                  <span className="text-xs font-semibold uppercase text-slate-400">
+                    iphone:
+                  </span>{" "}
+                  add website to Home Screen
+                </p>
+              </div>
+              <p className="pb-2 pt-4 text-slate-500 dark:text-slate-300">
+                You don&apos;t have to login here. Do it in the PWA.
+              </p>
+              <p className="pb-8 text-slate-500 dark:text-slate-300">
+                Installing or adding to Home screen will save workouts page as
+                startup page.
+              </p>
+              <Dialog.Close className="rounded-lg bg-slate-800 py-2 font-bold text-white dark:bg-white dark:font-extrabold dark:text-slate-800">
+                Got it!
+              </Dialog.Close>
+            </div>
           </div>
-          <p className="pb-4">
-            You don&apos;t have to login here. Do it in the PWA.
-          </p>
-          <p className="pb-16 ">
-            Installing or adding to Home screen will save workouts page as
-            startup page.
-          </p>
-
-          <Drawer.Close className="rounded-lg bg-slate-800 py-2 font-bold text-white dark:bg-white dark:font-extrabold dark:text-slate-800">
-            Got it!
-          </Drawer.Close>
-        </div>
-      </DrawerWrapper>
-    </Drawer.Root>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
