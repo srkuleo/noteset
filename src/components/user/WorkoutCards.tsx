e770829d559988405881c13d600baf440cf68dc0
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-02 04:05:21 +0100

    MODIFY: adds text pretty util class just in chase title is long enough to wrap

--- src/components/user/WorkoutCards.tsx
+++ src/components/user/WorkoutCards.tsx
@@ -23,7 +23,7 @@ export const WorkoutCards = async () => {
           className="flex w-full flex-col gap-4 rounded-xl border border-slate-300/60 bg-white px-4 py-6 shadow-md dark:border-slate-700/50 dark:bg-slate-800/90"
         >
           <div className="space-y-1 px-1">
-            <p className="font-manrope text-lg font-bold dark:text-slate-300">
+            <p className="text-pretty font-manrope text-lg font-bold dark:text-slate-300">
               {workout.title}
             </p>
             <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
