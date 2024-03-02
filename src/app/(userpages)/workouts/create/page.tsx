0d82cd4d2fb36f56843666047933785c040cad24
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-02 03:57:35 +0100

    MODIFY: removes Breadcrumbs, kinda felt rudundant, adds bigger heading moves from form

--- src/app/(userpages)/workouts/create/page.tsx
+++ src/app/(userpages)/workouts/create/page.tsx
@@ -1,6 +1,4 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { type Breadcrumb } from "@/util/types";
-import { Breadcrumbs } from "@/components/user/Breadcrumbs";
 import { CreateForm } from "@/components/user/CreateForm";
 
 export default async function CreateWorkoutPage() {
@@ -8,21 +6,12 @@ export default async function CreateWorkoutPage() {
   const user = await getUser();
   const userId = user?.id as string;
 
-  const breadcrumbs: Breadcrumb[] = [
-    {
-      label: "workouts",
-      href: "/workouts",
-    },
-    {
-      label: "create",
-      href: "/workouts/create",
-      active: true,
-    },
-  ];
 
   return (
     <>
-      <Breadcrumbs breadcrumbs={breadcrumbs} />
+      <p className="text-2xl font-bold font-manrope leading-none pt-2 pb-6">
+        Create a new workout
+      </p>
       <CreateForm userId={userId} />
     </>
   );
--- src/app/(userpages)/workouts/edit/page.tsx
+++ src/app/(userpages)/workouts/edit/page.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { type Metadata } from "next";
 import { getWorkoutById } from "@/db/query";
 import { editWorkout } from "@/util/actions";
-import { type Breadcrumb } from "@/util/types";
-import { Breadcrumbs } from "@/components/user/Breadcrumbs";
 import { SubmitFormButton } from "@/components/user/SubmitFormButton";
+
+import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Edit",
@@ -26,66 +25,56 @@ export default async function EditWorkoutPage({
     workoutId: coercedWorkoutId,
   });
 
-  const breadcrumbs: Breadcrumb[] = [
-    {
-      label: "workouts",
-      href: "/workouts",
-    },
-    {
-      label: workout.title,
-      href: `/workouts/edit?id=${searchParams.id}`,
-      active: true,
-    },
-  ];
-
   return (
     <>
-      <Breadcrumbs breadcrumbs={breadcrumbs} />
-      <div className="rounded-lg bg-white px-4 py-8 shadow-md ring-1 ring-slate-400/30 dark:bg-slate-800">
-        <h2 className="text-center text-lg font-semibold">Edit workout</h2>
-        <form action={editWorkoutWithId} className="space-y-4 pt-4">
-          <div className="flex flex-col gap-2">
-            <label
-              htmlFor="title"
-              className="pl-1 text-sm font-semibold uppercase dark:text-slate-300"
-            >
-              Title
-            </label>
-            <input
-              id="title"
-              name="workoutTitle"
-              type="text"
-              defaultValue={workout.title}
-              className="input-field input-focus-ring"
-            />
-          </div>
+      <p className="pb-6 pt-2 font-manrope text-2xl font-bold leading-none">
+        Editing {workout.title}
+      </p>
+      <form
+        action={editWorkoutWithId}
+        className="space-y-4 rounded-lg bg-white p-6 shadow-md ring-1 ring-slate-300/50 dark:bg-slate-800 dark:ring-slate-700/70"
+      >
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="title"
+            className="pl-1 text-sm font-semibold uppercase dark:text-slate-300"
+          >
+            Title
+          </label>
+          <input
+            id="title"
+            name="workoutTitle"
+            type="text"
+            defaultValue={workout.title}
+            className="input-field input-focus-ring"
+          />
+        </div>
 
-          <div className="flex flex-col gap-2">
-            <label
-              htmlFor="description"
-              className="pl-1 text-sm font-semibold uppercase dark:text-slate-300"
-            >
-              Description
-            </label>
-            <input
-              id="description"
-              name="workoutDescription"
-              type="text"
-              defaultValue={workout.description}
-              className="input-field input-focus-ring"
-            />
-          </div>
-          <div className="flex items-center justify-end gap-2 pt-4">
-            <Link
-              href="/workouts"
-              className="rounded-lg px-3 py-2 text-sm font-semibold active:scale-95 active:bg-slate-100 dark:text-slate-200 active:dark:bg-slate-900/60 "
-            >
-              Cancel
-            </Link>
-            <SubmitFormButton buttonText="Save" />
-          </div>
-        </form>
-      </div>
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="description"
+            className="pl-1 text-sm font-semibold uppercase dark:text-slate-300"
+          >
+            Description
+          </label>
+          <input
+            id="description"
+            name="workoutDescription"
+            type="text"
+            defaultValue={workout.description || "Description not provided."}
+            className="input-field input-focus-ring"
+          />
+        </div>
+        <div className="flex items-center justify-end gap-2 pt-4">
+          <Link
+            href="/workouts"
+            className="rounded-lg px-3 py-2 text-sm font-semibold active:scale-95 active:bg-slate-100 dark:text-slate-200 active:dark:bg-slate-900/60 "
+          >
+            Cancel
+          </Link>
+          <SubmitFormButton label="Save" loading="Saving" />
+        </div>
+      </form>
     </>
   );
 }
