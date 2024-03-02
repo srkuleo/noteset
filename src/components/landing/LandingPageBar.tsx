b7829f3e39e1b8c7142633d26b16ccfd87486661
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-02 03:59:14 +0100

    MODIFY: revert changes back to original, fixed header with overscroll

--- src/components/landing/LandingPageBar.tsx
+++ src/components/landing/LandingPageBar.tsx
@@ -5,7 +5,7 @@ import { GitHubButton } from "./GitHubButton";
 
 export const LandingPageBar = () => {
   return (
-    <div className="border-b border-slate-300 px-4 dark:border-slate-800/90">
+    <div className="fixed inset-x-0 top-0 border-b border-slate-300 bg-slate-200/60 px-4 backdrop-blur-md pt-safe-top dark:border-slate-800/90 dark:bg-slate-950/65">
       <div className="flex justify-between py-2">
         <Link href="/">
           <Logo />
