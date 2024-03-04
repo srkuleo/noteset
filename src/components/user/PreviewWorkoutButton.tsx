1744dbbc52c7a2640b7bf910b2b3607cb4e4508e
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-04 23:57:47 +0100

    MODIFY: animation name changes, improves readability

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -15,7 +15,7 @@ export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
 
-        <Dialog.Content className="fixed inset-x-0 bottom-0 select-none px-4 pb-12 data-[state=closed]:animate-content-hide data-[state=open]:animate-content-show">
+        <Dialog.Content className="fixed inset-x-0 bottom-0 select-none px-4 pb-12 data-[state=closed]:animate-modal-slide-down data-[state=open]:animate-modal-slide-up">
           <div className="rounded-modal bg-white ring-1 ring-slate-200 dark:bg-slate-900 dark:ring-slate-700/80">
             <Dialog.Title className="rounded-t-modal border-b border-b-slate-300/65 bg-slate-200/70 p-2 text-center font-manrope text-lg font-bold dark:border-b-slate-700/80 dark:bg-slate-800">
               Preview: {workout.title}
@@ -34,7 +34,7 @@ export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
                 ))}
               </div>
 
-              <Dialog.Close className="flex items-center justify-center gap-1 rounded-lg bg-slate-800 py-2 font-bold text-white active:bg-slate-600 dark:bg-white dark:font-extrabold dark:text-slate-600 active:dark:bg-slate-700 active:dark:text-white">
+              <Dialog.Close className="flex items-center justify-center gap-1 rounded-lg bg-slate-800 py-2 font-bold text-white outline-none active:bg-slate-600 dark:bg-white dark:font-extrabold dark:text-slate-600 active:dark:bg-slate-700 active:dark:text-white">
                 Hide
                 {HideIcon}
               </Dialog.Close>
