78e88a6d4de5ce0e479a4aadd51d1e12fdf47134
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 01:11:55 +0100

    MODIFY: new way of establishing connection - recommended by Drizzle from now on

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import * as schema from "./schema";
 
-const connection = connect({
-  url: process.env.DATABASE_URL,
+const client = new Client({
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
 });
-
-export const db = drizzle(connection, { schema: schema });
+export const db = drizzle(client, { schema: schema });
