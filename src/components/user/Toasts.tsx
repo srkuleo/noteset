172b86add9337209c7a0ed5278915610153fa712
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 23:09:37 +0100

    MODIFY: final look for the toasts, few semantic changes

--- src/components/user/Toasts.tsx
+++ src/components/user/Toasts.tsx
@@ -4,11 +4,11 @@ import { ErrorIcon, SuccessIcon } from "../icons/user/feedback";
 
 const SuccessToast = ({ message }: { message: string }) => {
   return (
-    <div className="mb-12 pb-safe-bottom">
-      <div className="flex items-center gap-4 rounded-xl bg-green-50 px-4 py-2 font-manrope text-green-600 shadow-md">
+    <div className="mb-7 pb-safe-bottom">
+      <div className="flex items-center gap-4 rounded-xl bg-green-50 px-4 py-2 shadow-md">
         <SuccessIcon />
 
-        <div className="flex flex-col gap-0.5">
+        <div className="flex flex-col gap-0.5 font-manrope text-green-600">
           <p className="font-bold">Success!</p>
           <p className="text-sm font-semibold text-green-500">{message}</p>
         </div>
@@ -19,12 +19,12 @@ const SuccessToast = ({ message }: { message: string }) => {
 
 const ErrorToast = ({ message }: { message: string }) => {
   return (
-    <div className="mb-12 pb-safe-bottom">
-      <div className="flex items-center gap-4 rounded-xl bg-red-50 px-4 py-2 font-manrope text-red-600 shadow-md">
+    <div className="mb-7 pb-safe-bottom">
+      <div className="flex items-center gap-4 rounded-xl bg-red-50 px-4 py-2 shadow-md">
         <ErrorIcon />
 
-        <div className="flex flex-col gap-0.5">
-          <p className="font-bold">Oops!</p>
+        <div className="flex flex-col gap-0.5 font-manrope">
+          <p className="font-bold text-red-600">Oops!</p>
           <p className="text-sm font-semibold text-red-400">{message}</p>
         </div>
       </div>
