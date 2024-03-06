ea092db256dc585faefd61fd6d4d59efa44f3dbb
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 01:09:17 +0100

    MODIFY: adds option to customize default styling with className prop and twMerge

--- src/components/user/SubmitFormButton.tsx
+++ src/components/user/SubmitFormButton.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { useFormStatus } from "react-dom";
+import { twMerge } from "tailwind-merge";
+
+type SubmitButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  label: string;
+  loading: string;
+};
 
 export const SubmitFormButton = ({
   label,
   loading,
-}: {
-  label: string;
-  loading: string;
-}) => {
+  className,
+}: SubmitButtonProps) => {
   const { pending } = useFormStatus();
 
   return (
     <button
       type="submit"
       disabled={pending}
-      className="rounded-lg bg-green-500 px-4 py-1.5 font-manrope font-semibold text-white shadow-sm active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:bg-green-600"
+      className={twMerge(
+        "rounded-lg bg-green-500 px-4 py-1.5 font-manrope font-semibold text-white shadow-sm active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:bg-green-600",
+        className,
+      )}
     >
       {pending ? loading : label}
     </button>
