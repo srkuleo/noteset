6eb67ee5d5445c9211130bd980d747ee822625d7
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 22:14:40 +0100

    MODIFY: adds one more parent div on both toasts with margine and padding bottom

--- src/components/user/Toasts.tsx
+++ src/components/user/Toasts.tsx
@@ -4,12 +4,14 @@ import { ErrorIcon, SuccessIcon } from "../icons/user/feedback";
 
 const SuccessToast = ({ message }: { message: string }) => {
   return (
-    <div className="mb-12 flex items-center gap-4 rounded-xl bg-green-50 px-4 py-2 font-manrope text-green-600 shadow-md">
-      <SuccessIcon />
+    <div className="mb-12 pb-safe-bottom">
+      <div className="flex items-center gap-4 rounded-xl bg-green-50 px-4 py-2 font-manrope text-green-600 shadow-md">
+        <SuccessIcon />
 
-      <div className="flex flex-col gap-0.5">
-        <p className="font-bold">Success!</p>
-        <p className="text-sm font-semibold text-green-500">{message}</p>
+        <div className="flex flex-col gap-0.5">
+          <p className="font-bold">Success!</p>
+          <p className="text-sm font-semibold text-green-500">{message}</p>
+        </div>
       </div>
     </div>
   );
@@ -17,12 +19,14 @@ const SuccessToast = ({ message }: { message: string }) => {
 
 const ErrorToast = ({ message }: { message: string }) => {
   return (
-    <div className="mb-12 flex items-center gap-4 rounded-xl bg-red-50 px-4 py-2 font-manrope text-red-600 shadow-md">
-      <ErrorIcon />
+    <div className="mb-12 pb-safe-bottom">
+      <div className="flex items-center gap-4 rounded-xl bg-red-50 px-4 py-2 font-manrope text-red-600 shadow-md">
+        <ErrorIcon />
 
-      <div className="flex flex-col gap-0.5">
-        <p className="font-bold">Oops!</p>
-        <p className="text-sm font-semibold text-red-400">{message}</p>
+        <div className="flex flex-col gap-0.5">
+          <p className="font-bold">Oops!</p>
+          <p className="text-sm font-semibold text-red-400">{message}</p>
+        </div>
       </div>
     </div>
   );
