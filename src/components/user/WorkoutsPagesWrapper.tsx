92e1bd144c9455d5c4af4e66277cd3315ee2c4b9
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 01:11:27 +0100

    ADD: reusable wrapper component for all the workouts pages, with options to override default styling

---
+++ src/components/user/WorkoutsPagesWrapper.tsx
@@ -0,0 +1,12 @@
+import type { HTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+type WrapperProps = HTMLAttributes<HTMLDivElement> & {
+  children: ReactNode;
+};
+
+export const WorkoutsPagesWrapper = ({ children, className }: WrapperProps) => {
+  return (
+    <div className={twMerge("px-6 pb-safe-bottom", className)}>{children}</div>
+  );
+};
