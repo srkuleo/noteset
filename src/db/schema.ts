99461a45f6cafe5faec0c9706a4735beac690ace
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-06 01:20:36 +0100

    MODIFY: schema now matches the new name for Exercise type

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import type { Exercise } from "@/util/types";
+import type { ExerciseType } from "@/util/types";
 import {
   date,
   index,
@@ -14,7 +14,7 @@ export const workouts = mysqlTable(
     id: serial("id").primaryKey(),
     title: varchar("title", { length: 30 }).notNull(),
     description: varchar("description", { length: 80 }),
-    exercises: json("exercises").$type<Exercise[]>().notNull(),
+    exercises: json("exercises").$type<ExerciseType[]>().notNull(),
     status: varchar("status", { length: 10 }).default("current"),
     userId: varchar("user_id", { length: 255 }).notNull(),
     doneAt: date("done_at"),
