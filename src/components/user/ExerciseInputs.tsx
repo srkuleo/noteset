deeac994c22fc371d75eede2e38a38f2f62ae2ec
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-08 23:58:32 +0100

    ADD: module with all 4 reusable inputs

---
+++ src/components/user/ExerciseInputs.tsx
@@ -0,0 +1,197 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { InputFieldError } from "./InputFieldError";
+
+export const NameInput = ({
+  name,
+  nameError,
+  handleNameInput,
+}: {
+  name: string;
+  nameError: string[] | undefined;
+  handleNameInput: (input: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label
+        htmlFor="name"
+        className="pl-1 text-sm font-semibold uppercase dark:text-slate-200"
+      >
+        Name
+      </label>
+      <input
+        id="name"
+        value={name}
+        type="text"
+        placeholder="e.g. Bench press"
+        className={twMerge(
+          "input-field",
+          "py-2",
+          nameError && "ring-red-500 dark:ring-red-500",
+        )}
+        onChange={(e) => handleNameInput(e.target.value)}
+      />
+      <InputFieldError errorArr={nameError} className="gap-3" />
+    </div>
+  );
+};
+
+export const SetsInput = ({
+  sets,
+  setsError,
+  handleSetsInput,
+}: {
+  sets: number;
+  setsError: string[] | undefined;
+  handleSetsInput: (input: string | number) => void;
+}) => {
+  const chooseSets = [1, 2, 3, 4] as const;
+  const [needMoreSets, setNeedMoreSets] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label
+        htmlFor={needMoreSets ? "setsInput" : "Set 1"}
+        className="pl-1 text-sm font-semibold uppercase dark:text-slate-200"
+      >
+        Sets
+      </label>
+      {needMoreSets ? (
+        <div className="space-x-4 pb-[6px]">
+          <input
+            autoFocus
+            id="setsInput"
+            name="sets"
+            type="number"
+            inputMode="numeric"
+            placeholder="More..."
+            value={sets === 0 ? "" : sets}
+            className={twMerge("input-field", "max-w-[40%] py-2")}
+            onChange={(e) => handleSetsInput(e.target.value)}
+          />
+          <button
+            type="button"
+            className="text-sm font-semibold"
+            onClick={() => setNeedMoreSets(false)}
+          >
+            Back
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center gap-2 overflow-x-scroll p-1 no-scrollbar">
+          {chooseSets.map((cS) => (
+            <button
+              key={cS}
+              id={`Set ${cS}`}
+              type="button"
+              className={twMerge(
+                "rounded-xl bg-slate-200/80 px-6 py-2 font-manrope text-sm ring-1 ring-slate-400/50 dark:bg-slate-900/80 dark:ring-slate-700",
+                sets === cS &&
+                  "bg-green-500 text-white dark:bg-green-600 dark:ring-slate-50",
+              )}
+              onClick={() => handleSetsInput(cS)}
+            >
+              {cS}
+            </button>
+          ))}
+          <button
+            type="button"
+            className="min-w-fit pl-2 text-sm font-semibold"
+            onClick={() => setNeedMoreSets(true)}
+          >
+            More sets
+          </button>
+        </div>
+      )}
+      <InputFieldError errorArr={setsError} className="gap-3" />
+    </div>
+  );
+};
+
+export const RepsInputs = ({
+  form,
+  reps,
+  repsError,
+  handleRepsInput,
+}: {
+  reps: string[];
+  repsError: string[] | undefined;
+  handleRepsInput: (eventValue: string, index: number) => void;
+  form: "edit" | "add";
+}) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <label
+        htmlFor="rep 1"
+        className="flex items-center gap-2 pl-1 font-manrope text-sm font-semibold uppercase dark:text-slate-200"
+      >
+        Reps
+        <span className="text-xs lowercase italic text-slate-400/65 dark:text-slate-500">
+          (eg. 6-8, 10)
+        </span>
+      </label>
+      <div className="flex snap-x snap-proximity gap-2 overflow-x-scroll p-1 no-scrollbar">
+        {reps.map((rep, index) => (
+          <input
+            required
+            autoFocus={form === "add" && index === 0}
+            key={`Rep: ${index + 1}`}
+            id={`rep ${index + 1}`}
+            value={rep}
+            type="text"
+            placeholder={`Rep ${index + 1}`}
+            className={twMerge(
+              "input-field",
+              "max-w-[40%] px-0 py-1.5 text-center",
+              repsError && !/^(?:\d+|\d+-\d+)$/.test(rep)
+                ? "ring-red-500 dark:ring-red-500"
+                : "",
+            )}
+            onChange={(e) => handleRepsInput(e.target.value, index)}
+          />
+        ))}
+      </div>
+      <InputFieldError errorArr={repsError} className="gap-3" />
+    </div>
+  );
+};
+
+export const WeightInputs = ({
+  weights,
+  handleWeightInput,
+}: {
+  weights: number[];
+  handleWeightInput: (eventValue: string, index: number) => void;
+}) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <label
+        htmlFor="weight 1"
+        className="flex items-center gap-2 pl-1 font-manrope text-sm font-semibold uppercase dark:text-slate-200"
+      >
+        Weights
+        <span className="text-xs lowercase italic text-slate-400/65 dark:text-slate-500">
+          (number only)
+        </span>
+      </label>
+      <div className="flex snap-x snap-proximity gap-2 overflow-x-scroll p-1 no-scrollbar">
+        {weights.map((weight, index) => (
+          <input
+            required
+            key={`Weight: ${index + 1}`}
+            id={`weight ${index + 1}`}
+            value={weight === 0 ? "" : weight}
+            type="number"
+            inputMode="numeric"
+            placeholder={`Weight ${index + 1}`}
+            className={twMerge(
+              "input-field",
+              "max-w-[40%] px-0 py-1.5 text-center",
+            )}
+            onChange={(e) => handleWeightInput(e.target.value, index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
