0d8b466f93cb5828654f027e452406d35d912edb
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-08 23:58:00 +0100

    MODIFY: final look, just few more tweaks to add tomorrow, like empty exercise state UI and remove modal? for exercise remove option

--- src/components/user/CreateWorkoutForm.tsx
+++ src/components/user/CreateWorkoutForm.tsx
@@ -3,27 +3,40 @@
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 import { useToastNotification, useWorkouts } from "@/util/hooks";
+import { createWorkout } from "@/util/actions";
 import { AddExerciseForm } from "./AddExerciseForm";
-import { SubmitFormButton } from "./SubmitFormButton";
-import { InputFieldError } from "./InputFieldError";
 import { EditExerciseForm } from "./EditExerciseForm";
+import { RemoveExerciseIcon } from "../icons/user/modify";
+import { InputFieldError } from "./InputFieldError";
+import { SubmitFormButton } from "./SubmitFormButton";
 
 export const CreateWorkoutForm = ({ userId }: { userId: string }) => {
   const {
-    formState,
-    formAction,
-    exercises,
-    formRef,
+    workout,
+    createWorkoutRes,
+    setWorkout,
+    setCreateWorkoutRes,
     updateExercises,
     editExercises,
-  } = useWorkouts(userId);
+    removeExercise,
+    resetWorkoutForm,
+  } = useWorkouts();
 
-  useToastNotification(formState);
+  useToastNotification(createWorkoutRes);
+
+  async function clientAction() {
+    const res = await createWorkout(userId, workout);
+
+    setCreateWorkoutRes({ ...res });
+
+    if (res.status === "success") {
+      resetWorkoutForm();
+    }
+  }
 
   return (
     <form
-      ref={formRef}
-      action={formAction}
+      action={clientAction}
       className="space-y-4 rounded-lg bg-white p-6 shadow-md ring-1 ring-slate-300/50 dark:bg-slate-800 dark:ring-slate-700/70"
     >
       <div className="flex flex-col gap-2">
@@ -35,52 +48,71 @@ export const CreateWorkoutForm = ({ userId }: { userId: string }) => {
         </label>
         <input
           id="title"
-          name="workoutTitle"
+          value={workout.title}
           type="text"
           placeholder="e.g. Upper 1"
           className={twMerge(
             "input-field",
-            formState.errors?.title && "ring-red-500 dark:ring-red-500",
+            createWorkoutRes.errors?.title && "ring-red-500 dark:ring-red-500",
           )}
+          onChange={(e) => setWorkout({ ...workout, title: e.target.value })}
         />
-        <InputFieldError errorArr={formState.errors?.title} className="pl-1" />
+        <InputFieldError
+          errorArr={createWorkoutRes.errors?.title}
+          className="pl-1"
+        />
       </div>
 
       <div className="flex flex-col gap-2">
         <label
           htmlFor="description"
-          className="pl-1 text-sm font-semibold uppercase dark:text-slate-200"
+          className="flex gap-1 pl-1 text-sm font-semibold uppercase dark:text-slate-200"
         >
           Description
+          <span className="text-xs lowercase italic text-slate-400/65 dark:text-slate-500">
+            (optional)
+          </span>
         </label>
         <input
           id="description"
-          name="workoutDescription"
+          value={workout.description}
           type="text"
           placeholder="e.g. Workout for the upper body"
           className="input-field"
+          onChange={(e) =>
+            setWorkout({ ...workout, description: e.target.value })
+          }
         />
       </div>
 
       <AddExerciseForm updateExercises={updateExercises} />
 
-      {exercises.length > 0 && (
+      {workout.exercises.length > 0 && (
         <div
           className={twMerge(
             "flex snap-x snap-proximity items-start gap-4 overflow-x-scroll px-2 py-4 no-scrollbar",
-            exercises.length === 1 && "justify-center",
+            workout.exercises.length === 1 && "justify-center",
           )}
         >
-          {exercises.map((exercise, index) => (
+          {workout.exercises.map((exercise, index) => (
             <div
               key={exercise.name}
               className="relative min-w-[95%] snap-center rounded-lg bg-slate-50 p-4 shadow-md ring-1 ring-slate-200 dark:bg-slate-900/50 dark:ring-slate-700"
             >
-              <EditExerciseForm
-                exercise={exercise}
-                exerciseIndex={index}
-                editExercises={editExercises}
-              />
+              <div className="absolute -right-2 -top-4 flex gap-3">
+                <EditExerciseForm
+                  exercise={exercise}
+                  exerciseIndex={index}
+                  editExercises={editExercises}
+                />
+                <button
+                  className="rounded-full bg-red-500 p-1.5 text-white"
+                  onClick={() => removeExercise(index)}
+                >
+                  {RemoveExerciseIcon}
+                  <span className="sr-only">Remove exercise</span>
+                </button>
+              </div>
               <div className="grid grid-cols-exercise gap-2 text-xs">
                 <p className="font-bold italic">Name</p>
                 <p className="text-center font-bold italic">Sets</p>
@@ -109,14 +141,16 @@ export const CreateWorkoutForm = ({ userId }: { userId: string }) => {
         </div>
       )}
       <InputFieldError
-        errorArr={formState.errors?.exercises}
+        errorArr={createWorkoutRes.errors?.exercises}
         className="justify-center py-4"
       />
 
       <div
         className={twMerge(
           "flex justify-end gap-2",
-          exercises.length === 0 && !formState.errors?.exercises && "pt-4",
+          workout.exercises.length === 0 &&
+            !createWorkoutRes.errors?.exercises &&
+            "pt-4",
         )}
       >
         <Link
