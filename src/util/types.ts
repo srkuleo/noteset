c11ab86a709e79a18a1ef46a0a2c0a5bddd18ee7
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-08 03:33:41 +0100

    MODIFY: adds regex for string inside reps array, which only accept numerical strings or string representing numberical range e.g. 10-12. Based on that error ring around rep input is being rendered.

--- src/util/types.ts
+++ src/util/types.ts
@@ -38,7 +38,12 @@ export const ExerciseSchema = z.object({
     .min(2, { message: "Exercise name must be at least 2 characters long." })
     .max(30, { message: "Too long. Keep it less than 30 characters." }),
   sets: z.number().min(1, { message: "Please choose the number of sets." }),
-  reps: z.array(z.string()),
+  reps: z
+    .array(
+      z
+        .string()
+        .regex(/^(?:\d+|\d+-\d+)$/, { message: "Reps must be number or range." }),
+    ),
   weights: z.array(z.number()),
   comment: z.string().max(80, { message: "Comment is too long" }).optional(),
 });
