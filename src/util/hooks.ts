0b85e91d5fd43911efbf48ae81dfaef05c364a25
Srdjan <milicevic.srdjan0@gmail.com>
2024-03-09 00:03:24 +0100

    MODIFY: way more concise, removes redundant state and moves it closer to the child that uses it like needMoreSets and chooseSets, remove confusing useRef for form resetting since it's now done from clientAction

--- src/util/hooks.ts
+++ src/util/hooks.ts
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from "react";
-import { useFormState } from "react-dom";
-import { createWorkout } from "./actions";
 import { showToast } from "@/components/user/Toasts";
 
 import type {
   ExerciseActionResponse,
   ExerciseType,
   WorkoutActionResponse,
+  WorkoutType,
 } from "./types";
 
 const initErrors: ExerciseActionResponse = {
@@ -16,11 +15,16 @@ const initErrors: ExerciseActionResponse = {
 
 export const useExerciseForm = (initExercise: ExerciseType) => {
   const [tempExercise, setTempExercise] = useState(initExercise);
-  const [needMoreSets, setNeedMoreSets] = useState(false);
   const [exerciseFormErrors, setExerciseFormErrors] = useState(initErrors);
-  const chooseSets = [1, 2, 3, 4] as const;
 
-  const handleSetsInput = (input: string | number) => {
+  function handleNameInput(input: string) {
+    setTempExercise({
+      ...tempExercise,
+      name: input,
+    });
+  }
+
+  function handleSetsInput(input: string | number) {
     const sets = Number(input);
     const reps = Array.from({ length: sets }, () => "");
     const weights = Array.from({ length: sets }, () => 0);
@@ -31,17 +35,30 @@ export const useExerciseForm = (initExercise: ExerciseType) => {
       reps: [...reps],
       weights: [...weights],
     });
-  };
 
-  const handleRepsInput = (eventValue: string, index: number) => {
+    if (
+      exerciseFormErrors.errors?.sets &&
+      exerciseFormErrors.errors.sets.length > 0
+    ) {
+      setExerciseFormErrors({
+        ...exerciseFormErrors,
+        errors: {
+          ...exerciseFormErrors.errors,
+          sets: undefined,
+        },
+      });
+    }
+  }
+
+  function handleRepsInput(eventValue: string, index: number) {
     const modifiedReps = tempExercise.reps.toSpliced(index, 1, eventValue);
 
     console.log(modifiedReps);
 
     setTempExercise({ ...tempExercise, reps: [...modifiedReps] });
-  };
+  }
 
-  const handleWeightInput = (eventValue: string, index: number) => {
+  function handleWeightInput(eventValue: string, index: number) {
     const weight = Number(eventValue);
 
     const modifiedWeights = tempExercise.weights.toSpliced(index, 1, weight);
@@ -49,89 +66,122 @@ export const useExerciseForm = (initExercise: ExerciseType) => {
     console.log(modifiedWeights);
 
     setTempExercise({ ...tempExercise, weights: [...modifiedWeights] });
-  };
+  }
 
   return {
-    chooseSets,
     tempExercise,
-    needMoreSets,
     exerciseFormErrors,
-    setTempExercise,
-    setNeedMoreSets,
     setExerciseFormErrors,
+    handleNameInput,
     handleSetsInput,
     handleRepsInput,
     handleWeightInput,
   };
 };
 
-export const useToastNotification = (formState: WorkoutActionResponse) => {
-  const prevTimestamp = useRef(formState.timestamp);
+export const useToastNotification = (
+  createWorkoutRes: WorkoutActionResponse,
+) => {
+  const prevTimestamp = useRef(createWorkoutRes.timestamp);
 
   //renders a new toast notif for every form validation process
   useEffect(() => {
-    if (formState.message && formState.timestamp !== prevTimestamp.current) {
-      if (formState.status === "error") {
-        showToast(formState.message, "error");
+    if (
+      createWorkoutRes.message &&
+      createWorkoutRes.timestamp !== prevTimestamp.current
+    ) {
+      if (createWorkoutRes.status === "error") {
+        showToast(createWorkoutRes.message, "error");
       } else {
-        showToast(formState.message, "success");
+        showToast(createWorkoutRes.message, "success");
       }
     }
-  }, [formState.status, formState.message, formState.timestamp]);
+  }, [
+    createWorkoutRes.status,
+    createWorkoutRes.message,
+    createWorkoutRes.timestamp,
+  ]);
 };
 
-const initExercises: ExerciseType[] = [];
+const initWorkout: WorkoutType = {
+  title: "",
+  description: "",
+  exercises: [],
+};
 
-const emptyFormState: WorkoutActionResponse = {
+const emptyRes: WorkoutActionResponse = {
   status: "unset",
   message: "",
   errors: {},
   timestamp: Date.now(),
 };
 
-/* contains exercises state, setter, ref for the workout form reset, 
-formState and formAction for handling validation on the server 
-and saving error messages to be rendered in toast notif and below inputs */
-export const useWorkouts = (userId: string) => {
-  const [exercises, setExercises] = useState(initExercises);
-  const [formState, formAction] = useFormState(
-    createWorkout.bind(null, userId, exercises),
-    emptyFormState,
-  );
-  const formRef = useRef<HTMLFormElement>(null);
-  const prevTimestamp = useRef(formState.timestamp);
-
-  //resets form if workout is created and empties exercises state
-  useEffect(() => {
-    if (!formRef.current) return;
-
-    if (
-      formState.status === "success" &&
-      formState.timestamp !== prevTimestamp.current
-    ) {
-      formRef.current.reset();
-
-      setExercises(initExercises);
-
-      prevTimestamp.current = formState.timestamp;
-    }
-  }, [formState.status, formState.timestamp]);
+/* contains workout state (data passed to server action), 
+createWorkoutRes (a state that preserve the server action feedback, needed for
+toast notif, errors and reseting form),
+resetForm function that runs if createWorkoutRes.status is successful
+and two handler functions for creating and editing existing exercise inside a workout */
+export const useWorkouts = () => {
+  const [workout, setWorkout] = useState(initWorkout);
+  const [createWorkoutRes, setCreateWorkoutRes] = useState(emptyRes);
 
   function updateExercises(newExercise: ExerciseType) {
-    setExercises([...exercises, { ...newExercise }]);
+    setWorkout({
+      title: workout.title,
+      description: workout.description,
+      exercises: [...workout.exercises, newExercise],
+    });
+
+    if (
+      createWorkoutRes.errors?.exercises &&
+      createWorkoutRes.errors.exercises.length > 0
+    ) {
+      setCreateWorkoutRes({
+        ...createWorkoutRes,
+        errors: {
+          ...createWorkoutRes.errors,
+          exercises: undefined,
+        },
+      });
+    }
   }
 
   function editExercises(editedExercise: ExerciseType, index: number) {
-    const modifiedExercises = exercises.toSpliced(index, 1, editedExercise);
-    setExercises([...modifiedExercises]);
+    const modifiedExercises = workout.exercises.toSpliced(
+      index,
+      1,
+      editedExercise,
+    );
+
+    setWorkout({
+      title: workout.title,
+      description: workout.description,
+      exercises: [...modifiedExercises],
+    });
+  }
+
+  function removeExercise(index: number) {
+    const modifiedExercises = workout.exercises.filter((_, i) => i !== index);
+
+    setWorkout({
+      title: workout.title,
+      description: workout.description,
+      exercises: modifiedExercises,
+    });
+  }
+
+  function resetWorkoutForm() {
+    setWorkout(initWorkout);
   }
 
   return {
-    formRef,
-    exercises,
+    workout,
+    createWorkoutRes,
+    setWorkout,
+    setCreateWorkoutRes,
     updateExercises,
     editExercises,
-    formState,
-    formAction,
+    removeExercise,
+    resetWorkoutForm,
   };
 };
