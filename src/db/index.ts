e61941c30043c53d3e92a6bce0eb5189e81a5ccd
Srdjan <milicevic.srdjan0@gmail.com>
2024-04-06 22:18:03 +0200

    MODIFY: new way of connecting to Neon pg db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,7 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
 
-const client = new Client({
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-});
-export const db = drizzle(client, { schema: schema });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+export const db = drizzle(pool, { schema: schema });
