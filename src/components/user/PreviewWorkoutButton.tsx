30510cd23b508d6d31023236de2f87db9e49ab99
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-05 17:08:28 +0200

    MODIFY: changes to how modal interacts with clicks, adds drag to close modal feature

--- src/components/user/PreviewWorkoutButton.tsx
+++ src/components/user/PreviewWorkoutButton.tsx
@@ -1,25 +1,28 @@
 "use client";
 
-import * as Dialog from "@radix-ui/react-dialog";
+import { Drawer } from "vaul";
 import { HideIcon, ShowIcon } from "../icons/user/preview";
 
 import type { Workout } from "@/db/schema";
 
 export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger className="rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
+    <Drawer.Root>
+      <Drawer.Trigger className="rounded-lg px-2 py-1 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600">
         {ShowIcon}
-      </Dialog.Trigger>
+      </Drawer.Trigger>
 
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
 
-        <Dialog.Content className="fixed inset-x-0 bottom-0 select-none px-4 pb-12 data-[state=closed]:animate-modal-slide-down data-[state=open]:animate-modal-slide-up">
+        <Drawer.Content className="fixed inset-x-0 bottom-0 select-none px-4 pb-12">
           <div className="rounded-modal bg-white ring-1 ring-slate-200 dark:bg-slate-900 dark:ring-slate-700/80">
-            <Dialog.Title className="rounded-t-modal border-b border-b-slate-300/65 bg-slate-200/70 p-2 text-center font-manrope text-lg font-bold dark:border-b-slate-700/80 dark:bg-slate-800">
-              Preview: {workout.title}
-            </Dialog.Title>
+            <div className="rounded-t-modal border-b border-b-slate-300/65 bg-slate-200/70 p-2 dark:border-b-slate-700/80 dark:bg-slate-800">
+              <Drawer.Handle className="dark:bg-slate-500 bg-slate-300" />
+              <Drawer.Title className="pt-2 text-center font-manrope text-lg font-bold">
+                Preview: {workout.title}
+              </Drawer.Title>
+            </div>
 
             <div className="flex flex-col p-4">
               <p className="pb-2">
@@ -34,14 +37,14 @@ export const PreviewWorkoutButton = ({ workout }: { workout: Workout }) => {
                 ))}
               </div>
 
-              <Dialog.Close className="flex items-center justify-center gap-1 rounded-lg bg-slate-800 py-2 font-bold text-white outline-none active:bg-slate-600 dark:bg-white dark:font-extrabold dark:text-slate-600 active:dark:bg-slate-700 active:dark:text-white">
+              <Drawer.Close className="flex items-center justify-center gap-1 rounded-lg bg-slate-800 py-2 font-bold text-white outline-none active:bg-slate-600 dark:bg-white dark:font-extrabold dark:text-slate-600 active:dark:bg-slate-700 active:dark:text-white">
                 Hide
                 {HideIcon}
-              </Dialog.Close>
+              </Drawer.Close>
             </div>
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
