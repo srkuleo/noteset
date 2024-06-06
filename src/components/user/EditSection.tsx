f47e6d961ab4d986ab49594457aa183fee1f7e35
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-07 01:16:10 +0200

    MODIFY: refactors UI to better suits Vauls behaviour

--- src/components/user/EditSection.tsx
+++ src/components/user/EditSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import * as Dialog from "@radix-ui/react-dialog";
+import { Drawer } from "vaul";
 import { useState } from "react";
 import { removeWorkout } from "@/util/actions";
 import { DangerIcon } from "../icons/user/warning";
@@ -22,15 +22,18 @@ export const EditSection = ({ workout }: { workout: Workout }) => {
         {EditWorkoutIcon}
       </Link>
 
-      <Dialog.Root open={open} onOpenChange={setOpen}>
-        <Dialog.Trigger className="select-none rounded-full p-1.5 text-red-500 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:text-red-400 dark:shadow-slate-900 dark:ring-slate-600">
+      <Drawer.Root open={open} onOpenChange={setOpen}>
+        <Drawer.Trigger className="select-none rounded-full p-1.5 text-red-500 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:text-red-400 dark:shadow-slate-900 dark:ring-slate-600">
           {RemoveWorkoutIcon}
-        </Dialog.Trigger>
+        </Drawer.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
+        <Drawer.Portal>
+          <Drawer.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
 
-          <Dialog.Content className="fixed inset-x-0 bottom-0 select-none space-y-4 px-4 pb-12 data-[state=closed]:animate-modal-slide-down data-[state=open]:animate-modal-slide-up">
+          <Drawer.Content
+            data-vaul-no-drag
+            className="fixed inset-x-0 bottom-0 select-none space-y-4 px-4 pb-12 focus:outline-none"
+          >
             <div className="flex flex-col items-center gap-3 rounded-modal bg-slate-50/90 pt-5 dark:bg-slate-700/80">
               <div className="rounded-full bg-red-400 p-2 text-white shadow-sm dark:bg-red-200 dark:text-red-500">
                 {DangerIcon}
@@ -57,12 +60,18 @@ export const EditSection = ({ workout }: { workout: Workout }) => {
                 Remove {workout.title}
               </button>
             </div>
-            <Dialog.Close className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90">
+            <button
+              onClick={async () => {
+                await new Promise((resolve) => setTimeout(resolve, 200));
+                setOpen(false);
+              }}
+              className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
+            >
               Cancel
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+            </button>
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
     </div>
   );
 };
