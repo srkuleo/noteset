93d83edd92949a96ab5863777a8b3a4d4d9e40c1
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-08 23:17:38 +0200

    MODIFY: fixes an issue with component still using radix dialog instead of vaul drawer, removes unused custom animations

--- src/components/user/RemoveExerciseModal.tsx
+++ src/components/user/RemoveExerciseModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import * as Dialog from "@radix-ui/react-dialog";
+import { Drawer } from "vaul";
 import { RemoveExerciseIcon } from "../icons/user/modify";
 import { DangerIcon } from "../icons/user/warning";
 
@@ -13,16 +13,19 @@ export const RemoveExerciseModal = ({
   const [open, setOpen] = useState(false);
 
   return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
-      <Dialog.Trigger className="select-none rounded-full bg-red-500 p-1.5 text-white">
+    <Drawer.Root open={open} onOpenChange={setOpen}>
+      <Drawer.Trigger className="select-none rounded-full bg-red-500 p-1.5 text-white">
         {RemoveExerciseIcon}
         <span className="sr-only">Remove exercise</span>
-      </Dialog.Trigger>
+      </Drawer.Trigger>
 
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/70" />
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
 
-        <Dialog.Content className="fixed inset-x-0 bottom-0 select-none space-y-4 px-4 pb-12 data-[state=closed]:animate-modal-slide-down data-[state=open]:animate-modal-slide-up">
+        <Drawer.Content
+          data-vaul-no-drag
+          className="fixed inset-x-0 bottom-0 select-none space-y-4 px-4 pb-12 focus:outline-none"
+        >
           <div className="flex flex-col items-center gap-3 rounded-modal bg-slate-50/90 pt-5 dark:bg-slate-700/80">
             <div className="rounded-full bg-red-400 p-2 text-white shadow-sm dark:bg-red-200 dark:text-red-500">
               {DangerIcon}
@@ -30,7 +33,9 @@ export const RemoveExerciseModal = ({
             <div className="px-1 pt-2">
               <p className="text-center font-semibold leading-snug text-slate-600 dark:text-slate-400">
                 Are you sure you want to remove{" "}
-                <span className="font-bold text-slate-700 dark:text-slate-200">{exerciseName}</span>{" "}
+                <span className="font-bold text-slate-700 dark:text-slate-200">
+                  {exerciseName}
+                </span>{" "}
                 exercise from the current list? This action is irreversible.
               </p>
             </div>
@@ -44,11 +49,11 @@ export const RemoveExerciseModal = ({
               Remove {exerciseName}
             </button>
           </div>
-          <Dialog.Close className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90">
+          <Drawer.Close className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90">
             Cancel
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+          </Drawer.Close>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
