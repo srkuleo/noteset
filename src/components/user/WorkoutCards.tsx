6aa389d8a9fbf28ccc5808c11b6951a3e8d12be3
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-14 21:12:05 +0200

    MODIFY: now uses useSearchPArams to read searchParam returned by editWorkout action, run useEffect onMount to display toast confirmation and replaces route with regular /workouts route to escape potential toast rerender if page is reloaded

--- src/components/user/WorkoutCards.tsx
+++ src/components/user/WorkoutCards.tsx
@@ -1,17 +1,34 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { showToast } from "./Toasts";
 import { EmptyPage } from "./EmptyPage";
 import { EditSection } from "./EditSection";
-import { PreviewWorkoutButton } from "./PreviewWorkoutButton";
 import { RemoveWorkoutModal } from "./RemoveWorkoutModal";
+import { PreviewWorkoutButton } from "./PreviewWorkoutButton";
 
 import type { Workout } from "@/db/schema";
 
 export const WorkoutCards = ({ workouts }: { workouts: Workout[] }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const [openRemoveModal, setOpenRemoveModal] = useState(false);
   const [workoutToRemove, setWorkoutToRemove] = useState({ title: "", id: 0 });
+
+  const message = searchParams.get("message");
+
+  useEffect(() => {
+    if (message) {
+      //Shows toast if searchParam is provided
+      showToast(message, "success");
+
+      /* Replaces the URL with original /workouts route, preventing toast 
+      from re-rendering on subsequent reloads */
+      router.replace("/workouts");
+    }
+  }, []);
 
   return (
     <>
@@ -31,7 +48,7 @@ export const WorkoutCards = ({ workouts }: { workouts: Workout[] }) => {
               className="flex w-full flex-col gap-4 rounded-xl border border-slate-300/60 bg-white px-4 py-6 shadow-md dark:border-slate-700/50 dark:bg-slate-800/90"
             >
               <div className="space-y-1.5 px-1">
-                <p className="text-pretty font-manrope text-lg font-bold dark:text-slate-300 uppercase">
+                <p className="text-pretty font-manrope text-lg font-bold uppercase dark:text-slate-300">
                   {workout.title}
                 </p>
                 <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
