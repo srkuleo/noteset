477a6ac2f047adb09ecb3587d5a0f581025c5a1a
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-16 00:19:43 +0200

    MODIFY: changes the way connection between Vercel serverless environment and Neon db is established. Recommended by both Drizzle and Neon since websockets and pools don't persist in Vercel environment.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const sql = neon(process.env.DATABASE_URL);
 
-export const db = drizzle(pool, { schema: schema });
+export const db = drizzle(sql, { schema: schema });
