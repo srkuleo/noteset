ebc69de6290d628a267f4f489a5ef3d970a137db
Srdjan <milicevic.srdjan0@gmail.com>
2024-06-20 17:10:21 +0200

    MODIFY: exerciseToRemove moved to the ExercisesCarousel and renamed to "currentExercise" used by both edit and remove modal, simpler workflow and easier to comprehend

--- src/components/user/CreateWorkoutForm.tsx
+++ src/components/user/CreateWorkoutForm.tsx
@@ -1,29 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { emptyWorkout, useWorkouts } from "@/util/hooks";
 import { createWorkout } from "@/util/actions";
 import { showToast } from "./Toasts";
-import { AddExerciseForm } from "./AddExerciseForm";
-import { EditExerciseForm } from "./EditExerciseForm";
-import { RemoveExerciseModal } from "./RemoveExerciseModal";
-import { AddIcon, RemoveExerciseIcon } from "../icons/user/modify";
 import { InputFieldError } from "./InputFieldError";
 import { SubmitFormButton } from "./SubmitFormButton";
+import { ExercisesCarousel } from "./ExercisesCarousel";
 
 export const CreateWorkoutForm = ({ userId }: { userId: string }) => {
-  const [openAddDrawer, setOpenAddDrawer] = useState(false);
-  const [openRemoveModal, setOpenRemoveModal] = useState(false);
-
   const {
     workout,
     actionRes,
-    exerciseToRemove,
     setWorkout,
     setActionRes,
-    setExerciseToRemove,
     updateExercises,
     editExercises,
     removeExercise,
@@ -91,110 +82,12 @@ export const CreateWorkoutForm = ({ userId }: { userId: string }) => {
         />
       </div>
 
-      <AddExerciseForm
-        isOpen={openAddDrawer}
-        setIsOpen={setOpenAddDrawer}
+      <ExercisesCarousel
+        exercises={workout.exercises}
         updateExercises={updateExercises}
-      />
-
-      <RemoveExerciseModal
-        isOpen={openRemoveModal}
-        setIsOpen={setOpenRemoveModal}
-        exerciseName={exerciseToRemove.name}
+        editExercises={editExercises}
         removeExercise={removeExercise}
       />
-
-      {workout.exercises.length === 0 ? (
-        <div className="pt-4">
-          <div className="rounded-xl border-2 border-dashed border-slate-300/80 bg-slate-50 px-4 py-8 dark:border-slate-700 dark:bg-slate-900/40">
-            <p className="pb-4 text-center text-sm font-semibold text-slate-400/80 dark:text-slate-500">
-              Currently no exercise added.
-            </p>
-            <div className="flex w-full justify-center focus:outline-none">
-              <button
-                type="button"
-                onClick={() => setOpenAddDrawer(true)}
-                className="rounded-full bg-violet-500/90 p-2 text-white shadow-md dark:bg-violet-500"
-              >
-                <AddIcon size={24} strokeWidth={1.5} />
-                <span className="sr-only">Add Exercise</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div className="flex w-full justify-center py-2 focus:outline-none">
-            <button
-              type="button"
-              onClick={() => setOpenAddDrawer(true)}
-              className="rounded-full bg-violet-500/90 p-2 text-white shadow-md dark:bg-violet-500"
-            >
-              <AddIcon size={24} strokeWidth={1.5} />
-              <span className="sr-only">Add Exercise</span>
-            </button>
-          </div>
-
-          <div
-            className={twMerge(
-              "flex snap-x snap-proximity items-start gap-4 overflow-x-scroll px-2 py-4 no-scrollbar",
-              workout.exercises.length === 1 && "justify-center",
-            )}
-          >
-            {workout.exercises.map((exercise, index) => (
-              <div
-                key={exercise.name}
-                className="relative min-w-[95%] snap-center rounded-lg bg-slate-50 p-4 shadow-md ring-1 ring-slate-200 dark:bg-slate-900/50 dark:ring-slate-700"
-              >
-                <div className="absolute -right-2 -top-4 flex gap-3">
-                  <EditExerciseForm
-                    exercise={exercise}
-                    exerciseIndex={index}
-                    editExercises={editExercises}
-                  />
-                  <button
-                    type="button"
-                    onClick={() => {
-                      setExerciseToRemove({
-                        name: exercise.name,
-                        id: index,
-                      });
-                      setOpenRemoveModal(true);
-                    }}
-                    className="select-none rounded-full bg-red-500 p-1.5 text-white"
-                  >
-                    {RemoveExerciseIcon}
-                    <span className="sr-only">Remove exercise</span>
-                  </button>
-                </div>
-                <div className="grid grid-cols-exercise gap-2 text-xs">
-                  <p className="font-bold italic">Name</p>
-                  <p className="text-center font-bold italic">Sets</p>
-                  <p className="text-center font-bold italic">Reps</p>
-                  <p className="text-center font-bold italic">Weights - kg</p>
-
-                  <div className="col-span-4 h-[1px] bg-slate-200 dark:bg-slate-700" />
-
-                  <p className="my-auto dark:text-slate-200">{exercise.name}</p>
-                  <p className="my-auto text-center dark:text-slate-200">
-                    {exercise.sets}
-                  </p>
-                  <div className="flex grow flex-col items-center justify-center gap-2 dark:text-slate-200">
-                    {exercise.reps.map((rep, i) => (
-                      <p key={`Rep: ${i + 1}`}>{rep}</p>
-                    ))}
-                  </div>
-                  <div className="flex grow flex-col items-center justify-center gap-2 dark:text-slate-200">
-                    {exercise.weights.map((weight, i) => (
-                      <p key={`Weight: ${i + 1}`}>{weight}</p>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
       <InputFieldError
         errorArr={actionRes.errors?.exercises}
         className="justify-center py-4"
