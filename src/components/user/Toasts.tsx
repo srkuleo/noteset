33ab4457e7ebf3215423b2d4b2e96ea7a6a2f086
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:15:57 +0200

    MODIFY: one universal toast with different styling depending on the type

--- src/components/user/Toasts.tsx
+++ src/components/user/Toasts.tsx
@@ -1,56 +1,55 @@
+import Link from "next/link";
 import debounce from "lodash.debounce";
 import { toast } from "sonner";
+import { twMerge } from "tailwind-merge";
+import { ErrorIcon, SuccessIcon } from "../icons/user/toasts";
 
-export const SuccessToast = ({ message }: { message: string }) => {
-  return (
-    <div className="pb-safe-bottom">
-      <div className="flex justify-between gap-4 rounded-xl bg-white px-4 py-3 shadow-md ring-1 ring-slate-300/60 dark:bg-slate-900 dark:ring-slate-700/90">
-        <div className="flex flex-col gap-0.5 font-manrope">
-          <p className="font-bold text-green-600">Success!</p>
-          <p className="text-sm font-semibold text-slate-400/95 dark:text-slate-200">
-            {message}
-          </p>
-        </div>
-
-        <button className="text-xs text-slate-400/80 underline underline-offset-2 dark:text-slate-300">
-          Hide
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export const ErrorToast = ({ message }: { message: string }) => {
-  return (
-    <div className="pb-safe-bottom">
-      <div className="flex justify-between gap-4 rounded-xl bg-white px-4 py-3 shadow-md ring-1 ring-slate-300/60 dark:bg-slate-900 dark:ring-slate-700/90">
-        <div className="flex flex-col gap-0.5 font-manrope">
-          <p className="font-bold text-red-600">Oops...</p>
-          <p className="text-sm font-semibold text-slate-400/95 dark:text-slate-200">
-            {message}
-          </p>
-        </div>
-
-        <button className="text-xs text-slate-400/80 underline underline-offset-2 dark:text-slate-300">
-          Hide
-        </button>
-      </div>
-    </div>
-  );
-};
-
-type ToastType = "error" | "success";
+type ToastType = "error" | "success" | "success-redirect";
 
 export const showToast = debounce(
-  (message: string, type: ToastType) =>
+  (message: string, type: ToastType, path?: string, linkText?: string) =>
+    // Accepts 2-4 args
     toast.custom((t) => (
-      <div onClick={() => toast.dismiss(t)}>
-        {type === "success" ? (
-          <SuccessToast message={message} />
-        ) : (
-          <ErrorToast message={message} />
-        )}
+      <div onClick={() => toast.dismiss(t)} className="pb-safe-bottom">
+        <Toast message={message} type={type} path={path} linkText={linkText} />
       </div>
     )),
   250,
 );
+
+export const Toast = ({
+  message,
+  type,
+  path,
+  linkText,
+}: {
+  message: string;
+  type: ToastType;
+  path?: string;
+  linkText?: string;
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "flex items-center justify-between gap-2 rounded-lg bg-white p-4 font-manrope text-sm shadow-md ring-[1.5px] dark:bg-slate-900",
+        type === "error" ? "ring-red-500" : "ring-green-500",
+      )}
+    >
+      <div className="flex items-center gap-1.5">
+        {type === "success" ? (
+          <SuccessIcon />
+        ) : type === "error" ? (
+          <ErrorIcon />
+        ) : null}
+
+        <p className="text-slate-800 dark:text-slate-200">{message}</p>
+      </div>
+      
+      {path && linkText && (
+        <Link href={path} className="text-center font-bold text-green-500">
+          {linkText}
+        </Link>
+      )}
+    </div>
+  );
+};
