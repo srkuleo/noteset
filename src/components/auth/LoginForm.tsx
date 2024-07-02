abd7e3041dbda6367542abc12d14f30343bfa325
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:05:53 +0200

    ADD: client side for with error handling for login page

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { login, type AuthActionResponse } from "@/util/actions/auth";
+import { HideIcon, ShowIcon } from "../icons/user/preview";
+import { InputFieldError } from "../user/InputFieldError";
+import { SubmitFormButton } from "../user/FormButtons";
+
+export const LoginForm = () => {
+  const [actionRes, setActionRes] = useState<AuthActionResponse>({});
+  const [showPassword, setShowPassword] = useState(false);
+
+  function togglePasswordVisibility() {
+    setShowPassword(!showPassword);
+  }
+
+  async function clientLogin(formData: FormData) {
+    const res = await login(formData);
+
+    setActionRes({ ...res });
+  }
+
+  return (
+    <form action={clientLogin} className="flex flex-col gap-4">
+      <input
+        id="username"
+        name="username"
+        type="text"
+        placeholder="Username"
+        autoComplete="username"
+        className={twMerge(
+          "input-field",
+          "bg-white dark:bg-slate-800/50",
+          actionRes.errors?.username && "ring-red-500 dark:ring-red-500",
+        )}
+      />
+      <InputFieldError errorArr={actionRes.errors?.username} className="pl-1" />
+
+      <div className="relative">
+        <input
+          id="password"
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          autoComplete="current-password"
+          required
+          className={twMerge(
+            "input-field",
+            "w-full bg-white dark:bg-slate-800/50",
+            actionRes.errors?.password && "ring-red-500 dark:ring-red-500",
+          )}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute inset-y-0 right-0 px-3 py-2"
+        >
+          {showPassword ? (
+            <HideIcon className="size-6" />
+          ) : (
+            <ShowIcon className="size-6" />
+          )}
+        </button>
+      </div>
+      <InputFieldError errorArr={actionRes.errors?.password} className="pl-1" />
+
+      <InputFieldError message={actionRes.message} />
+
+      <SubmitFormButton
+        label="Login"
+        loading="Authenticating..."
+        className="mt-6 rounded-full py-2.5"
+      />
+    </form>
+  );
+};
