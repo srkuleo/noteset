1cebb71b0e6369752f8bf89282f69e0988da5154
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:11:09 +0200

    RENAME: now exports 2 form buttons

--- src/components/user/FormButtons.tsx
+++ src/components/user/FormButtons.tsx
@@ -28,3 +28,17 @@ export const SubmitFormButton = ({
     </button>
   );
 };
+
+export const ConfirmLogOutButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 disabled:dark:text-green-800 disabled:dark:bg-slate-900/75 dark:border-slate-600 active:dark:bg-slate-600/90"
+    >
+      {pending ? "Logging Out..." : "Log Out"}
+    </button>
+  );
+};
