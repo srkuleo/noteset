2f5428bb67d1a125c3fc2c8e63df6ed5c00a4a7c
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:05:25 +0200

    MODIFY: formatted

--- src/app/workout/layout.tsx
+++ src/app/workout/layout.tsx
@@ -4,14 +4,10 @@ export const metadata: Metadata = {
   title: "Workout",
 };
 
-export default function WorkoutPageLayout({
+export default async function WorkoutPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <main className="flex min-h-screen flex-col">
-     {children}
-    </main>
-  );
+  return <main className="flex min-h-screen flex-col">{children}</main>;
 }
