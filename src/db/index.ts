1bd738c2fa83008a6a6f5a9c40c8a8fd9ea78cd0
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:18:00 +0200

    MODIFY: adds drizzle adapter for lucia auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,14 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema: schema });
+
+export const adapter = new DrizzlePostgreSQLAdapter(
+  db,
+  schema.sessions,
+  schema.users,
+);
