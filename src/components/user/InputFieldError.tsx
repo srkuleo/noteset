81f8f0d4b627fdf9d63b5d2e7f0953a5f9aef8a3
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:12:20 +0200

    MODIFY: now accepts message for situation where errors are not provided - such as one universal error message for forms

--- src/components/user/InputFieldError.tsx
+++ src/components/user/InputFieldError.tsx
@@ -2,23 +2,41 @@ import { twMerge } from "tailwind-merge";
 import { ErrorTriangleIcon } from "../icons/user/warning";
 
 type InputFieldErrorProps = React.HTMLAttributes<HTMLParagraphElement> & {
-  errorArr: string[] | undefined;
+  errorArr?: string[];
+  message?: string;
 };
 
 export const InputFieldError = ({
   errorArr,
+  message,
   className,
 }: InputFieldErrorProps) => {
+  if (message) {
+    return (
+      <div className={twMerge("flex items-center gap-2.5 pt-2", className)}>
+        <ErrorTriangleIcon size={6} />
+
+        <p className="font-semibold leading-tight text-red-500 dark:text-slate-100">
+          {message}
+        </p>
+      </div>
+    );
+  }
+
   if (!errorArr) {
     return null;
   }
 
-  return (
-    <div className={twMerge("flex items-center gap-1.5 pt-1", className)}>
-      <div className="text-red-500">{ErrorTriangleIcon}</div>
-      <p className="text-sm font-semibold leading-tight text-red-500">
-        {errorArr[0]}
+  return errorArr.map((error, i) => (
+    <div
+      key={i}
+      className={twMerge("flex items-center gap-1.5 pt-1", className)}
+    >
+      <ErrorTriangleIcon size={5} />
+
+      <p className="text-sm font-semibold leading-tight text-red-500 dark:text-white">
+        {error}
       </p>
     </div>
-  );
+  ));
 };
