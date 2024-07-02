36c6d0b61ca8701fcadc6e7d64880a36276cbe2b
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-02 03:20:36 +0200

    MODIFY: adds users and sessions table, adds foreign key to userId column in both sessions and workouts table,

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,9 +7,42 @@ import {
   timestamp,
   varchar,
   pgEnum,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 export const statusEnum = pgEnum("status", workoutStatus);
+
+export const users = pgTable(
+  "users",
+  {
+    id: varchar("id", { length: 255 }).primaryKey(),
+    username: varchar("username", { length: 32 }).unique().notNull(),
+    email: varchar("email", { length: 255 }).unique().notNull(),
+    hashedPassword: varchar("hashed_password", { length: 100 }).notNull(),
+    isVerifiedEmail: boolean("is_verified_email").default(false),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+      mode: "string",
+    }).defaultNow(),
+  },
+  (table) => {
+    return {
+      usernameIndex: index("username_index").on(table.username),
+      emailIndex: index("email_index").on(table.email),
+    };
+  },
+);
+
+export const sessions = pgTable("sessions", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  userId: varchar("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
 
 export const workouts = pgTable(
   "workouts",
@@ -19,8 +52,10 @@ export const workouts = pgTable(
     description: varchar("description", { length: 80 }).notNull(),
     exercises: json("exercises").$type<ExerciseType[]>().notNull(),
     status: statusEnum("status").default("current"),
-    userId: varchar("user_id", { length: 255 }).notNull(),
-    doneAt: timestamp("done_at").defaultNow(),
+    userId: varchar("user_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    doneAt: timestamp("done_at", { mode: "string" }),
     timeElapsed: varchar("time_elapsed", { length: 100 }),
   },
   (table) => {
