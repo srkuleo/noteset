e4c47a0629c0a2468e9ed39d62bb498c04e89fcd
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-05 22:15:54 +0200

    MODIFY: decrease the icons in navbar

--- src/components/icons/user/links.tsx
+++ src/components/icons/user/links.tsx
@@ -5,7 +5,7 @@ export const HomeIcon = (
     viewBox="0 0 24 24"
     strokeWidth={2}
     stroke="currentColor"
-    className="size-6"
+    className="size-[22px]"
   >
     <path
       strokeLinecap="round"
@@ -22,7 +22,7 @@ export const ProfileIcon = (
     viewBox="0 0 24 24"
     strokeWidth={2}
     stroke="currentColor"
-    className="size-6"
+    className="size-[22px]"
   >
     <path
       strokeLinecap="round"
@@ -39,7 +39,7 @@ export const LogsIcon = (
     viewBox="0 0 24 24"
     strokeWidth={2}
     stroke="currentColor"
-    className="size-6"
+    className="size-[22px]"
   >
     <path
       strokeLinecap="round"
--- src/components/icons/user/logout.tsx
+++ src/components/icons/user/logout.tsx
@@ -5,7 +5,7 @@ export const LogoutIcon = (
     viewBox="0 0 24 24"
     strokeWidth={2}
     stroke="currentColor"
-    className="size-6"
+    className="size-[22px]"
   >
     <path
       strokeLinecap="round"
