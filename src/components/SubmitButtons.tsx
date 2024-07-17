704f5c24460a7bc574dd0c4f959b7de9ee0b5667
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-18 00:48:45 +0200

    MODIFY: moves out of user folder since it is used by both user and landing layout

--- src/components/SubmitButtons.tsx
+++ src/components/SubmitButtons.tsx
@@ -4,8 +4,8 @@ import { useFormStatus } from "react-dom";
 import { twMerge } from "tailwind-merge";
 
 type SubmitButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  label: string;
-  loading: string;
+  label: string | React.ReactNode;
+  loading: string | React.ReactNode;
 };
 
 export const SubmitFormButton = ({
@@ -36,7 +36,7 @@ export const ConfirmLogOutButton = () => {
     <button
       type="submit"
       disabled={pending}
-      className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 disabled:dark:text-green-800 disabled:dark:bg-slate-900/75 dark:border-slate-600 active:dark:bg-slate-600/90"
+      className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
     >
       {pending ? "Logging Out..." : "Log Out"}
     </button>
--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import debounce from "lodash.debounce";
 import { toast } from "sonner";
-import { ErrorIcon, SuccessIcon } from "../icons/user/toasts";
+import { ErrorIcon, SuccessIcon } from "./icons/user/toasts";
 
 type ToastType = "error" | "success" | "success-redirect";
 
@@ -9,7 +9,7 @@ export const showToast = debounce(
   (message: string, type: ToastType, path?: string, linkText?: string) =>
     // Accepts 2-4 args
     toast.custom((t) => (
-      <div onClick={() => toast.dismiss(t)} className="pb-safe-bottom">
+      <div onClick={() => toast.dismiss(t)} className="select-none pb-16">
         <Toast message={message} type={type} path={path} linkText={linkText} />
       </div>
     )),
@@ -28,9 +28,7 @@ export const Toast = ({
   linkText?: string;
 }) => {
   return (
-    <div
-      className="flex items-center justify-between gap-2 rounded-lg bg-white p-4 font-manrope text-sm shadow-md ring-[1.5px] dark:bg-slate-900 ring-slate-300/90 dark:ring-slate-800"
-    >
+    <div className="flex items-center justify-between gap-2 rounded-lg bg-white p-4 font-manrope text-sm shadow-lg ring-[1.5px] ring-slate-400/55 dark:bg-slate-900 dark:shadow-black dark:ring-slate-800">
       <div className="flex items-center gap-1.5">
         {type === "success" ? (
           <SuccessIcon />
@@ -40,7 +38,7 @@ export const Toast = ({
 
         <p className="text-slate-800 dark:text-slate-200">{message}</p>
       </div>
-      
+
       {path && linkText && (
         <Link href={path} className="text-center font-bold text-green-500">
           {linkText}
