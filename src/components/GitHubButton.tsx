b01188b38e9c21def5cc041fe8411fe2810c734e
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-18 00:45:26 +0200

    MODIFY: moves out of landing page folder since it is used by both user and landing layout

--- src/components/GitHubButton.tsx
+++ src/components/GitHubButton.tsx
@@ -1,4 +1,4 @@
-import { GitHubIcon } from "../icons/landing/github";
+import { GitHubIcon } from "./icons/github";
 
 export const GitHubButton = () => {
   return (
@@ -6,6 +6,7 @@ export const GitHubButton = () => {
       href="https://github.com/srkuleo/noteset"
       target="_blank"
       rel="noreferrer"
+      className="dark:text-slate-400"
     >
       {GitHubIcon}
       <p className="sr-only">Visit project on GitHub</p>
