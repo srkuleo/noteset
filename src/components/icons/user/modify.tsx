44e718a7452a3cc980d44870a1c3b2a27a964234
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-18 00:59:26 +0200

    MODIFY: adds icon that indicates exercise is draggable inside sort list

--- src/components/icons/user/modify.tsx
+++ src/components/icons/user/modify.tsx
@@ -58,6 +58,23 @@ export const RemoveWorkoutIcon = (
   </svg>
 );
 
+export const DragExerciseIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.8}
+    stroke="currentColor"
+    className="size-[18px]"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+    />
+  </svg>
+);
+
 export const EditExerciseIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -80,7 +97,7 @@ export const RemoveExerciseIcon = (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.8}
+    strokeWidth={2.5}
     stroke="currentColor"
     className="size-[14px]"
   >
