f0ca6c4811c92e583628de6c72d25d2e690ad811
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-18 01:37:10 +0200

    MODIFY: adds noBodyStyles to avoid body layout shift, small UI changes, add z-index 9999 to make sure it always renders on top of everything

--- src/components/user/RemoveExerciseModal.tsx
+++ src/components/user/RemoveExerciseModal.tsx
@@ -6,7 +6,7 @@ export const RemoveExerciseModal = ({
   exerciseName,
   setIsOpen,
   removeExercise,
-  isEditWorkoutPage,
+  isEditWorkoutPage = false,
   openEditDrawer,
 }: {
   isOpen: boolean;
@@ -17,26 +17,23 @@ export const RemoveExerciseModal = ({
   openEditDrawer?: () => void;
 }) => {
   return (
-    <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
+    <Drawer.Root open={isOpen} onOpenChange={setIsOpen} noBodyStyles>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
+        <Drawer.Overlay className="fixed inset-0 z-[9999] bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
 
         <Drawer.Content
           aria-describedby={undefined}
           data-vaul-no-drag
-          className="fixed inset-x-0 bottom-0 select-none space-y-4 px-4 pb-12 focus:outline-none"
+          className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
         >
           <div className="flex flex-col gap-3 rounded-modal bg-slate-50/90 pt-5 dark:bg-slate-700/70">
             <div className="mx-auto w-fit rounded-full bg-red-400 p-2 text-white shadow-sm dark:bg-red-200 dark:text-red-500">
               {DangerIcon}
             </div>
 
-            <Drawer.Title className="px-2 pt-2 text-center font-nunito text-sm leading-snug text-slate-600 dark:text-slate-400">
-              Are you sure you want to remove{" "}
-              <span className="font-semibold text-slate-700 dark:text-slate-100">
-                {exerciseName}
-              </span>{" "}
-              exercise from the current list? This action is irreversible.
+            <Drawer.Title className="px-2 pt-2 text-center font-nunito text-sm font-semibold leading-snug dark:text-slate-400">
+              Are you sure you want to remove this exercise from the current
+              list? This action is irreversible.
             </Drawer.Title>
 
             <div className="flex flex-col">
@@ -51,7 +48,7 @@ export const RemoveExerciseModal = ({
                   Modify {exerciseName}
                 </button>
               )}
-              
+
               <button
                 onClick={() => {
                   removeExercise();
