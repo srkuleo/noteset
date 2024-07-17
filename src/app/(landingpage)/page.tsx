4e975a9cf3b737742ce91e4da5f5aae4b27aa833
Srdjan <milicevic.srdjan0@gmail.com>
2024-07-17 18:45:03 +0200

    MODIFY: small UI modifications, now properly centers main content

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import { getAuth } from "@/util/actions/auth";
 import { TooltipDrawer } from "@/components/landing/TooltipDrawer";
-import { ArrowIcon } from "@/components/icons/landing/arrow";
+import { ArrowRightIcon } from "@/components/icons/arrows";
 
 export default async function LandingPage() {
   const { user } = await getAuth();
@@ -12,8 +12,8 @@ export default async function LandingPage() {
   }
 
   return (
-    <>
-      <h1 className="mx-auto bg-gradient-to-r from-green-400 to-violet-500 bg-clip-text pb-6 text-7xl font-extrabold text-transparent dark:to-violet-600">
+    <div className="flex h-full flex-col items-center justify-center pb-28 md:pb-36">
+      <h1 className="bg-gradient-to-r from-green-400 to-violet-500 bg-clip-text pb-6 text-7xl font-extrabold text-transparent dark:to-violet-600">
         Note<span className="font-bold">Set</span>
       </h1>
 
@@ -25,15 +25,15 @@ export default async function LandingPage() {
       <div className="flex justify-center gap-4">
         <TooltipDrawer />
 
-        <div className="w-[1px] bg-slate-300/50 dark:bg-slate-700/80" />
+        <div className="w-[1px] bg-slate-300 dark:bg-slate-700/80" />
 
         <Link
           href="/login"
           className="flex items-center gap-1 rounded-full px-4 py-2 font-manrope font-semibold text-slate-600 active:bg-slate-200 dark:text-slate-300 active:dark:bg-slate-800"
         >
-          Join {ArrowIcon}
+          Join {ArrowRightIcon}
         </Link>
       </div>
-    </>
+    </div>
   );
 }
