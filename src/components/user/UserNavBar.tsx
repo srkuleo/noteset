df7eb74406d7b9238b8e5642dca6a222fbd41d43
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-05 23:25:46 +0200

    MODIFY: final navbar look

--- src/components/user/UserNavBar.tsx
+++ src/components/user/UserNavBar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 import { Drawer } from "vaul";
 import { twJoin } from "tailwind-merge";
 import { logout } from "@/util/actions/auth";
@@ -30,21 +31,24 @@ export const UserPageNavBar = () => {
   const path = usePathname();
 
   return (
-    <nav className="flex justify-center gap-4 border-t border-slate-300/80 pb-7 pt-2 text-slate-400 transition dark:border-slate-800 dark:text-slate-500">
+    <nav className="flex justify-center gap-3 border-t border-slate-300/80 pb-7 text-slate-400 dark:border-slate-800 dark:text-slate-500">
       {navBarLinks.map((link) => (
         <Link
           key={link.href}
           href={link.href}
           className={twJoin(
-            "flex flex-col items-center justify-center gap-0.5 px-2 transition",
-            path.includes(link.href) && "text-green-500",
+            "relative px-2 py-2 active:rounded-full active:bg-slate-200 dark:active:bg-slate-700",
+            path.includes(link.href) && "text-green-500 dark:text-white",
           )}
         >
           {link.icon}
 
-          <p className="font-manrope text-[10px] font-extrabold uppercase">
-            {link.label}
-          </p>
+          {path.includes(link.href) && (
+            <motion.div
+              layoutId="active-page"
+              className="absolute bottom-0 h-[3px] w-8 rounded-full bg-green-500"
+            />
+          )}
 
           <p className="sr-only">{link.label} button</p>
         </Link>
@@ -60,11 +64,9 @@ export const LogoutButton = () => {
 
   return (
     <Drawer.Root open={open} onOpenChange={setOpen} noBodyStyles>
-      <Drawer.Trigger className="flex flex-col items-center justify-center gap-0.5 transition">
+      <Drawer.Trigger className="px-2 py-2 active:rounded-full active:bg-slate-200 dark:active:bg-slate-700">
         {LogoutIcon}
-        <span className="font-manrope text-[10px] font-extrabold uppercase">
-          Log out
-        </span>
+
         <p className="sr-only">Logout button</p>
       </Drawer.Trigger>
 
