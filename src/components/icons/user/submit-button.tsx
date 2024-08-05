d9f8d5b2ef38836b3c7a21dfffe63348853bc340
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-05 23:23:27 +0200

    MODIFY: turns CheckmarkIcon into RFC, needed an option to modify className

--- src/components/icons/user/submit-button.tsx
+++ src/components/icons/user/submit-button.tsx
@@ -1,19 +1,21 @@
-export const CheckmarkIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.8}
-    stroke="currentColor"
-    className="size-[22px]"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="m4.5 12.75 6 6 9-13.5"
-    />
-  </svg>
-);
+export const CheckmarkIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.8}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 12.75 6 6 9-13.5"
+      />
+    </svg>
+  );
+};
 
 export const ThreeDotIcon = (
   <svg
