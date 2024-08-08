06a70372a2d333660bc2c04e12965b871b71149c
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-08 15:20:02 +0200

    MODIFY: new, revamped design for dropdown menu

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,10 +2,11 @@
 
 import { useState } from "react";
 import { useTheme } from "next-themes";
-import { AnimatePresence, motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import * as Dialog from "@radix-ui/react-dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { ThemeIcon, SystemIcon, MoonIcon, SunIcon } from "./icons/theme";
 import { CheckmarkIcon } from "./icons/user/submit-button";
-import { twMerge } from "tailwind-merge";
 
 type ThemeOptions = "system" | "dark" | "light";
 
@@ -19,71 +20,40 @@ export const ThemeButton = () => {
   }
 
   return (
-    <>
-      <button onClick={() => setOpen(!open)} className="dark:text-slate-400">
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger className="focus:outline-none dark:text-slate-400">
         {ThemeIcon}
         <p className="sr-only">Toggle theme</p>
-      </button>
+      </Dialog.Trigger>
 
-      <AnimatePresence>
-        {open && (
-          <motion.div
-            key="overlay"
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-              transition: { duration: 0.4, ease: [0.36, 0.66, 0.04, 1] },
-            }}
-            exit={{
-              opacity: 0,
-              transition: {
-                delay: 0.1,
-                duration: 0.15,
-                ease: [0.36, 0.66, 0.04, 1],
-              },
-            }}
-            onClick={() => setOpen(false)}
-            className="fixed inset-0 z-10 bg-slate-700/15 backdrop-blur-sm dark:bg-slate-950/15"
-          />
-        )}
-      </AnimatePresence>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          onClick={() => setOpen(false)}
+          className="data-[state=open]:animate-overlay-show data-[state=closed]:animate-overlay-hide fixed inset-0 z-10 bg-slate-700/15 backdrop-blur-sm dark:bg-slate-950/15"
+        />
 
-      <AnimatePresence>
-        {open && (
-          <motion.div
-            key="dropdown-menu"
-            initial={{ opacity: 0, scale: 0, originX: 1, originY: 0 }}
-            animate={{
-              opacity: 1,
-              scale: 1,
-              originX: 1,
-              originY: 0,
-              transition: { duration: 0.4, ease: [0.36, 0.66, 0.04, 1] },
-            }}
-            exit={{
-              opacity: 0,
-              scale: 0,
-              originX: 1,
-              originY: 0,
-              transition: {
-                delay: 0.1,
-                duration: 0.15,
-                ease: [0.36, 0.66, 0.04, 1],
-              },
-            }}
-            className="translate absolute right-16 top-14 z-10 flex w-48 origin-top-right flex-col rounded-modal bg-white ring-1 ring-slate-300 dark:bg-slate-800 dark:text-slate-400 dark:ring-slate-700"
-          >
+        <Dialog.Content
+          aria-describedby={undefined}
+          onOpenAutoFocus={(e) => {
+            e.preventDefault();
+          }}
+          className="data-[state=open]:animate-dropdown-menu-scale-up data-[state=closed]:animate-dropdown-menu-scale-down absolute right-20 top-16 z-10 pt-safe-top"
+        >
+          <VisuallyHidden asChild>
+            <Dialog.Title>Choose a colour theme</Dialog.Title>
+          </VisuallyHidden>
+
+          <div className="flex w-48 flex-col rounded-modal bg-white/60 ring-1 ring-slate-400/70 dark:bg-slate-800/75 dark:text-slate-400 dark:ring-slate-700">
             <button
               onClick={() => chooseTheme("system")}
               className={twMerge(
-                "flex items-center justify-between rounded-t-modal p-2 active:bg-slate-200 active:dark:bg-slate-800",
-                theme === "system" && "bg-slate-100/80 dark:bg-slate-900/70",
+                "flex items-center gap-2 rounded-t-modal p-2 active:bg-slate-200 active:dark:bg-slate-800 [&>*:nth-child(2)]:mr-auto",
+                theme === "system" && "bg-white dark:bg-slate-900",
               )}
             >
-              <div className="flex items-center gap-2">
-                {SystemIcon}
-                <p className="text-sm font-semibold uppercase">System</p>
-              </div>
+              {SystemIcon}
+              <p className="text-sm font-semibold uppercase">System</p>
+
               {theme === "system" && (
                 <CheckmarkIcon className="size-5 text-green-500" />
               )}
@@ -93,14 +63,13 @@ export const ThemeButton = () => {
             <button
               onClick={() => chooseTheme("dark")}
               className={twMerge(
-                "flex items-center justify-between border-y border-slate-300/50 p-2 active:bg-slate-200 dark:border-slate-700/80 active:dark:bg-slate-800",
-                theme === "dark" && "bg-slate-100/80 dark:bg-slate-900/70",
+                "flex items-center gap-2 border-y border-slate-300/50 p-2 active:bg-slate-200 dark:border-slate-700/80 active:dark:bg-slate-800 [&>*:nth-child(2)]:mr-auto",
+                theme === "dark" && "dark:bg-slate-900",
               )}
             >
-              <div className="flex items-center gap-2">
-                {MoonIcon}
-                <p className="text-sm font-semibold uppercase">Dark</p>
-              </div>
+              {MoonIcon}
+              <p className="text-sm font-semibold uppercase">Dark</p>
+
               {theme === "dark" && (
                 <CheckmarkIcon className="size-5 text-green-500" />
               )}
@@ -110,22 +79,21 @@ export const ThemeButton = () => {
             <button
               onClick={() => chooseTheme("light")}
               className={twMerge(
-                "flex items-center justify-between rounded-b-modal p-2 active:bg-slate-200 active:dark:bg-slate-800",
-                theme === "light" && "bg-slate-100/80 dark:bg-slate-900/70",
+                "flex items-center gap-2 rounded-b-modal p-2 active:bg-slate-200 active:dark:bg-slate-800 [&>*:nth-child(2)]:mr-auto",
+                theme === "light" && "bg-white",
               )}
             >
-              <div className="flex items-center gap-2">
-                {SunIcon}
-                <p className="text-sm font-semibold uppercase">Light</p>
-              </div>
+              {SunIcon}
+              <p className="text-sm font-semibold uppercase">Light</p>
+
               {theme === "light" && (
                 <CheckmarkIcon className="size-5 text-green-500" />
               )}
               <p className="sr-only">Light theme</p>
             </button>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
