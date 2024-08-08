41cee8aa034ad24f97bf68f3674e24f29a8b084c
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-08 15:25:46 +0200

    MODIFY: doesn't accept isPending prop anymore, pending state and UI handled through fieldset and group tailwind utility

--- src/components/user/WorkoutFormHeader.tsx
+++ src/components/user/WorkoutFormHeader.tsx
@@ -8,40 +8,42 @@ import type { ExerciseType } from "@/util/types";
 
 export const WorkoutFormHeader = ({
   heading,
-  isPending,
   formId,
   updateExercises,
 }: {
   heading: string;
-  isPending: boolean;
   formId: string;
   updateExercises: (newExercise: ExerciseType) => void;
 }) => {
   const [openAddDrawer, setOpenAddDrawer] = useState(false);
 
   return (
-    <div className="flex items-center gap-2.5 border-b border-slate-300 px-6 py-4 dark:border-slate-800 [&>*:nth-child(2)]:mr-auto">
-      <Link href="/workouts" className="text-slate-400 dark:text-slate-500">
+    <div className="flex items-center gap-3 border-b border-slate-300 px-6 py-4 dark:border-slate-800">
+      <Link
+        href="/workouts"
+        className="text-slate-400 group-disabled:pointer-events-none group-disabled:opacity-50 dark:text-slate-500"
+      >
         {ArrowLeftIcon}
       </Link>
 
       <h2 className="text-2xl">{heading}</h2>
 
-      <AddExerciseDrawer
-        isOpen={openAddDrawer}
-        isPending={isPending}
-        setIsOpen={setOpenAddDrawer}
-        updateExercises={updateExercises}
-      />
+      <div className="flex flex-1 items-center justify-end gap-3 group-disabled:pointer-events-none group-disabled:opacity-50">
+        <AddExerciseDrawer
+          isOpen={openAddDrawer}
+          setIsOpen={setOpenAddDrawer}
+          updateExercises={updateExercises}
+        />
 
-      <button
-        type="submit"
-        form={formId}
-        disabled={isPending}
-        className="rounded-full bg-green-500 p-2 text-white shadow-sm active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:bg-green-600"
-      >
-        {isPending ? ThreeDotIcon : <CheckmarkIcon className="size-[22px]" />}
-      </button>
+        <button
+          type="submit"
+          form={formId}
+          className="inline-flex rounded-full bg-green-500 p-2 text-white shadow-sm active:scale-95 dark:bg-green-600"
+        >
+          <CheckmarkIcon className="size-[22px] group-disabled:opacity-0" />
+          <ThreeDotIcon className="absolute size-[22px] group-enabled:opacity-0" />
+        </button>
+      </div>
     </div>
   );
 };
