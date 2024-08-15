3ae17922079aa3ce83039a1bc6a8272559edee3c
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-15 04:24:20 +0200

    MODIFY: refactoring, easier to read and now has a button to close the modal on success and error toast types

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -5,45 +5,46 @@ import { ErrorIcon, SuccessIcon } from "./icons/user/toasts";
 
 type ToastType = "error" | "success" | "success-redirect";
 
+// Accepts 2-4 args
+// - message to be rendered inside toast
+// - type of toast (error, success, redirect) - different colours and design
+// - path to which redirect button redirects user after clicking
+// -linkText - text to be rendered as a redirect button
 export const showToast = debounce(
   (message: string, type: ToastType, path?: string, linkText?: string) =>
-    // Accepts 2-4 args
     toast.custom((t) => (
-      <div onClick={() => toast.dismiss(t)} className="select-none pb-16">
-        <Toast message={message} type={type} path={path} linkText={linkText} />
+      <div className="select-none pb-3">
+        <div className="flex items-center justify-between gap-2 rounded-lg bg-white p-4 shadow-lg ring-[1.5px] ring-slate-400/55 dark:bg-slate-900 dark:shadow-black dark:ring-slate-800">
+          <div className="flex items-center gap-1.5">
+            {type === "success" ? (
+              <SuccessIcon />
+            ) : type === "error" ? (
+              <ErrorIcon />
+            ) : null}
+
+            <p className="font-manrope text-sm text-slate-800 dark:text-slate-200">
+              {message}
+            </p>
+          </div>
+
+          {path && linkText ? (
+            <Link
+              href={path}
+              onClick={() => toast.dismiss(t)}
+              className="text-center font-bold text-green-500"
+            >
+              {linkText}
+            </Link>
+          ) : (
+            <button
+              onClick={() => toast.dismiss(t)}
+              className="text-center font-bold text-green-500"
+            >
+              Close
+            </button>
+          )}
+        </div>
       </div>
     )),
   250,
 );
-
-export const Toast = ({
-  message,
-  type,
-  path,
-  linkText,
-}: {
-  message: string;
-  type: ToastType;
-  path?: string;
-  linkText?: string;
-}) => {
-  return (
-    <div className="flex items-center justify-between gap-2 rounded-lg bg-white p-4 font-manrope text-sm shadow-lg ring-[1.5px] ring-slate-400/55 dark:bg-slate-900 dark:shadow-black dark:ring-slate-800">
-      <div className="flex items-center gap-1.5">
-        {type === "success" ? (
-          <SuccessIcon />
-        ) : type === "error" ? (
-          <ErrorIcon />
-        ) : null}
-
-        <p className="text-slate-800 dark:text-slate-200">{message}</p>
-      </div>
-
-      {path && linkText && (
-        <Link href={path} className="text-center font-bold text-green-500">
-          {linkText}
-        </Link>
-      )}
-    </div>
-  );
-};
