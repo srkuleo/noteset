f0c09bedcf8125598a67701ade7cd9bbe5f9931c
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-15 04:19:13 +0200

    MODIFY:  adds color schema meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export const viewport: Viewport = {
     { media: "(prefers-color-scheme: dark)", color: "#0f172a" },
     { media: "(prefers-color-scheme: light)", color: "#ffffff" },
   ],
+  colorScheme: "dark light",
 };
 
 export const metadata: Metadata = {
