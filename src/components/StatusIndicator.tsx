34af142c3bbfcb70295f000bcbe999b0f21e9499
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-15 05:35:52 +0200

    MODIFY: finetune colours for status dot indicator

--- src/components/StatusIndicator.tsx
+++ src/components/StatusIndicator.tsx
@@ -4,7 +4,7 @@ export const StatusIndicator = ({ status }: Pick<WorkoutType, "status">) => {
   return (
     <div className="flex items-center gap-1 rounded-full px-3 py-0.5 shadow-md ring-1 ring-inset ring-slate-300 dark:shadow-slate-900 dark:ring-slate-600">
       <div
-        className={`${status === "current" ? "bg-blue-400 dark:bg-blue-500" : status === "done" ? "bg-green-500" : "bg-slate-800"} size-2 rounded-full`}
+        className={`${status === "current" ? "bg-blue-400 dark:bg-blue-500" : status === "done" ? "bg-green-500 dark:bg-green-600" : "bg-slate-500 dark:bg-slate-600"} size-2 rounded-full`}
       />
 
       <p className="font-nunito text-[8px] font-semibold uppercase">{status}</p>
