d3b45a17d8d36798f7faa78009d9fd527ca741c0
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-15 04:25:22 +0200

    MODIFY: changes name to CheckMarkIcon for better readability and to match QuestionMarkIcon

--- src/components/icons/user/submit-button.tsx
+++ src/components/icons/user/submit-button.tsx
@@ -1,4 +1,4 @@
-export const CheckmarkIcon = ({ className }: { className: string }) => {
+export const CheckMarkIcon = ({ className }: { className: string }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
