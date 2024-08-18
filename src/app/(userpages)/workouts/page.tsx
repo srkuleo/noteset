86b48f3bba6717113815124a40bd96d036ae18f4
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-18 03:37:06 +0200

    MODIFY:  extracts workouts skeleton since it is now used by both workouts and logs pages

--- src/app/(userpages)/workouts/page.tsx
+++ src/app/(userpages)/workouts/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 import { UserPagesHeadingText } from "@/components/user/UserPagesHeadingText";
 import { AddIcon } from "@/components/icons/user/modify";
+import { LoadingWorkoutsSkeleton } from "@/components/user/LoadingWorkoutsSkeleton";
 import { WorkoutsPageContent } from "@/components/user/WorkoutsPageContent";
 
 import type { Metadata } from "next";
@@ -33,30 +34,3 @@ export default async function WorkoutsPage() {
     </>
   );
 }
-
-const LoadingWorkoutsSkeleton = () => {
-  return [1, 2, 3, 4].map((skeleton) => (
-    <div
-      key={skeleton}
-      className="flex w-full flex-col gap-4 rounded-xl bg-white px-4 py-6 shadow-md dark:bg-slate-800/90"
-    >
-      <div className="space-y-1 px-1">
-        <div className="h-7 w-24 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-900/80" />
-        <div className="h-4 w-52 animate-pulse rounded-lg bg-slate-100 dark:bg-slate-900/50" />
-      </div>
-
-      <div className="h-[1px] bg-green-200 dark:bg-green-900/70" />
-
-      <div className="flex justify-between px-1">
-        <div className="flex gap-2">
-          <div className="h-8 w-9 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-900/80" />
-          <div className="h-8 w-18 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-900/80" />
-        </div>
-        <div className="flex items-center gap-4">
-          <div className="size-6 animate-pulse rounded-full bg-slate-200 dark:bg-slate-900/50" />
-          <div className="size-5 animate-pulse rounded-full bg-slate-200 dark:bg-slate-900/50" />
-        </div>
-      </div>
-    </div>
-  ));
-};
