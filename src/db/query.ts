90dc375e37b13ef5c96b0c050be0014d56a73964
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-18 03:53:09 +0200

    MODIFY: queries now get user id from getAuth function, adds getUserDoneWorkouts and getWorkoutByIdWithoutId queries

--- src/db/query.ts
+++ src/db/query.ts
@@ -1,10 +1,17 @@
 "use server";
 
-import { and, eq } from "drizzle-orm";
+import { and, desc, eq } from "drizzle-orm";
 import { db } from ".";
 import { workouts } from "./schema";
+import { getAuth } from "@/util/actions/auth";
 
-export async function getUserWorkouts(userId: string) {
+export async function getUserWorkouts() {
+  const { user } = await getAuth();
+
+  if (!user) {
+    throw new Error("Unauthorized action. Please login.");
+  }
+
   try {
     const userWorkouts = await db
       .select({
@@ -15,7 +22,7 @@ export async function getUserWorkouts(userId: string) {
         status: workouts.status,
       })
       .from(workouts)
-      .where(and(eq(workouts.userId, userId), eq(workouts.status, "current")))
+      .where(and(eq(workouts.userId, user.id), eq(workouts.status, "current")))
       .orderBy(workouts.id);
 
     console.log("Workouts fetched.");
@@ -27,12 +34,75 @@ export async function getUserWorkouts(userId: string) {
   }
 }
 
-export async function getWorkoutById(workoutId: number, userId: string) {
+export async function getUserDoneWorkouts() {
+  const { user } = await getAuth();
+
+  if (!user) {
+    throw new Error("Unauthorized action. Please login.");
+  }
+
+  try {
+    const userWorkouts = await db
+      .select({
+        id: workouts.id,
+        title: workouts.title,
+        description: workouts.description,
+        exercises: workouts.exercises,
+        status: workouts.status,
+        doneAt: workouts.doneAt,
+        duration: workouts.duration,
+      })
+      .from(workouts)
+      .where(and(eq(workouts.userId, user.id), eq(workouts.status, "done")))
+      .orderBy(desc(workouts.id));
+
+    console.log("Done workouts fetched.");
+
+    return userWorkouts;
+  } catch (error) {
+    console.error("Database error:", error);
+    throw new Error("Failed to retrive user's workouts.");
+  }
+}
+
+export async function getWorkoutById(workoutId: number) {
+  const { user } = await getAuth();
+
+  if (!user) {
+    throw new Error("Unauthorized action. Please login.");
+  }
+
   try {
     const workout = await db.query.workouts.findFirst({
-      where: and(eq(workouts.userId, userId), eq(workouts.id, workoutId)),
+      where: and(eq(workouts.userId, user.id), eq(workouts.id, workoutId)),
       columns: {
         id: true,
+        title: true,
+        description: true,
+        exercises: true,
+      },
+    });
+
+    console.log("Workout retrived.");
+
+    return workout;
+  } catch (error) {
+    console.error("Database error:", error);
+    throw new Error("Failed to retrive selected workout.");
+  }
+}
+
+export async function getWorkoutByIdWithoutId(workoutId: number) {
+  const { user } = await getAuth();
+
+  if (!user) {
+    throw new Error("Unauthorized action. Please login.");
+  }
+
+  try {
+    const workout = await db.query.workouts.findFirst({
+      where: and(eq(workouts.userId, user.id), eq(workouts.id, workoutId)),
+      columns: {
         title: true,
         description: true,
         exercises: true,
