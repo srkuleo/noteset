f76e048d93ebf8a8624f0beed8c542bb50b9ebec
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-18 03:46:53 +0200

    ADD: logs server component for checking authentication and querying db

---
+++ src/components/user/LogsPageContent.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+import { getAuth } from "@/util/actions/auth";
+import { getUserDoneWorkouts } from "@/db/query";
+import { DoneWorkoutCards } from "./DoneWorkoutCards";
+
+export const LogsPageContent = async () => {
+  const { user } = await getAuth();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  const doneWorkouts = await getUserDoneWorkouts();
+
+  return <DoneWorkoutCards doneWorkouts={doneWorkouts} />;
+};
