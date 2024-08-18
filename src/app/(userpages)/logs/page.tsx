2be6efb9db2d85c65e68a2d559877963653ba461
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-18 03:36:18 +0200

    MODIFY: WIP getting UI layed out

--- src/app/(userpages)/logs/page.tsx
+++ src/app/(userpages)/logs/page.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from "react";
 import { redirect } from "next/navigation";
 import { getAuth } from "@/util/actions/auth";
 import { UserPagesHeadingText } from "@/components/user/UserPagesHeadingText";
+import { LoadingWorkoutsSkeleton } from "@/components/user/LoadingWorkoutsSkeleton";
+import { LogsPageContent } from "@/components/user/LogsPageContent";
 
 import type { Metadata } from "next";
 
@@ -21,7 +24,13 @@ export default async function LogsPage() {
         <UserPagesHeadingText label="Logs" />
       </div>
 
-      <main className="px-6 py-2"></main>
+      {/* Add search bar and filter button */}
+
+      <main className="space-y-4 overflow-y-auto overscroll-contain scroll-smooth px-6 py-4">
+        <Suspense fallback={<LoadingWorkoutsSkeleton />}>
+          <LogsPageContent />
+        </Suspense>
+      </main>
     </>
   );
 }
