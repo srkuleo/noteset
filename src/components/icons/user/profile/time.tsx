f224244322ea95a12eef1f692dc4db5850ce8881
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-20 03:44:45 +0200

    ADD: time icon, part of time format button

---
+++ src/components/icons/user/profile/time.tsx
@@ -0,0 +1,16 @@
+export const TimeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.7}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
