308a741d8ccacbc8e80037f8fcc091c8157b841a
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-20 03:37:41 +0200

    MODIFY: UI improvements, add user profile dropdown menu with preferences and log out button

--- src/app/(userpages)/profile/page.tsx
+++ src/app/(userpages)/profile/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { getAuth } from "@/util/actions/auth";
 import { UserPagesHeadingText } from "@/components/user/UserPagesHeadingText";
+import { ProfileDropDownMenu } from "@/components/user/profile/ProfileDropDownMenu";
 
 import type { Metadata } from "next";
 
@@ -15,19 +16,42 @@ export default async function ProfilePage() {
     redirect("/login");
   }
 
-  const userJoined = user.createdAt.substring(0, 10);
-
   return (
     <>
-      <div className="border-b border-slate-300/80 px-6 py-4 dark:border-slate-800">
+      <div className="flex justify-between border-b border-slate-300/80 px-6 py-4 dark:border-slate-800">
         <UserPagesHeadingText label="Profile page" />
+
+        <ProfileDropDownMenu preferences={user.preferences} />
       </div>
 
-      <main className="space-y-4 px-6 py-2">
-        <p className="">Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Verified: {user.isVerified ? "Yes" : "No"}</p>
-        <p>Joined: {userJoined}</p>
+      <main className="space-y-4">
+        {!user.isVerified && (
+          <div className="flex justify-center gap-2 bg-amber-400 py-2 dark:bg-amber-600">
+            <p className="text-lg text-white">You are not verified.</p>
+            <button className="text-sm text-violet-500 dark:text-violet-800">
+              Send verification email
+            </button>
+          </div>
+        )}
+
+        <div className="space-y-8 px-6 pb-4">
+          <div>
+            <p className="font-manrope text-3xl font-semibold">
+              {user.username}
+            </p>
+            <p className="italic">{user.email}</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="font-semibold dark:text-slate-300">Member since:</p>
+            <p className="font-bold">
+              {user.createdAt
+                .toLocaleDateString()
+                .replaceAll(". ", "-")
+                .replace(".", "")}
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
