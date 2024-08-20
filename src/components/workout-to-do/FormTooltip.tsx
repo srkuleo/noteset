2b5cf28dcb6c6c12981a59d9a15b7388ed139f8b
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-20 03:53:43 +0200

    MODIFY: small improvements regarding UI

--- src/components/workout-to-do/FormTooltip.tsx
+++ src/components/workout-to-do/FormTooltip.tsx
@@ -15,22 +15,17 @@ export const FormTooltip = () => {
           setOpen(true);
         }}
         className={twMerge(
-          "absolute right-11 top-[26px] rounded-full p-1 active:bg-white dark:active:bg-slate-700",
+          "mr-1.5 rounded-full p-1.5 active:bg-slate-300/60 dark:active:bg-slate-300/50",
           open && "z-20",
         )}
       >
-        <QuestionMarkIcon
-          className={twMerge(
-            "size-6 text-slate-400/80 dark:text-slate-500",
-            open && "text-white dark:text-white",
-          )}
-        />
+        <QuestionMarkIcon className="size-7 text-white" />
       </button>
 
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-10 bg-slate-700/45 backdrop-blur-sm data-[state=closed]:animate-overlay-hide data-[state=open]:animate-overlay-show dark:bg-slate-950/45" />
 
-        <Dialog.Content className="absolute right-[50px] top-[204px] z-10 max-w-[75%] pt-safe-top data-[state=closed]:animate-dropdown-menu-scale-down data-[state=open]:animate-dropdown-menu-scale-up sm:max-w-sm">
+        <Dialog.Content className="absolute right-8 top-[52px] z-10 max-w-[75%] pt-safe-top data-[state=closed]:animate-dropdown-menu-scale-down data-[state=open]:animate-dropdown-menu-scale-up sm:max-w-sm">
           <div className="flex flex-col gap-4 rounded-modal bg-white/75 p-6 ring-1 ring-slate-400/80 dark:bg-slate-800/85 dark:text-slate-400 dark:ring-slate-700">
             <Dialog.Title className="text-lg text-slate-800 dark:text-white sm:text-xl">
               Good to Know
@@ -40,34 +35,49 @@ export const FormTooltip = () => {
               <ul className="list-disc space-y-2 text-pretty pl-4 text-sm sm:text-base">
                 <li>
                   Tap{" "}
-                  <span className="font-bold uppercase text-violet-400 dark:text-violet-400">
+                  <span className="font-bold uppercase text-green-500 dark:text-green-600">
                     plus
                   </span>{" "}
                   button to add a new exercise.
                 </li>
 
                 <li>
-                  When done with adding or editing your workout, tap{" "}
+                  <span className="font-bold text-slate-800 dark:text-white">
+                    You don&apos;t have to populate all the exercise fields
+                  </span>
+                  , only those that you have done.
+                </li>
+
+                <li>
+                  <span className="font-bold uppercase text-slate-800 dark:text-white">
+                    Add set
+                  </span>{" "}
+                  button allows you to add additional set for the selected
+                  exercise.
+                </li>
+
+                <li>
+                  <span className="font-bold uppercase text-slate-800 dark:text-white">
+                    Add note
+                  </span>{" "}
+                  button lets you leave a note regarding selected exercise.
+                </li>
+
+                <li>
+                  When done with working out, tap{" "}
                   <span className="font-bold uppercase text-green-500 dark:text-green-600">
-                    check mark
+                    Done
                   </span>{" "}
                   button to submit the form.
                 </li>
 
                 <li>
-                  Fields with &quot;e.g.&quot; or &quot;Rep 1, Weight 1&quot;
-                  don&apos;t carry any value.{" "}
+                  Workout duration is being{" "}
                   <span className="font-bold text-slate-800 dark:text-white">
-                    Those must be provided with data.
-                  </span>
-                </li>
-
-                <li>
-                  You need to add{" "}
-                  <span className="font-bold uppercase text-slate-800 dark:text-white">
-                    at least one exercise
+                    tracked in the background.
                   </span>{" "}
-                  before submitting the form otherwise it will result in error.
+                  You will be able to see the exact duration on the post-workout
+                  page.
                 </li>
               </ul>
             </Dialog.Description>
