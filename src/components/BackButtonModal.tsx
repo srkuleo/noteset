cef42a012748bfdd43e48a1e2ec3b5b4e6891ef2
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-20 03:39:11 +0200

    MODIFY: reusable back button needed for any page containing form, prevents from accidentally going back without warning losing all the form data

---
+++ src/components/BackButtonModal.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { useState } from "react";
+import { Drawer } from "vaul";
+import { ArrowLeftIcon } from "./icons/arrows";
+
+export const BackButtonModal = ({ className }: { className: string }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Drawer.Root open={open} onOpenChange={setOpen} noBodyStyles>
+      <button
+        type="button"
+        onClick={async () => {
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          setOpen(true);
+        }}
+        className={className}
+      >
+        {ArrowLeftIcon}
+
+        <p className="sr-only">Logout button</p>
+      </button>
+
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 z-[9999] bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
+
+        <Drawer.Content
+          aria-describedby={undefined}
+          data-vaul-no-drag
+          className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
+        >
+          <div className="flex flex-col gap-3 rounded-modal bg-white/90 text-center dark:bg-slate-700/70">
+            <Drawer.Title className="px-2 pb-2 pt-5 text-sm font-semibold">
+              Are you sure you want to leave?
+            </Drawer.Title>
+
+            <Link
+              href="/workouts"
+              className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
+            >
+              Back to Home page
+            </Link>
+          </div>
+
+          <button
+            type="button"
+            onClick={async () => {
+              await new Promise((resolve) => setTimeout(resolve, 100));
+              setOpen(false);
+            }}
+            className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
+          >
+            Cancel
+          </button>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  );
+};
