a58d52875dbe31cd552e3ba007a7ff8134fed5e2
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-20 04:02:54 +0200

    MODIFY: small UX changes, move to different folder

--- src/components/workout-to-do/WorkoutToDoForm.tsx
+++ src/components/workout-to-do/WorkoutToDoForm.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-import Link from "next/link";
 import debounce from "lodash.debounce";
 import { useState } from "react";
+import { redirect } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { Drawer } from "vaul";
 import { submitDoneWorkout } from "@/util/actions/workout";
-import { ArrowLeftIcon } from "../icons/arrows";
-import { AddExerciseDrawer } from "../user/AddExerciseDrawer";
-import { AddIcon } from "../icons/user/modify";
+import { BackButtonModal } from "../BackButtonModal";
 import { FormTooltip } from "./FormTooltip";
+import { AddIcon } from "../icons/user/modify";
+import { AddExerciseDrawer } from "../user/AddExerciseDrawer";
 
 import type { CreateWorkoutType, ExerciseType } from "@/util/types";
 import { ThemeButton } from "../ThemeButton";
-import { redirect } from "next/navigation";
 
 type TimeType = { start: Date | null; end: Date | null };
 type NoteType = { add: boolean; onExercise: string };
@@ -96,18 +95,6 @@ export const WorkoutToDoForm = ({
   );
 
   function addNewSet(exerciseId: string) {
-    const modifiedExercisesToDo = exercisesToDo.map(
-      (exercise): ExerciseType =>
-        exercise.id === exerciseId
-          ? {
-              ...exercise,
-              sets: exercise.sets + 1,
-              reps: [...exercise.reps, ""],
-              weights: [...exercise.weights, ""],
-            }
-          : exercise,
-    );
-
     const modifiedCurrExercises = currWorkout.exercises.map(
       (exercise): ExerciseType =>
         exercise.id === exerciseId
@@ -120,11 +107,28 @@ export const WorkoutToDoForm = ({
           : exercise,
     );
 
-    setExercisesToDo(modifiedExercisesToDo);
     setCurrWorkout((prev) => {
       return { ...prev, exercises: modifiedCurrExercises };
     });
   }
+
+  // function removeNewSet(exerciseId: string, setIndex: number) {
+  //   const modifiedCurrExercises = currWorkout.exercises.map(
+  //     (exercise): ExerciseType =>
+  //       exercise.id === exerciseId
+  //         ? {
+  //             ...exercise,
+  //             sets: exercise.sets - 1,
+  //             reps: exercise.reps.toSpliced(setIndex, 1),
+  //             weights: exercise.weights.toSpliced(setIndex, 1),
+  //           }
+  //         : exercise,
+  //   );
+
+  //   setCurrWorkout((prev) => {
+  //     return { ...prev, exercises: modifiedCurrExercises };
+  //   });
+  // }
 
   function handleNoteInput(
     event: React.ChangeEvent<HTMLInputElement>,
@@ -146,7 +150,16 @@ export const WorkoutToDoForm = ({
   }
 
   function updateExercises(newExercise: ExerciseType) {
-    const modifiedCurrExercises = [...currWorkout.exercises, newExercise];
+    const newExerciseWithoutRepsAndWeights: ExerciseType = {
+      ...newExercise,
+      reps: newExercise.reps.map(() => ""),
+      weights: newExercise.weights.map(() => ""),
+    };
+
+    const modifiedCurrExercises = [
+      ...currWorkout.exercises,
+      newExerciseWithoutRepsAndWeights,
+    ];
 
     setCurrWorkout((prev) => {
       return {
@@ -181,8 +194,7 @@ export const WorkoutToDoForm = ({
     }
   }
 
-  console.log(new Date().toLocaleDateString());
-  console.log(new Date().toISOString());
+  console.log(currWorkout);
 
   return (
     <form
@@ -191,8 +203,8 @@ export const WorkoutToDoForm = ({
       className="flex h-full flex-col"
     >
       <header className="bg-gradient-to-r from-green-600 from-20% to-violet-600 pt-safe-top dark:from-green-700 dark:to-violet-700">
-        <div className="flex items-center gap-3 px-4 py-3 [&>*:nth-child(2)]:mr-auto [&>*:nth-child(3)]:mr-2">
-          <BackButton />
+        <div className="flex items-center gap-3 px-4 py-2 [&>*:nth-child(2)]:mr-auto [&>*:nth-child(3)]:mr-2">
+          <BackButtonModal className="mr-4 rounded-full p-1.5 text-white active:bg-slate-300/60 dark:active:bg-slate-300/50" />
 
           <p className="font-manrope text-xl uppercase text-white">
             {currWorkout.title}
@@ -203,7 +215,7 @@ export const WorkoutToDoForm = ({
       </header>
 
       <main className="flex flex-col divide-y divide-slate-300 overflow-auto overscroll-contain px-6 dark:divide-slate-800">
-        {exercisesToDo.map((exercise, i) => (
+        {currWorkout.exercises.map((exercise, exerciseIndex) => (
           <div key={exercise.id} className="flex flex-col py-6">
             <p className="pb-2 text-2xl font-bold">{exercise.name}</p>
 
@@ -213,7 +225,7 @@ export const WorkoutToDoForm = ({
                   autoFocus
                   id="note"
                   type="text"
-                  value={currWorkout.exercises[i]?.note}
+                  value={exercise.note}
                   placeholder="Leave a note..."
                   onChange={(e) => handleNoteInput(e, exercise.id)}
                   className="w-full rounded-none border-b-2 border-violet-500 bg-transparent py-0.5 font-semibold placeholder-slate-400/80 caret-violet-500 placeholder:text-sm placeholder:italic focus:placeholder-slate-300 focus:outline-none dark:text-white dark:placeholder-slate-500 dark:focus:placeholder-slate-700"
@@ -222,7 +234,7 @@ export const WorkoutToDoForm = ({
                 <button
                   type="button"
                   onClick={async () => {
-                    await new Promise((resolve) => setTimeout(resolve, 200));
+                    await new Promise((resolve) => setTimeout(resolve, 100));
                     setNote({ add: false, onExercise: exercise.id });
                   }}
                   className="rounded-[8px] px-4 font-manrope text-lg font-bold text-blue-400 active:bg-slate-300 dark:text-blue-500 dark:active:bg-slate-700"
@@ -232,7 +244,7 @@ export const WorkoutToDoForm = ({
               </div>
             ) : (
               <p className="font-semibold italic text-slate-400 dark:text-slate-400">
-                {currWorkout.exercises[i]?.note}
+                {exercise.note}
               </p>
             )}
 
@@ -244,15 +256,15 @@ export const WorkoutToDoForm = ({
                 >
                   Reps
                 </label>
-                {exercise.reps.map((rep, i) => (
+                {exercise.reps.map((rep, repIndex) => (
                   <input
-                    key={`${exercise.name} - rep ${i + 1}`}
-                    id={`${exercise.name} - rep ${i + 1}`}
+                    key={`${exercise.name} - rep ${repIndex + 1}`}
+                    id={`${exercise.name} - rep ${repIndex + 1}`}
                     type="text"
                     inputMode="numeric"
-                    placeholder={rep}
+                    placeholder={exercisesToDo[exerciseIndex]?.reps[repIndex]}
                     onChange={(e) => {
-                      handleRepsInput(e, exercise.id, i);
+                      handleRepsInput(e, exercise.id, repIndex);
                     }}
                     className={twMerge(
                       "input-field w-full py-1.5 text-center caret-violet-500 focus:ring-violet-500 dark:caret-violet-500 dark:focus:ring-violet-500",
@@ -268,15 +280,17 @@ export const WorkoutToDoForm = ({
                 >
                   Weights - kg
                 </label>
-                {exercise.weights.map((weight, i) => (
+                {exercise.weights.map((weight, weightIndex) => (
                   <input
-                    key={`${exercise.name} - weight ${i + 1}`}
-                    id={`${exercise.name} - weight ${i + 1}`}
+                    key={`${exercise.name} - weight ${weightIndex + 1}`}
+                    id={`${exercise.name} - weight ${weightIndex + 1}`}
                     type="text"
                     inputMode="decimal"
-                    placeholder={weight}
+                    placeholder={
+                      exercisesToDo[exerciseIndex]?.weights[weightIndex]
+                    }
                     onChange={(e) => {
-                      handleWeightInput(e, exercise.id, i);
+                      handleWeightInput(e, exercise.id, weightIndex);
                     }}
                     className={twMerge(
                       "input-field w-full py-1.5 text-center caret-violet-500 focus:ring-violet-500 dark:caret-violet-500 dark:focus:ring-violet-500",
@@ -316,10 +330,10 @@ export const WorkoutToDoForm = ({
         ))}
       </main>
 
-      <footer className="flex items-center justify-between border-t border-slate-300/80 px-8 pb-8 pt-3 text-end dark:border-slate-800">
+      <footer className="flex items-center justify-between border-t border-slate-300/80 px-6 pb-6 pt-2 text-end dark:border-slate-800">
         <AddExerciseDrawer
           updateExercises={updateExercises}
-          className="mr-2 text-green-500 dark:text-green-600"
+          className="rounded-full p-2 text-green-500 active:scale-95 active:bg-slate-200 dark:text-green-600 dark:active:bg-slate-700"
         />
 
         <DoneButton
@@ -338,55 +352,6 @@ export const WorkoutToDoForm = ({
   );
 };
 
-const BackButton = () => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <Drawer.Root open={open} onOpenChange={setOpen} noBodyStyles>
-      <Drawer.Trigger className="p-2 text-white active:rounded-full active:bg-slate-600">
-        {ArrowLeftIcon}
-
-        <p className="sr-only">Logout button</p>
-      </Drawer.Trigger>
-
-      <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 z-[9999] bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
-
-        <Drawer.Content
-          aria-describedby={undefined}
-          data-vaul-no-drag
-          className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
-        >
-          <div className="flex flex-col gap-3 rounded-modal bg-slate-50/90 text-center dark:bg-slate-700/70">
-            <Drawer.Title className="px-2 pb-2 pt-5 text-sm font-semibold">
-              Are you sure you want to leave? Your workout and any provided data
-              won&apos;t be saved.
-            </Drawer.Title>
-
-            <Link
-              href="/workouts"
-              className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
-            >
-              Back to Home page
-            </Link>
-          </div>
-
-          <button
-            type="button"
-            onClick={async () => {
-              await new Promise((resolve) => setTimeout(resolve, 100));
-              setOpen(false);
-            }}
-            className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
-          >
-            Cancel
-          </button>
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
-  );
-};
-
 const DoneButton = ({
   formId,
   stopTimer,
@@ -398,10 +363,17 @@ const DoneButton = ({
 
   return (
     <Drawer.Root open={open} onOpenChange={setOpen} noBodyStyles>
-      <Drawer.Trigger className="font-manrope text-xl font-extrabold text-green-500 dark:text-green-600">
+      <button
+        type="button"
+        onClick={async () => {
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          setOpen(true);
+        }}
+        className="rounded-lg px-2.5 py-1 text-xl font-extrabold text-green-500 active:scale-95 active:bg-slate-200 dark:text-green-600 dark:active:bg-slate-700"
+      >
         Done
         <p className="sr-only">Done button</p>
-      </Drawer.Trigger>
+      </button>
 
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 z-[9999] bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
@@ -411,7 +383,7 @@ const DoneButton = ({
           data-vaul-no-drag
           className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
         >
-          <div className="flex flex-col gap-3 rounded-modal bg-slate-50/90 text-center dark:bg-slate-700/70">
+          <div className="flex flex-col gap-3 rounded-modal bg-white/90 text-center dark:bg-slate-700/70">
             <Drawer.Title className="px-2 pb-2 pt-5 text-sm font-semibold">
               Are you sure you want to submit workout?
             </Drawer.Title>
