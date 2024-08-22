e22899113e22d6da43ed5d86d83ddaa80c93f20b
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-22 20:59:19 +0200

    MODIFY: user auth done on layout level

--- src/components/user/WorkoutsPageContent.tsx
+++ src/components/user/WorkoutsPageContent.tsx
@@ -1,15 +1,7 @@
-import { redirect } from "next/navigation";
-import { getAuth } from "@/util/actions/auth";
 import { getUserWorkouts } from "@/db/query";
 import { WorkoutCards } from "./WorkoutCards";
 
 export const WorkoutsPageContent = async () => {
-  const { user } = await getAuth();
-
-  if (!user) {
-    redirect("/login");
-  }
-
   const workouts = await getUserWorkouts();
 
   return <WorkoutCards workouts={workouts} />;
