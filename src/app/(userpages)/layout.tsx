de398f49cc8a3fdb2a5956cd96efd6c209b64e8f
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-22 20:56:00 +0200

    MODIFY: now imports userpagesheader component

--- src/app/(userpages)/layout.tsx
+++ src/app/(userpages)/layout.tsx
@@ -1,8 +1,6 @@
 import { redirect } from "next/navigation";
 import { getAuth } from "@/util/actions/auth";
-import { Logo } from "@/components/icons/logo";
-import { ThemeButton } from "@/components/ThemeButton";
-import { GitHubButton } from "@/components/GitHubButton";
+import { UserPagesHeader } from "@/components/UserPagesHeader";
 import { UserPagesNavBar } from "@/components/user/UserPagesNavBar";
 
 export default async function UserPagesLayout({
@@ -24,16 +22,3 @@ export default async function UserPagesLayout({
     </div>
   );
 }
-
-const UserPagesHeader = () => {
-  return (
-    <header className="px-4 pt-2">
-      <div className="relative flex items-center gap-4 rounded-[28px] bg-white px-4 py-2 shadow-md ring-1 ring-slate-200 dark:bg-slate-800 dark:ring-slate-700 [&>*:nth-child(1)]:mr-auto">
-        {Logo}
-
-        <ThemeButton />
-        <GitHubButton />
-      </div>
-    </header>
-  );
-};
