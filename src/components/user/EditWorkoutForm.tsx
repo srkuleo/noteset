2d885bc3b6ab2e7f78325b5eff27f38fa7651b76
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-28 02:13:14 +0200

    MODIFY: new UI, new footer, no longer renders nav bar at the bottom, adds fomr pending UI and state control with Tanstack query

--- src/components/user/EditWorkoutForm.tsx
+++ src/components/user/EditWorkoutForm.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
 import { useWorkouts } from "@/util/hooks";
 import { editWorkout } from "@/util/actions/workout";
 import { showToast } from "../Toasts";
-import { WorkoutFormHeader } from "./WorkoutFormHeader";
 import { FormTooltip } from "./FormTooltip";
 import { TitleInput, DescriptionInput } from "./WorkoutInputs";
-import { ExercisesList } from "../ExercisesList";
+import { ExercisesList } from "./ExercisesList";
 import { ErrorComponent } from "../ErrorComponent";
+import { AddExerciseDrawer } from "./AddExerciseDrawer";
 
 import type { QueriedByIdWorkoutType } from "@/db/schema";
 
@@ -17,10 +18,31 @@ export const EditWorkoutForm = ({
   workoutToEdit: QueriedByIdWorkoutType;
 }) => {
   const {
+    data: actionRes,
+    mutate: clientAction,
+    isPending,
+  } = useMutation({
+    mutationFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
+      const { title: originalTitle, id: workoutId } = workoutToEdit;
+
+      const res = await editWorkout(workout, workoutId, originalTitle);
+
+      return res;
+    },
+    onSuccess: (res) => {
+      if (res.status === "success-redirect") {
+        showToast(res.message, res.status, "/home", "View workouts");
+      } else if (res.status === "success" || res.status === "error") {
+        showToast(res.message, res.status);
+      }
+    },
+  });
+
+  const {
     workout,
     setWorkout,
-    actionRes,
-    setActionRes,
     handleTitleInput,
     handleDescriptionInput,
     updateExercises,
@@ -28,47 +50,16 @@ export const EditWorkoutForm = ({
     removeExercise,
   } = useWorkouts(workoutToEdit);
 
-  async function clientAction() {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const { title: originalTitle, id: workoutId } = workoutToEdit;
-
-    const res = await editWorkout(workout, workoutId, originalTitle);
-
-    setActionRes({ ...res });
-
-    if (res.status === "success-redirect") {
-      showToast(res.message, res.status, "/workouts", "View workouts");
-    } else if (res.status === "success" || res.status === "error") {
-      showToast(res.message, res.status);
-    }
-  }
-
   return (
     <>
-      <fieldset disabled={actionRes.status === "pending"} className="group">
-        <WorkoutFormHeader
-          heading="Edit workout"
-          formId="edit-workout-form"
-          updateExercises={updateExercises}
-        />
-      </fieldset>
-
       <main className="relative overflow-y-auto overscroll-contain scroll-smooth px-8 py-4">
-        <form
-          id="edit-workout-form"
-          action={clientAction}
-          onSubmit={() => setActionRes({ ...actionRes, status: "pending" })}
-        >
-          <fieldset
-            disabled={actionRes.status === "pending"}
-            className="group space-y-4"
-          >
+        <form id="edit-workout-form" action={() => clientAction()}>
+          <fieldset disabled={isPending} className="group space-y-4">
             <FormTooltip />
 
             <TitleInput
               title={workout.title}
-              titleError={actionRes.errors?.title}
+              titleError={actionRes && actionRes.errors?.title}
               handleTitleInput={handleTitleInput}
             />
 
@@ -81,20 +72,37 @@ export const EditWorkoutForm = ({
               editForm
               workout={workout}
               setWorkout={setWorkout}
-              exercisesError={actionRes.errors?.exercises}
+              exercisesError={actionRes && actionRes.errors?.exercises}
               editExercises={editExercises}
               removeExercise={removeExercise}
             />
 
-            {actionRes.status === "error" && (
+            {actionRes && actionRes.status === "error" && (
               <ErrorComponent
                 message={actionRes.message}
-                className="justify-center"
+                className="justify-center group-disabled:opacity-30"
               />
             )}
           </fieldset>
         </form>
       </main>
+
+      <fieldset disabled={isPending} className="group">
+        <footer className="flex items-center justify-between border-t border-slate-300/80 px-6 pb-6 pt-2 group-disabled:pointer-events-none group-disabled:opacity-50 dark:border-slate-800">
+          <AddExerciseDrawer
+            className="rounded-full p-1.5 text-violet-400 active:bg-slate-200 dark:text-violet-400 dark:active:bg-slate-700"
+            updateExercises={updateExercises}
+          />
+
+          <button
+            type="submit"
+            form="edit-workout-form"
+            className="rounded-lg px-3 py-1.5 text-xl font-extrabold text-green-500 active:scale-95 active:bg-slate-200 dark:active:bg-slate-700"
+          >
+            {isPending ? "Saving..." : "Save"}
+          </button>
+        </footer>
+      </fieldset>
     </>
   );
 };
