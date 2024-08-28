92edee2dc62b7b721d869d4683d7da83f5e089c3
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-28 02:04:09 +0200

    MODIFY: adds Tanstack Query useMutation for easier form pending state and action response handling

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,76 +1,76 @@
 "use client";
 
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 import { twMerge } from "tailwind-merge";
 import { login } from "@/util/actions/auth";
 import { HideIcon, ShowIcon } from "../icons/user/preview";
 import { ErrorComponent } from "../ErrorComponent";
-import { SubmitFormButton } from "../SubmitButtons";
-
-import type { AuthActionResponse } from "@/util/types";
+import { AuthButton } from "../SubmitButtons";
 
 export const LoginForm = () => {
-  const [actionRes, setActionRes] = useState<AuthActionResponse>({});
+  const {
+    isPending,
+    isError,
+    error,
+    mutate: serverAction,
+  } = useMutation({
+    mutationFn: login,
+  });
   const [showPassword, setShowPassword] = useState(false);
 
-  async function clientLogin(formData: FormData) {
-    const res = await login(formData);
-
-    setActionRes({ ...res });
-  }
-
   return (
-    <form action={clientLogin} className="flex flex-col gap-4">
-      <input
-        id="username"
-        name="username"
-        type="text"
-        placeholder="Username"
-        autoComplete="username"
-        className={twMerge(
-          "input-field",
-          "bg-white dark:bg-slate-800/50",
-          actionRes.errors?.username && "ring-red-500 dark:ring-red-500",
-        )}
-      />
-      <ErrorComponent errorArr={actionRes.errors?.username} className="pl-1" />
-
-      <div className="relative">
+    <form action={serverAction} className="flex flex-col">
+      <fieldset disabled={isPending} className="group space-y-4">
         <input
-          id="password"
-          name="password"
-          type={showPassword ? "text" : "password"}
-          placeholder="Password"
-          autoComplete="current-password"
-          required
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Username"
+          autoComplete="username"
           className={twMerge(
             "input-field",
-            "w-full bg-white dark:bg-slate-800/50",
-            actionRes.errors?.password && "ring-red-500 dark:ring-red-500",
+            "w-full bg-white group-disabled:opacity-50 dark:bg-slate-800/50",
+            isError && "ring-red-500 dark:ring-red-500",
           )}
         />
-        <button
-          type="button"
-          onClick={() => {
-            setShowPassword(!showPassword);
-          }}
-          className="absolute inset-y-0 right-0 px-3 py-2"
-        >
-          {showPassword ? (
-            <HideIcon className="size-6" />
-          ) : (
-            <ShowIcon className="size-6" />
-          )}
-        </button>
-      </div>
-      <ErrorComponent errorArr={actionRes.errors?.password} className="pl-1" />
 
-      <ErrorComponent message={actionRes.message} />
+        <div className="relative group-disabled:opacity-50">
+          <input
+            id="password"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            autoComplete="current-password"
+            required
+            className={twMerge(
+              "input-field",
+              "w-full bg-white dark:bg-slate-800/50",
+              isError && "ring-red-500 dark:ring-red-500",
+            )}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+            className="absolute inset-y-0 right-0 px-3 py-2"
+          >
+            {showPassword ? (
+              <HideIcon className="size-6" />
+            ) : (
+              <ShowIcon className="size-6" />
+            )}
+          </button>
+        </div>
 
-      <SubmitFormButton
+        {isError && <ErrorComponent message={error?.message} />}
+      </fieldset>
+
+      <AuthButton
+        pending={isPending}
         label="Login"
         loading="Authenticating..."
-        className="mt-6 rounded-full py-2.5 shadow-md"
       />
     </form>
   );
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,145 +1,168 @@
 "use client";
 
 import { useRef, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 import { twMerge } from "tailwind-merge";
 import { signUp } from "@/util/actions/auth";
 import { showToast } from "../Toasts";
 import { HideIcon, ShowIcon } from "../icons/user/preview";
 import { ErrorComponent } from "../ErrorComponent";
-import { SubmitFormButton } from "../SubmitButtons";
-
-import type { AuthActionResponse } from "@/util/types";
+import { AuthButton } from "../SubmitButtons";
 
 export const SignUpForm = () => {
-  const [actionRes, setActionRes] = useState<AuthActionResponse>({});
+  const {
+    isPending,
+    data: actionRes,
+    mutate: clientAction,
+  } = useMutation({
+    mutationFn: signUp,
+    onSuccess: (actionRes) => {
+      if (actionRes.message && actionRes.status === "success-redirect") {
+        showToast(actionRes.message, actionRes.status, "/login", "Log in");
+        formRef.current?.reset();
+      }
+    },
+  });
   const [show, setShow] = useState({ password: false, confirmPassword: false });
   const formRef = useRef<HTMLFormElement>(null);
 
-  async function clientSignUp(formData: FormData) {
-    const res = await signUp(formData);
-
-    if (res.status === "success-redirect" && res.message) {
-      showToast(res.message, res.status, "/workouts", "View workouts");
-      formRef.current?.reset();
-    }
-
-    setActionRes({ ...res });
-  }
-
   return (
-    <form ref={formRef} action={clientSignUp} className="flex flex-col gap-4">
-      <input
-        id="username"
-        name="username"
-        type="text"
-        placeholder="Username"
-        autoComplete="username"
-        required
-        className={twMerge(
-          "input-field",
-          "bg-white dark:bg-slate-800/50",
-          actionRes.errors?.username && "ring-red-500 dark:ring-red-500",
-        )}
-      />
-      <ErrorComponent errorArr={actionRes.errors?.username} className="pl-1" />
-
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="Email"
-        autoComplete="email"
-        required
-        className={twMerge(
-          "input-field",
-          "bg-white dark:bg-slate-800/50",
-          actionRes.errors?.email && "ring-red-500 dark:ring-red-500",
-        )}
-      />
-      <ErrorComponent errorArr={actionRes.errors?.email} className="pl-1" />
-
-      <div className="relative">
+    <form ref={formRef} action={clientAction} className="flex flex-col">
+      <fieldset disabled={isPending} className="group space-y-4">
         <input
-          id="password"
-          name="password"
-          type={show.password ? "text" : "password"}
-          placeholder="Password"
-          autoComplete="new-password"
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Username"
+          autoComplete="username"
           required
           className={twMerge(
             "input-field",
-            "w-full bg-white dark:bg-slate-800/50",
-            actionRes.errors?.password && "ring-red-500 dark:ring-red-500",
-          )}
-        />
-        <button
-          type="button"
-          onClick={() =>
-            setShow((prev) => {
-              return {
-                ...prev,
-                password: !prev.password,
-              };
-            })
-          }
-          className="absolute inset-y-0 right-0 px-3 py-2"
-        >
-          {show.password ? (
-            <HideIcon className="size-6" />
-          ) : (
-            <ShowIcon className="size-6" />
-          )}
-        </button>
-      </div>
-      <ErrorComponent errorArr={actionRes.errors?.password} className="pl-1" />
-
-      <div className="relative">
-        <input
-          id="confirmPassword"
-          name="confirmPassword"
-          type={show.confirmPassword ? "text" : "password"}
-          placeholder="Confirm password"
-          autoComplete="new-password"
-          required
-          className={twMerge(
-            "input-field",
-            "w-full bg-white dark:bg-slate-800/50",
-            actionRes.errors?.confirmPassword &&
+            "w-full bg-white group-disabled:opacity-50 dark:bg-slate-800/50",
+            actionRes &&
+              actionRes.errors?.username &&
               "ring-red-500 dark:ring-red-500",
           )}
         />
-        <button
-          type="button"
-          onClick={() =>
-            setShow((prev) => {
-              return {
-                ...prev,
-                confirmPassword: !prev.confirmPassword,
-              };
-            })
-          }
-          className="absolute inset-y-0 right-0 px-3 py-2"
-        >
-          {show.confirmPassword ? (
-            <HideIcon className="size-6" />
-          ) : (
-            <ShowIcon className="size-6" />
+        {actionRes && (
+          <ErrorComponent
+            errorArr={actionRes.errors?.username}
+            className="pl-1"
+          />
+        )}
+
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          autoComplete="email"
+          required
+          className={twMerge(
+            "input-field",
+            "w-full bg-white group-disabled:opacity-50 dark:bg-slate-800/50",
+            actionRes &&
+              actionRes.errors?.email &&
+              "ring-red-500 dark:ring-red-500",
           )}
-        </button>
-      </div>
-      <ErrorComponent
-        errorArr={actionRes.errors?.confirmPassword}
-        className="pl-1"
-      />
+        />
+        {actionRes && (
+          <ErrorComponent errorArr={actionRes.errors?.email} className="pl-1" />
+        )}
 
-      {actionRes.status === "error" && (
-        <ErrorComponent message={actionRes.message} />
-      )}
+        <div className="relative group-disabled:opacity-50">
+          <input
+            id="password"
+            name="password"
+            type={show.password ? "text" : "password"}
+            placeholder="Password"
+            autoComplete="new-password"
+            required
+            className={twMerge(
+              "input-field",
+              "w-full bg-white dark:bg-slate-800/50",
+              actionRes &&
+                actionRes.errors?.password &&
+                "ring-red-500 dark:ring-red-500",
+            )}
+          />
+          <button
+            type="button"
+            onClick={() =>
+              setShow((prev) => {
+                return {
+                  ...prev,
+                  password: !prev.password,
+                };
+              })
+            }
+            className="absolute inset-y-0 right-0 px-3 py-2"
+          >
+            {show.password ? (
+              <HideIcon className="size-6" />
+            ) : (
+              <ShowIcon className="size-6" />
+            )}
+          </button>
+        </div>
+        {actionRes && (
+          <ErrorComponent
+            errorArr={actionRes.errors?.password}
+            className="pl-1"
+          />
+        )}
 
-      <SubmitFormButton
+        <div className="relative group-disabled:opacity-50">
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            type={show.confirmPassword ? "text" : "password"}
+            placeholder="Confirm password"
+            autoComplete="new-password"
+            required
+            className={twMerge(
+              "input-field",
+              "w-full bg-white dark:bg-slate-800/50",
+              actionRes &&
+                actionRes.errors?.confirmPassword &&
+                "ring-red-500 dark:ring-red-500",
+            )}
+          />
+          <button
+            type="button"
+            onClick={() =>
+              setShow((prev) => {
+                return {
+                  ...prev,
+                  confirmPassword: !prev.confirmPassword,
+                };
+              })
+            }
+            className="absolute inset-y-0 right-0 px-3 py-2"
+          >
+            {show.confirmPassword ? (
+              <HideIcon className="size-6" />
+            ) : (
+              <ShowIcon className="size-6" />
+            )}
+          </button>
+        </div>
+        {actionRes && (
+          <ErrorComponent
+            errorArr={actionRes.errors?.confirmPassword}
+            className="pl-1"
+          />
+        )}
+
+        {actionRes && actionRes.status === "error" && (
+          <ErrorComponent message={actionRes.message} />
+        )}
+      </fieldset>
+
+      <AuthButton
+        pending={isPending}
         label="Sign Up"
         loading="Creating profile..."
-        className="mt-6 rounded-full py-2.5 shadow-md"
       />
     </form>
   );
