89defa57ffd1a580d3daf6d8a826ecef23513117
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-28 02:19:48 +0200

    MODIFY: add query provider

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,19 +1,24 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 
+const queryClient = new QueryClient();
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      themes={["system", "dark", "light"]}
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-      <Toaster duration={4000} position="bottom-center" />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        themes={["system", "dark", "light"]}
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster duration={4000} position="bottom-center" />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
