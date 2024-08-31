075ab2a0c26eb43d32f9107f7f5d6a6d1377cf36
Srdjan <milicevic.srdjan0@gmail.com>
2024-08-31 02:29:15 +0200

    MODIFY: adds optional className prop for customization

--- src/components/StatusIndicator.tsx
+++ src/components/StatusIndicator.tsx
@@ -1,8 +1,20 @@
+import { twMerge } from "tailwind-merge";
+
 import type { WorkoutType } from "@/db/schema";
 
-export const StatusIndicator = ({ status }: Pick<WorkoutType, "status">) => {
+export const StatusIndicator = ({
+  status,
+  className,
+}: {
+  className?: string;
+} & Pick<WorkoutType, "status">) => {
   return (
-    <div className="flex items-center gap-1 rounded-full px-3 py-0.5 shadow-md ring-1 ring-inset ring-slate-300 dark:shadow-slate-900 dark:ring-slate-600">
+    <div
+      className={twMerge(
+        "flex items-center gap-1 rounded-full px-3 py-0.5 shadow-md ring-1 ring-inset ring-slate-300 dark:shadow-slate-900 dark:ring-slate-600",
+        className,
+      )}
+    >
       <div
         className={`${status === "current" ? "bg-blue-400 dark:bg-blue-500" : status === "done" ? "bg-green-500 dark:bg-green-600" : "bg-slate-500 dark:bg-slate-600"} size-2 rounded-full`}
       />
