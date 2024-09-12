6f54b1e22be60c6c883c1a2e9b5c9daf33077b7f
Srdjan <milicevic.srdjan0@gmail.com>
2024-09-12 02:07:14 +0200

    MODIFY: is now coloured to match the body colour

--- src/components/landing/LandingPageBar.tsx
+++ src/components/landing/LandingPageBar.tsx
@@ -5,7 +5,7 @@ import { GitHubButton } from "../GitHubButton";
 
 export const LandingPageBar = () => {
   return (
-    <header className="border-b border-slate-400/40 px-4 shadow-xl dark:border-slate-800/90">
+    <header className="border-b border-slate-400/40 bg-white px-4 dark:border-slate-800/90 dark:bg-slate-950">
       <div className="flex items-center gap-4 py-2 [&>*:nth-child(1)]:mr-auto">
         <Link href="/">{Logo}</Link>
 
