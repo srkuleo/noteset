2de7b35bd164297f91c58b27982ee79d60afc59a
Srdjan <milicevic.srdjan0@gmail.com>
2024-09-13 02:43:07 +0200

    MODIFY: removes themebutton, now part of the profile page settings

--- src/components/landing/LandingPageBar.tsx
+++ src/components/landing/LandingPageBar.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 import { Logo } from "../icons/logo";
-import { ThemeButton } from "../ThemeButton";
 import { GitHubButton } from "../GitHubButton";
 
 export const LandingPageBar = () => {
   return (
-    <header className="border-b border-slate-400/40 bg-white px-4 dark:border-slate-800/90 dark:bg-slate-950">
-      <div className="flex items-center gap-4 py-2 [&>*:nth-child(1)]:mr-auto">
+    <header className="border-b border-slate-400/40 bg-white px-6 py-2 dark:border-slate-800/90 dark:bg-slate-950">
+      <div className="flex items-center justify-between">
         <Link href="/">{Logo}</Link>
 
-        <ThemeButton />
         <GitHubButton />
       </div>
     </header>
--- src/components/user/UserPagesHeader.tsx
+++ src/components/user/UserPagesHeader.tsx
@@ -1,6 +1,5 @@
 import { twMerge } from "tailwind-merge";
 import { Logo } from "../icons/logo";
-import { ThemeButton } from "../ThemeButton";
 import { GitHubButton } from "../GitHubButton";
 
 import type { HTMLAttributes } from "react";
@@ -9,10 +8,9 @@ export const UserPagesHeader = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-10 bg-white px-4 pt-safe-top dark:bg-slate-950">
       <div className="pb-2 pt-2">
-        <div className="relative flex items-center gap-4 rounded-[28px] bg-white px-4 py-2 shadow-md ring-1 ring-slate-300/80 dark:bg-slate-900 dark:ring-slate-800 [&>*:nth-child(1)]:mr-auto">
+        <div className="flex items-center justify-between rounded-[28px] bg-white px-4 py-2 shadow-md ring-1 ring-slate-300/80 dark:bg-slate-900 dark:ring-slate-800">
           {Logo}
 
-          <ThemeButton />
           <GitHubButton />
         </div>
       </div>
@@ -30,7 +28,7 @@ export const UserPagesSubHeadingWrapper = ({
   return (
     <div
       className={twMerge(
-        "fixed inset-x-0 top-20 z-10 mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-950",
+        "fixed inset-x-0 top-[77px] z-10 mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-950",
         className,
       )}
     >
