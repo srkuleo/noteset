2d126bfbbb87d0f3afd74bd25729a875e72a7a48
Srdjan <milicevic.srdjan0@gmail.com>
2024-09-13 02:38:23 +0200

    MODIFY: renames and move DoneButton

--- src/components/SubmitButtons.tsx
+++ src/components/SubmitButtons.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import { useFormStatus } from "react-dom";
+import { Drawer } from "vaul";
 
 type SubmitButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string;
@@ -55,5 +54,77 @@ export const ConfirmLogOutButton = () => {
     >
       {pending ? "Logging Out..." : "Log Out"}
     </button>
+  );
+};
+
+export const SubmitDoneWorkoutButton = ({
+  formId,
+  pending,
+  open,
+  setOpen,
+  endWorkout,
+}: {
+  formId: string;
+  pending: boolean;
+  open: boolean;
+  setOpen: (isOpen: boolean) => void;
+  endWorkout: () => void;
+}) => {
+  return (
+    <Drawer.Root
+      open={open}
+      onOpenChange={setOpen}
+      noBodyStyles
+      disablePreventScroll
+    >
+      <button
+        type="button"
+        onClick={async () => {
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          setOpen(true);
+        }}
+        className="rounded-lg px-3 py-1.5 text-xl font-bold text-green-500 active:scale-95 active:bg-slate-200 dark:text-green-600 dark:active:bg-slate-700"
+      >
+        Done
+        <p className="sr-only">Done button</p>
+      </button>
+
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 z-[9999] bg-slate-900/40 backdrop-blur-sm dark:bg-slate-950/70" />
+
+        <Drawer.Content
+          aria-describedby={undefined}
+          data-vaul-no-drag
+          className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
+        >
+          <div className="flex flex-col gap-3 rounded-modal bg-white/90 text-center dark:bg-slate-700/70">
+            <Drawer.Title className="px-2 pb-2 pt-5 text-sm font-semibold">
+              Are you sure you want to submit workout?
+            </Drawer.Title>
+
+            <button
+              type="submit"
+              form={formId}
+              disabled={pending}
+              onClick={endWorkout}
+              className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
+            >
+              {pending ? "Submitting..." : "Submit"}
+            </button>
+          </div>
+
+          <button
+            onClick={async () => {
+              await new Promise((resolve) => setTimeout(resolve, 100));
+
+              setOpen(false);
+            }}
+            className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
+          >
+            Cancel
+          </button>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
