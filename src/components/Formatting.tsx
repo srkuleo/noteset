cbe5293d645d1789e4c31eef523b899b9bb7ae22
Srdjan <milicevic.srdjan0@gmail.com>
2024-09-17 02:33:59 +0200

    MODIFY: adds option to show day of the week alongside full date

--- src/components/Formatting.tsx
+++ src/components/Formatting.tsx
@@ -39,20 +39,32 @@ export const FormatWorkoutDuration = ({
 
 export const FormatDate = ({
   date,
+  withDayOfTheWeek,
   className,
 }: {
   date: Date | null;
+  withDayOfTheWeek?: boolean;
   className: string;
 }) => {
-  const formatDate = (date: Date) => {
+  const formatDate = (date: Date, withDayOfTheWeek?: boolean) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
 
+    if (withDayOfTheWeek) {
+      const dayOfTheWeek = date
+        .toLocaleString("en", { weekday: "short" })
+        .split(" ")[0];
+
+      return `${dayOfTheWeek} ${day}-${month}-${year}`;
+    }
+
     return `${day}-${month}-${year}`;
   };
 
-  if (date) {
+  if (date && withDayOfTheWeek) {
+    return <p className={className}>{formatDate(date, withDayOfTheWeek)}</p>;
+  } else if (date) {
     return <p className={className}>{formatDate(date)}</p>;
   }
 
