27ed5ae8a80ff9c5538708b56882f1197798e127
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-08 23:58:31 +0200

    MODIFY: now accepts searchParams from home page as queryParam prop, fetches different workouts - current or archived

--- src/components/user/HomePageDataFetcher.tsx
+++ src/components/user/HomePageDataFetcher.tsx
@@ -1,8 +1,14 @@
 import { getUserWorkouts } from "@/db/query";
 import { HomePageContent } from "./HomePageContent";
 
-export const HomePageDataFetcher = async () => {
-  const workouts = await getUserWorkouts();
+import type { WorkoutStatusType } from "@/util/types";
 
-  return <HomePageContent workouts={workouts} />;
+export const HomePageDataFetcher = async ({
+  queryParam,
+}: {
+  queryParam: WorkoutStatusType;
+}) => {
+  const workouts = await getUserWorkouts(queryParam);
+
+  return <HomePageContent workouts={workouts} status={queryParam} />;
 };
