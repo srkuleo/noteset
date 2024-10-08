231f749b224a071abbab5c489b3d1127b9ce1284
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-09 00:07:10 +0200

    MODIFY: now reads searchParams and renders a new switch button which utilize q searchParam

--- src/app/(user)/(main pages)/home/page.tsx
+++ src/app/(user)/(main pages)/home/page.tsx
@@ -4,38 +4,48 @@ import {
   UserPagesSubHeadingWrapper,
   UserPagesSubHeadingText,
 } from "@/components/user/UserPagesHeader";
+import { WorkoutQuerySwitchButton } from "@/components/user/WorkoutQuerySwitchButton";
 import { HomePageTooltip } from "@/components/Tooltips";
 import { AddIcon } from "@/components/icons/user/modify";
 import { LoadingWorkoutsSkeleton } from "@/components/Loading";
 import { HomePageDataFetcher } from "@/components/user/HomePageDataFetcher";
 
 import type { Metadata } from "next";
+import type { WorkoutStatusType } from "@/util/types";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-export default async function HomePage() {
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams: {
+    q: WorkoutStatusType;
+  };
+}) {
   return (
     <>
       <UserPagesSubHeadingWrapper>
-        <UserPagesSubHeadingText label="Workouts" />
+        <div className="flex items-center gap-1">
+          <UserPagesSubHeadingText label="Workouts" className="text-2xl" />
 
-        <div className="flex gap-1">
+          <WorkoutQuerySwitchButton />
+
           <HomePageTooltip />
-
-          <Link
-            href="/create"
-            className="rounded-full bg-white p-2 shadow-md ring-1 ring-slate-300 transition active:scale-95 active:bg-slate-200 dark:bg-slate-800 dark:ring-slate-600 dark:active:bg-slate-600"
-          >
-            <AddIcon size={24} strokeWidth={2} />
-            <p className="sr-only">Add a new workout</p>
-          </Link>
         </div>
+
+        <Link
+          href="/create"
+          className="rounded-full bg-white p-2 shadow-md ring-1 ring-slate-300 transition active:scale-95 active:bg-slate-200 dark:bg-slate-800 dark:ring-slate-600 dark:active:bg-slate-600"
+        >
+          <AddIcon size={24} strokeWidth={2} />
+          <p className="sr-only">Add a new workout</p>
+        </Link>
       </UserPagesSubHeadingWrapper>
 
       <Suspense fallback={<LoadingWorkoutsSkeleton />}>
-        <HomePageDataFetcher />
+        <HomePageDataFetcher queryParam={searchParams.q} />
       </Suspense>
     </>
   );
