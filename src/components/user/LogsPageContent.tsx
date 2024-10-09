6eedf7fc5f45bff88d58034ecf6832cbf76c238b
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-09 22:45:40 +0200

    MODIFY: utilize early return

--- src/components/user/LogsPageContent.tsx
+++ src/components/user/LogsPageContent.tsx
@@ -15,80 +15,81 @@ export const LogsPageContent = ({
   doneWorkouts: WorkoutType[];
   timeFormatPreference: TimeFormatType;
 }) => {
-  return (
-    <>
-      {doneWorkouts.length === 0 ? (
+  if (doneWorkouts.length === 0) {
+    return (
+      <main className="mt-safe-top flex flex-col justify-center px-8 pb-[91px] pt-40">
         <EmptyPage />
-      ) : (
-        <main className="mt-safe-top space-y-6 px-6 pb-[91px] pt-[158px]">
-          {doneWorkouts.map((doneWorkout) => (
-            <div key={doneWorkout.id} className="flex w-full gap-3">
-              <div className="min-w-[88px] rounded-lg border border-slate-300/80 bg-violet-400 shadow-md dark:border-slate-700 dark:bg-violet-500">
-                <div className="ml-1.5 flex flex-col items-center gap-2 rounded-r-[7px] bg-white p-3 font-manrope leading-none dark:bg-slate-800">
-                  <p className="font-semibold text-slate-400">
-                    {doneWorkout.doneAt?.toLocaleString("en", {
-                      month: "short",
-                    })}
-                  </p>
+      </main>
+    );
+  }
 
-                  <p className="font-extrabold text-slate-800 dark:text-white">
-                    {String(doneWorkout.doneAt?.getDate()).padStart(2, "0")}
-                  </p>
+  return (
+    <main className="mt-safe-top space-y-6 px-6 pb-[91px] pt-[158px]">
+      {doneWorkouts.map((doneWorkout) => (
+        <div key={doneWorkout.id} className="flex w-full gap-3">
+          <div className="min-w-[88px] rounded-lg border border-slate-300/80 bg-violet-400 shadow-md dark:border-slate-700 dark:bg-violet-500">
+            <div className="ml-1.5 flex flex-col items-center gap-2 rounded-r-[7px] bg-white p-3 font-manrope leading-none dark:bg-slate-800">
+              <p className="font-semibold text-slate-400">
+                {doneWorkout.doneAt?.toLocaleString("en", {
+                  month: "short",
+                })}
+              </p>
 
-                  <p className="font-bold dark:text-slate-300">
-                    {doneWorkout.doneAt?.getFullYear()}
-                  </p>
-                </div>
-              </div>
+              <p className="font-extrabold text-slate-800 dark:text-white">
+                {String(doneWorkout.doneAt?.getDate()).padStart(2, "0")}
+              </p>
 
-              <div className="mr-auto flex flex-col justify-center gap-1">
-                <p className="text-pretty font-manrope text-lg font-bold uppercase leading-none dark:text-slate-300">
-                  {doneWorkout.title}
-                </p>
-
-                <FormatWorkoutDuration
-                  timeFormat={timeFormatPreference}
-                  duration={doneWorkout.duration}
-                />
-
-                <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
-                  {doneWorkout.doneAt?.toLocaleString("en", {
-                    weekday: "long",
-                  })}
-                </p>
-              </div>
-
-              <div className="flex flex-col items-center justify-center gap-3 border-l border-slate-300/80 pl-3 dark:border-slate-800/80">
-                <StatusIndicator
-                  status={doneWorkout.status}
-                  className="bg-white px-4 py-2 dark:bg-slate-800"
-                />
-                <PreviewWorkoutButtonDrawer
-                  workout={doneWorkout}
-                  className="bg-white px-2.5 py-2 dark:bg-slate-800"
-                  size={5}
-                />
-              </div>
+              <p className="font-bold dark:text-slate-300">
+                {doneWorkout.doneAt?.getFullYear()}
+              </p>
             </div>
-          ))}
-        </main>
-      )}
-    </>
+          </div>
+
+          <div className="mr-auto flex flex-col justify-center gap-1">
+            <p className="text-pretty font-manrope text-lg font-bold uppercase leading-none dark:text-slate-300">
+              {doneWorkout.title}
+            </p>
+
+            <FormatWorkoutDuration
+              timeFormat={timeFormatPreference}
+              duration={doneWorkout.duration}
+            />
+
+            <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
+              {doneWorkout.doneAt?.toLocaleString("en", {
+                weekday: "long",
+              })}
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center gap-3 border-l border-slate-300/80 pl-3 dark:border-slate-800/80">
+            <StatusIndicator
+              status={doneWorkout.status}
+              className="bg-white px-4 py-2 dark:bg-slate-800"
+            />
+
+            <PreviewWorkoutButtonDrawer
+              workout={doneWorkout}
+              className="bg-white px-2.5 py-2 dark:bg-slate-800"
+              size={5}
+            />
+          </div>
+        </div>
+      ))}
+    </main>
   );
 };
 
 const EmptyPage = () => {
   return (
-    <main className="mt-safe-top flex flex-col justify-center px-8 pb-[91px] pt-40">
-      <div className="flex flex-col items-center gap-8 pb-18">
-        <div className="text-slate-400/60 dark:text-slate-700/80">
-          {EmptyIcon}
-        </div>
-
-        <div className="text-center">
-          <h3>Seems like you don&apos;t have any completed workout yet</h3>
-        </div>
+    <div className="flex flex-col items-center gap-8 pb-18">
+      <div className="text-slate-400/60 dark:text-slate-700/80">
+        {EmptyIcon}
       </div>
-    </main>
+
+      <div className="text-center">
+        <h3>Seems like you don&apos;t have any completed workout yet</h3>
+      </div>
+    </div>
   );
 };
