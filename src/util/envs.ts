6aac86841498b4b8185ced8812fdf02c9217e473
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-09 02:55:53 +0200

    MODIFY: disables eslint rule for empty object

--- src/util/envs.ts
+++ src/util/envs.ts
@@ -6,6 +6,7 @@ const envVariablesSchema = z.object({
 
 declare global {
   namespace NodeJS {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface ProcessEnv extends z.infer<typeof envVariablesSchema> {}
   }
 }
