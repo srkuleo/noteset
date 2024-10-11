950b435885eb944c4427f4c740b3b7b00931cb50
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-11 17:29:49 +0200

    MODIFY: UI finished, no longer WIP

--- src/components/workout-to-do/AddNewSetButton.tsx
+++ src/components/workout-to-do/AddNewSetButton.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
-import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 import { Drawer } from "vaul";
+import { twMerge } from "tailwind-merge";
 import { AddIcon } from "../icons/user/modify";
+import { ArrowLeftIcon } from "../icons/arrows";
 import { ErrorComponent } from "../ErrorComponent";
+import { ModalSubmitButton } from "../SubmitButtons";
 
 import {
   SetSchema,
   type ExerciseToDoType,
   type SetWithoutId,
 } from "@/util/types";
-import { ModalSubmitButton } from "../SubmitButtons";
 
 export const AddNewSetButton = ({
   exercise,
@@ -51,9 +53,9 @@ export const AddNewSetButton = ({
           aria-describedby={undefined}
           className="fixed inset-x-0 top-0 z-[9999] px-2 focus:outline-none"
         >
-          <div className="rounded-b-modal bg-slate-200 pt-safe-top dark:bg-slate-800 dark:ring-1 dark:ring-slate-700/80">
-            <div className="border-b border-b-slate-300 px-8 py-4 dark:border-b-slate-700">
-              <Drawer.Title className="font-manrope text-xl text-slate-800">
+          <div className="rounded-b-modal bg-white pb-3 ring-1 ring-slate-200 dark:bg-slate-900 dark:ring-slate-700/70">
+            <div className="space-y-4 border-b border-b-slate-300/50 bg-slate-200/55 pt-safe-top dark:border-b-slate-700/70 dark:bg-slate-800">
+              <Drawer.Title className="px-4 py-3 font-manrope text-xl text-slate-800">
                 Adding new set
               </Drawer.Title>
             </div>
@@ -62,6 +64,11 @@ export const AddNewSetButton = ({
               exercise={exercise}
               addNewSet={addNewSet}
               closeDrawer={() => setOpen(false)}
+            />
+
+            <Drawer.Handle
+              preventCycle
+              className="bg-slate-300 dark:bg-slate-600"
             />
           </div>
         </Drawer.Content>
@@ -79,23 +86,26 @@ const AddSetForm = ({
   addNewSet: (exerciseId: string, setData: SetWithoutId) => void;
   closeDrawer: () => void;
 }) => {
+  const [newSet, setNewSet] = useState<SetWithoutId>({
+    reps: "",
+    weight: "",
+    warmup: undefined,
+  });
+  const [showForm, setShowForm] = useState(false);
   const {
     mutate: clientAction,
     data: error,
     isPending,
+    reset,
   } = useMutation({
-    mutationFn: async (formData: FormData) => {
+    mutationFn: async () => {
       await new Promise((resolve) => setTimeout(resolve, 300));
 
       const isValidSet = SetSchema.pick({
         reps: true,
         weight: true,
         warmup: true,
-      }).safeParse({
-        reps: formData.get("reps"),
-        weight: formData.get("weight"),
-        warmup: Boolean(formData.get("warmup-set")),
-      });
+      }).safeParse(newSet);
 
       if (!isValidSet.success) {
         return isValidSet.error.flatten().fieldErrors;
@@ -107,80 +117,227 @@ const AddSetForm = ({
   });
 
   return (
-    <form
-      id="add-new-set"
-      action={clientAction}
-      className="rounded-b-modal bg-white px-8 py-4 dark:bg-slate-600"
-    >
-      <fieldset disabled={isPending} className="group flex flex-col space-y-8">
-        <div className="flex items-center gap-2 group-disabled:pointer-events-none group-disabled:opacity-50">
-          <label
-            htmlFor="warmup-set"
-            className="font-semibold uppercase dark:text-slate-400"
-          >
-            Warmup Set
-          </label>
+    <form id="add-new-set" action={() => clientAction()} className="p-6">
+      <fieldset
+        disabled={isPending}
+        className="group flex flex-col space-y-8 overflow-x-hidden p-2"
+      >
+        <AnimatePresence initial={false} mode="wait">
+          {!showForm ? (
+            <motion.div
+              key="select-set-type"
+              initial={{ opacity: 0, x: -64 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.4,
+                  ease: [0.36, 0.66, 0.04, 1],
+                },
+              }}
+              exit={{
+                opacity: 0,
+                x: -64,
+                transition: {
+                  duration: 0.3,
+                  ease: [0.36, 0.66, 0.04, 1],
+                },
+              }}
+            >
+              <div className="space-y-4">
+                <p className="text-center text-xl font-semibold text-slate-500 dark:text-slate-300">
+                  Select the type of the set:
+                </p>
 
-          <input
-            type="checkbox"
-            name="warmup-set"
-            id="warmup-set"
-            className="accent-green-500"
-          />
-        </div>
+                <div
+                  className={twMerge(
+                    "flex justify-evenly text-slate-700 dark:text-white",
+                    newSet.warmup !== undefined && "pb-4",
+                  )}
+                >
+                  <button
+                    type="button"
+                    onClick={() => setNewSet({ ...newSet, warmup: true })}
+                    className={twMerge(
+                      "rounded-xl px-5 py-[9px] font-manrope text-sm font-semibold uppercase shadow-md ring-1 ring-slate-200 dark:shadow-slate-950 dark:ring-slate-700 dark:active:bg-slate-800",
+                      newSet.warmup &&
+                        "bg-violet-400 text-white dark:bg-violet-500",
+                    )}
+                  >
+                    Warmup
+                  </button>
 
-        <div className="space-y-2 group-disabled:opacity-50">
-          <div className="flex justify-evenly">
-            <input
-              required
-              disabled={isPending}
-              id="reps"
-              name="reps"
-              type="text"
-              placeholder="Reps"
-              className={twMerge(
-                "input-field",
-                "w-1/3 py-2 text-center",
-                error?.reps && "ring-red-500 dark:ring-red-500",
-              )}
-            />
+                  <button
+                    type="button"
+                    onClick={() => setNewSet({ ...newSet, warmup: false })}
+                    className={twMerge(
+                      "rounded-xl px-5 py-[9px] font-manrope text-sm font-semibold uppercase shadow-md ring-1 ring-slate-200 dark:shadow-slate-950 dark:ring-slate-700 dark:active:bg-slate-800",
+                      newSet.warmup === false &&
+                        "bg-violet-400 text-white dark:bg-violet-500",
+                    )}
+                  >
+                    Working
+                  </button>
+                </div>
 
-            <input
-              required
-              disabled={isPending}
-              id="weight"
-              name="weight"
-              type="text"
-              inputMode="decimal"
-              placeholder="Weight"
-              className={twMerge(
-                "input-field",
-                "w-1/3 py-2 text-center",
-                error?.weight && "ring-red-500 dark:ring-red-500",
-              )}
-            />
-          </div>
+                <AnimatePresence initial={false}>
+                  {newSet.warmup !== undefined && (
+                    <motion.div
+                      initial={{ y: -32, opacity: 0, height: 0 }}
+                      animate={{
+                        y: 0,
+                        opacity: 1,
+                        height: "auto",
+                        transition: {
+                          duration: 0.4,
+                          delay: 0.04,
+                          ease: [0.36, 0.66, 0.04, 1],
+                        },
+                      }}
+                      exit={{
+                        opacity: 0,
+                        transition: {
+                          duration: 0.4,
+                          ease: [0.36, 0.66, 0.04, 1],
+                        },
+                      }}
+                    >
+                      <button
+                        type="button"
+                        onClick={async () => {
+                          await new Promise((resolve) =>
+                            setTimeout(resolve, 100),
+                          );
 
-          {(error?.reps || error?.weight) && (
-            <div className="space-y-2">
-              <ErrorComponent errorArr={error?.reps} />
-              <ErrorComponent errorArr={error?.weight} />
-            </div>
+                          setShowForm(true);
+                        }}
+                        className="flex w-full items-center justify-center gap-2 rounded-full bg-green-500 py-2 text-white shadow-md active:bg-green-400 dark:bg-green-600 dark:active:bg-green-500"
+                      >
+                        <p className="text-lg font-bold">Next</p>
+
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                          />
+                        </svg>
+                      </button>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            </motion.div>
+          ) : (
+            <motion.div
+              key="reps-and-weights-fields"
+              initial={{ opacity: 0, x: 64 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.4,
+                  ease: [0.36, 0.66, 0.04, 1],
+                },
+              }}
+              exit={{
+                opacity: 0,
+                x: 64,
+                transition: {
+                  duration: 0.3,
+                  ease: [0.36, 0.66, 0.04, 1],
+                },
+              }}
+            >
+              <div className="space-y-4 group-disabled:opacity-50">
+                <p className="text-center text-xl font-semibold text-slate-500 dark:text-slate-300">
+                  Please fill the empty fields
+                </p>
+
+                <div
+                  className={twMerge(
+                    "flex justify-evenly pb-4",
+                    (error?.reps || error?.weight) && "pb-0",
+                  )}
+                >
+                  <input
+                    required
+                    disabled={isPending}
+                    id="reps"
+                    name="reps"
+                    type="text"
+                    placeholder="Reps"
+                    onChange={(e) =>
+                      setNewSet({ ...newSet, reps: e.target.value })
+                    }
+                    className={twMerge(
+                      "input-field",
+                      "w-1/3 py-2 text-center dark:bg-slate-800",
+                      error?.reps && "ring-red-500 dark:ring-red-500",
+                    )}
+                  />
+
+                  <input
+                    required
+                    disabled={isPending}
+                    id="weight"
+                    name="weight"
+                    type="text"
+                    inputMode="decimal"
+                    placeholder="Weight"
+                    onChange={(e) =>
+                      setNewSet({ ...newSet, weight: e.target.value })
+                    }
+                    className={twMerge(
+                      "input-field",
+                      "w-1/3 py-2 text-center dark:bg-slate-800",
+                      error?.weight && "ring-red-500 dark:ring-red-500",
+                    )}
+                  />
+                </div>
+
+                {(error?.reps || error?.weight) && (
+                  <div className="space-y-2 pb-4">
+                    <ErrorComponent errorArr={error?.reps} />
+                    <ErrorComponent errorArr={error?.weight} />
+                  </div>
+                )}
+                <div className="flex gap-4">
+                  <button
+                    type="button"
+                    onClick={async () => {
+                      await new Promise((resolve) => setTimeout(resolve, 100));
+
+                      reset();
+                      setShowForm(false);
+                    }}
+                    className="rounded-full bg-slate-50 px-2 ring-1 ring-slate-300 active:scale-95 active:bg-slate-100 dark:bg-slate-800 dark:ring-slate-600 dark:active:bg-slate-700"
+                  >
+                    {ArrowLeftIcon}
+
+                    <p className="sr-only">Logout button</p>
+                  </button>
+
+                  <ModalSubmitButton
+                    label="Done"
+                    loading="Please wait..."
+                    pending={isPending}
+                    form="add-new-set"
+                    className="rounded-full py-2 text-lg font-bold shadow-md"
+                  />
+                </div>
+              </div>
+            </motion.div>
           )}
-        </div>
-
-        <ModalSubmitButton
-          label="Add"
-          loading="Adding..."
-          pending={isPending}
-          form="add-new-set"
-        />
+        </AnimatePresence>
       </fieldset>
-
-      <Drawer.Handle
-        preventCycle
-        className="mt-4 bg-slate-300 dark:bg-slate-600"
-      />
     </form>
   );
 };
