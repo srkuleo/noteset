0322e86d5e686b5ccc8aae1874d8ad5fca7b2f4b
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-18 19:50:26 +0200

    MODIFY: styling is now sent as prop since ConfirmLogOutButton is no longer styled initially

--- src/components/user/profile/LogOut.tsx
+++ src/components/user/profile/LogOut.tsx
@@ -46,7 +46,7 @@ export const Logout = () => {
             </Drawer.Title>
 
             <form action={logout} className="w-full">
-              <ConfirmLogOutButton />
+              <ConfirmLogOutButton className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800" />
             </form>
           </div>
 
