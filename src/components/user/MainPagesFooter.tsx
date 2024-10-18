9f98547b794a51fa98a4f5c1e1981e1b62d68f67
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-18 19:48:01 +0200

    MODIFY: no longer renders profile button, now renders add new workout button between home and logs - more logical to keep everything related to workouts in the same place

--- src/components/user/MainPagesFooter.tsx
+++ src/components/user/MainPagesFooter.tsx
@@ -2,56 +2,81 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
-import { twJoin } from "tailwind-merge";
-import {
-  HomeIcon,
-  ProfileIcon,
-  LogsIcon,
-} from "@/components/icons/user/navbar";
-
-type NavBarLinkType = {
-  href: "/home" | "/profile" | "/logs";
-  icon: React.ReactNode;
-  label: "Home" | "Logs" | "Profile";
-};
-
-const navBarLinks: NavBarLinkType[] = [
-  {
-    href: "/home",
-    icon: <HomeIcon strokeWidth={1.8} className="size-[30px]" />,
-    label: "Home",
-  },
-  { href: "/profile", icon: ProfileIcon, label: "Profile" },
-  { href: "/logs", icon: LogsIcon, label: "Logs" },
-];
+import { twMerge } from "tailwind-merge";
+import { HomeIcon, LogsIcon } from "@/components/icons/user/navbar";
+import { AddIcon } from "../icons/user/modify";
 
 export const MainPagesFooter = () => {
   const path = usePathname();
 
   return (
-    <nav className="fixed inset-x-0 bottom-0 flex justify-center gap-6 border-t border-slate-300/80 bg-white pb-7 text-slate-400 dark:border-slate-800 dark:bg-slate-900 dark:text-slate-500">
-      {navBarLinks.map((link) => (
-        <Link
-          key={link.href}
-          href={link.href === "/home" ? "/home?q=current" : link.href}
-          className={twJoin(
-            "relative rounded-full px-2 py-2 active:bg-slate-200 dark:active:bg-slate-700",
-            path === link.href && "text-green-500 dark:text-white",
+    <nav className="fixed inset-x-0 bottom-0 flex items-center justify-center gap-4 border-t border-slate-300/80 bg-white pb-7 pt-1 dark:border-slate-800 dark:bg-slate-900">
+      <Link
+        href="/home?q=current"
+        className={twMerge(
+          "flex flex-col items-center rounded-full px-3 py-1.5 active:bg-slate-200 dark:active:bg-slate-700",
+          path === "/home"
+            ? "text-green-500 dark:text-green-500"
+            : "text-slate-400 dark:text-slate-500",
+        )}
+      >
+        <HomeIcon
+          fill={path === "/home" ? "currentColor" : "transparent"}
+          strokeColor={path === "/home" ? "transparent" : "currentColor"}
+          strokeWidth={path === "/home" ? 0 : 1.2}
+          className="size-6"
+        />
+
+        <p
+          className={twMerge(
+            "text-center font-manrope text-[10px] uppercase",
+            path === "/home"
+              ? "font-extrabold text-green-500"
+              : "font-bold text-slate-400 dark:text-slate-500",
           )}
         >
-          {link.icon}
+          Home
+        </p>
 
-          {path === link.href && (
-            <motion.div
-              layoutId="active-page"
-              className="absolute bottom-0 h-[3px] w-8 rounded-full bg-green-500"
-            />
+        <p className="sr-only">Home button</p>
+      </Link>
+
+      <Link
+        href="/create"
+        className="flex items-center rounded-full bg-white p-2.5 shadow-md ring-1 ring-slate-300 transition active:scale-95 active:bg-slate-200 dark:bg-slate-800 dark:ring-slate-600 dark:active:bg-slate-600"
+      >
+        <AddIcon size={28} strokeWidth={2} />
+
+        <p className="sr-only">Add a new workout</p>
+      </Link>
+
+      <Link
+        href="/logs"
+        className={twMerge(
+          "flex flex-col items-center rounded-full px-3 py-1.5 active:bg-slate-200 dark:active:bg-slate-700",
+          path === "/logs"
+            ? "text-green-500 dark:text-green-500"
+            : "text-slate-400 dark:text-slate-500",
+        )}
+      >
+        <LogsIcon
+          fill={path === "/logs" ? "currentColor" : "transparent"}
+          strokeColor={path === "/logs" ? "transparent" : "currentColor"}
+          strokeWidth={path === "/logs" ? 0 : 1.2}
+          className="size-6"
+        />
+
+        <p
+          className={twMerge(
+            "text-center font-manrope text-[10px] uppercase",
+            path === "/logs" ? "font-extrabold" : "font-bold",
           )}
+        >
+          Logs
+        </p>
 
-          <p className="sr-only">{link.label} button</p>
-        </Link>
-      ))}
+        <p className="sr-only">Logs button</p>
+      </Link>
     </nav>
   );
 };
