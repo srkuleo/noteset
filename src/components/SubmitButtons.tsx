edd6cec154835b9f6f37d6240196e148d5288f2f
Srdjan <milicevic.srdjan0@gmail.com>
2024-10-18 19:40:49 +0200

    MODIFY: makes ConfirmLogOutButton unstyled - now can be properly reused

--- src/components/SubmitButtons.tsx
+++ src/components/SubmitButtons.tsx
@@ -48,15 +48,11 @@ export const ModalSubmitButton = ({
   );
 };
 
-export const ConfirmLogOutButton = () => {
+export const ConfirmLogOutButton = ({ className }: { className?: string }) => {
   const { pending } = useFormStatus();
 
   return (
-    <button
-      type="submit"
-      disabled={pending}
-      className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
-    >
+    <button type="submit" disabled={pending} className={className}>
       {pending ? "Logging Out..." : "Log Out"}
     </button>
   );
