2aee15388f8549bd28c3e13844398e62a144c154
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-15 00:13:02 +0100

    MODIFY: now acts as a wrapper component

--- src/components/landing/LandingPageBar.tsx
+++ src/components/landing/LandingPageBar.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
-import { Suspense } from "react";
 import { Logo } from "../icons/logo";
-import { LoadingLoginButtonSkeleton } from "../Loading";
-import { ProfileButton } from "../user/profile/ProfileButton";
 import { GitHubButton } from "../GitHubButton";
 
-export const LandingPageBar = () => {
+export const LandingPageBar = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
   return (
     <header className="border-b border-slate-400/40 px-6 py-2 dark:border-slate-800/90 md:border-b-0">
       <div className="flex items-center justify-between">
         <Link href="/">{Logo}</Link>
 
         <div className="flex items-center gap-4">
-          <Suspense fallback={<LoadingLoginButtonSkeleton />}>
-            <ProfileButton />
-          </Suspense>
+          {children}
 
           <GitHubButton />
         </div>
