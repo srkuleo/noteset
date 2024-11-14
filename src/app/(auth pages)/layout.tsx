e0b18de9528e0e98c5c76d389c0f74d97eb3dd07
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-14 23:36:30 +0100

    ADD: layout for auth pages

---
+++ src/app/(auth pages)/layout.tsx
@@ -0,0 +1,32 @@
+import { LandingPageBar } from "@/components/landing/LandingPageBar";
+import { UIMessage } from "@/components/landing/UIMessage";
+import { LoadingDataSpinner } from "@/components/Loading";
+import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
+
+export default async function AuthPagesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="fixed inset-x-0 top-0 bg-white pt-safe-top dark:bg-slate-900">
+        <LandingPageBar />
+
+        <UIMessage />
+      </div>
+
+      <main className="mt-safe-top flex px-8 pt-[81px] md:pt-[125px]">
+        <Suspense
+          fallback={
+            <LoadingDataSpinner message="Verifying session... Please wait" />
+          }
+        >
+          {children}
+        </Suspense>
+      </main>
+    </>
+  );
+}
