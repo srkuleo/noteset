85215342405fc587c72b35763f392218a440e996
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-15 00:23:05 +0100

    MODIFY: now acts as a wrapper component and is always rendered on top of the workouts/exercises

--- src/components/user/UserPagesHeader.tsx
+++ src/components/user/UserPagesHeader.tsx
@@ -1,14 +1,16 @@
 import { twMerge } from "tailwind-merge";
 import { Logo } from "../icons/logo";
-import { LoadingProfileButtonSkeleton } from "../Loading";
-import { ProfileButton } from "./profile/ProfileButton";
 import { GitHubButton } from "../GitHubButton";
 
-import { Suspense, type HTMLAttributes } from "react";
+import { type HTMLAttributes } from "react";
 
-export const UserPagesHeader = () => {
+export const UserPagesHeader = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
   return (
-    <header className="fixed inset-x-0 top-0 z-10 bg-white px-4 pt-safe-top dark:bg-slate-900">
+    <header className="fixed inset-x-0 top-0 z-20 bg-white px-4 pt-safe-top dark:bg-slate-900">
       <div className="pb-2 pt-2">
         <div className="flex items-center justify-between rounded-[28px] bg-white px-4 py-2 shadow-md ring-1 ring-slate-300/80 dark:bg-slate-800 dark:ring-slate-700">
           {Logo}
@@ -16,9 +18,7 @@ export const UserPagesHeader = () => {
           <div className="flex items-center gap-3">
             <GitHubButton />
 
-            <Suspense fallback={<LoadingProfileButtonSkeleton />}>
-              <ProfileButton />
-            </Suspense>
+            {children}
           </div>
         </div>
       </div>
@@ -36,7 +36,7 @@ export const UserPagesSubHeadingWrapper = ({
   return (
     <div
       className={twMerge(
-        "fixed inset-x-0 top-[77px] z-10 mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-900",
+        "fixed inset-x-0 top-[77px] z-20 mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-900",
         className,
       )}
     >
