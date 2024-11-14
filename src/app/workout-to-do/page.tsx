552519757e11e658b503ecf688c277074336ca72
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-14 23:45:17 +0100

    MODIFY: same as other pages, move SF to the DataFetching component, page now PPR static UI with proper fallback UIs for dynamic components

--- src/app/workout-to-do/page.tsx
+++ src/app/workout-to-do/page.tsx
@@ -1,23 +1,40 @@
-import { notFound, redirect } from "next/navigation";
-import { getAuth } from "@/util/actions/auth";
-import { getWorkoutByIdWithoutId } from "@/db/query";
-import { WorkoutToDoForm } from "@/components/workout-to-do/WorkoutToDoForm";
+import { Suspense } from "react";
+import { BackButtonModal } from "@/components/BackButtonModal";
+import { WorkoutToDoTitle } from "@/components/workout-to-do/WorkoutToDoTitle";
+import { WorkoutToDoTooltip } from "@/components/Tooltips";
+import { WorkoutToDoSkeleton } from "@/components/Loading";
+import { WorkoutToDoDataFetcher } from "@/components/workout-to-do/WorkoutToDoDataFetcher";
 
 export default async function WorkoutToDoPage({
   searchParams,
 }: {
   searchParams: { id: string };
 }) {
-  const { user } = await getAuth();
+  const workoutId = Number(searchParams.id);
 
-  if (!user) {
-    redirect("/login");
-  }
+  return (
+    <>
+      <header className="fixed inset-x-0 top-0 z-20 bg-gradient-to-r from-green-600 from-20% to-violet-600 pt-safe-top dark:from-green-700 dark:to-violet-700">
+        <div className="flex justify-between px-4 py-2">
+          <div className="flex items-center gap-4">
+            <BackButtonModal className="rounded-full p-1.5 text-white active:bg-slate-300/60 dark:active:bg-slate-300/50" />
 
-  const coercedId = Number(searchParams.id);
-  const workoutToDo = await getWorkoutByIdWithoutId(coercedId);
+            <Suspense
+              fallback={
+                <div className="mr-auto h-7 w-24 animate-pulse rounded-xl bg-white/20" />
+              }
+            >
+              <WorkoutToDoTitle workoutId={workoutId} />
+            </Suspense>
+          </div>
 
-  if (!workoutToDo) notFound();
+          <WorkoutToDoTooltip />
+        </div>
+      </header>
 
-  return <WorkoutToDoForm workoutToDo={workoutToDo} />;
+      <Suspense fallback={<WorkoutToDoSkeleton />}>
+        <WorkoutToDoDataFetcher workoutId={workoutId} />
+      </Suspense>
+    </>
+  );
 }
