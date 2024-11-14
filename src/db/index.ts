36a178fe8240a907d038e6db214a26f20d52148c
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-14 23:32:25 +0100

    MODIFY: adds new way to connect to neon db, removes lucia adapter - no longer in use

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,8 @@
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL);
-
-export const db = drizzle(sql, { schema: schema });
-
-export const adapter = new DrizzlePostgreSQLAdapter(
-  db,
-  schema.sessions,
-  schema.users,
-);
+export const db = drizzle({
+  connection: process.env.DATABASE_URL,
+  schema: schema,
+  casing: "snake_case",
+});
