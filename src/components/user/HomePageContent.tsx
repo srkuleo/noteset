165b34a0691e89cc5e446a10b962c7b53e394a10
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-15 00:19:56 +0100

    MODIFY: improves the UI of workout cards, add swipe actions and new state for tracking which workout to preview

--- src/components/user/HomePageContent.tsx
+++ src/components/user/HomePageContent.tsx
@@ -2,16 +2,54 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { unarchiveWorkout } from "@/util/actions/workout";
 import { showToast } from "../Toasts";
+import {
+  BUTTON_TIMEOUT,
+  SWIPE_AND_DRAWER_TIMEOUT,
+  timeout,
+} from "@/util/utils";
 import { EmptyIcon } from "../icons/user/warning";
 import { RemoveWorkoutModal } from "./RemoveWorkoutModal";
+import { SwipeAction } from "../swipe/SwipeAction";
 import { StatusIndicator } from "../StatusIndicator";
-import { PreviewWorkoutButtonDrawer } from "./PreviewWorkoutButtonDrawer";
-import { EditWorkoutIcon, TrashBinIcon } from "../icons/user/modify";
+import { ShowIcon, SolidShowIcon } from "../icons/user/preview";
+import {
+  SolidEditIcon,
+  SolidTrashBinIcon,
+  TrashBinIcon,
+} from "../icons/user/modify";
+import { PreviewWorkoutDrawer } from "./PreviewWorkoutDrawer";
 
 import type { PartialWorkoutType } from "@/db/schema";
 import type { WorkoutStatusType } from "@/util/types";
+
+const initWorkoutToRemove: {
+  title: string;
+  id: number;
+  status: WorkoutStatusType;
+} = {
+  title: "",
+  id: 0,
+  status: "current",
+};
+
+const initWorkoutToPreview: PartialWorkoutType = {
+  id: 0,
+  title: "",
+  description: "",
+  exercises: [],
+  status: "current",
+};
+
+type WorkoutSwipeActions = {
+  openPreviewDrawer: () => void;
+  openRemoveModal: () => void;
+  workoutToEditId: number;
+  workoutToPreview: () => void;
+  workoutToRemove: () => void;
+};
 
 export const HomePageContent = ({
   workouts,
@@ -21,11 +59,10 @@ export const HomePageContent = ({
   status: WorkoutStatusType;
 }) => {
   const [openRemoveModal, setOpenRemoveModal] = useState(false);
-  const [workoutToRemove, setWorkoutToRemove] = useState({
-    title: "",
-    id: 0,
-    status: "current" as WorkoutStatusType,
-  });
+  const [openPreviewModal, setOpenPreviewModal] = useState(false);
+  const [workoutToRemove, setWorkoutToRemove] = useState(initWorkoutToRemove);
+  const [workoutToPreview, setWorkoutToPreview] =
+    useState(initWorkoutToPreview);
 
   return (
     <>
@@ -35,6 +72,12 @@ export const HomePageContent = ({
         workoutToRemove={workoutToRemove}
       />
 
+      <PreviewWorkoutDrawer
+        workout={workoutToPreview}
+        open={openPreviewModal}
+        setOpen={setOpenPreviewModal}
+      />
+
       {workouts.length === 0 ? (
         <main className="mt-safe-top flex flex-col justify-center px-6 pb-[100px] pt-[157px]">
           <EmptyPage status={status} />
@@ -42,102 +85,100 @@ export const HomePageContent = ({
       ) : (
         <main className="mt-safe-top space-y-4 px-6 pb-[100px] pt-[157px]">
           {workouts.map((workout) => (
-            <div
-              key={workout.id}
-              className="flex w-full flex-col gap-4 rounded-xl border border-slate-300/80 bg-white px-4 py-6 shadow-md dark:border-slate-700 dark:bg-slate-800"
-            >
-              <div className="space-y-1.5 px-1">
-                <div className="flex justify-between gap-4">
+            <SwipeAction.Root key={workout.id}>
+              <SwipeAction.Trigger className="flex items-center justify-between rounded-xl border border-slate-300/80 bg-white p-6 shadow-md dark:border-slate-700 dark:bg-slate-800">
+                <div className="w-3/5 space-y-1">
                   <p className="text-pretty font-manrope text-lg font-bold uppercase dark:text-slate-300">
                     {workout.title}
                   </p>
 
-                  <StatusIndicator status={workout.status} />
+                  <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
+                    {workout.description
+                      ? workout.description
+                      : "Description not provided"}
+                  </p>
                 </div>
 
-                <p className="text-pretty text-sm font-semibold italic leading-none text-slate-400/80 dark:text-slate-400/60">
-                  {workout.description
-                    ? workout.description
-                    : "Description not provided"}
-                </p>
-              </div>
+                <div className="flex flex-col items-center gap-4">
+                  <StatusIndicator
+                    status={workout.status}
+                    className="px-3 py-2 ring-slate-300/80 dark:bg-slate-900 dark:ring-slate-700"
+                  />
 
-              <div className="h-[1px] bg-green-200 dark:bg-green-900/70" />
-
-              <div className="flex gap-2 px-1 [&>*:nth-child(2)]:mr-auto">
-                <PreviewWorkoutButtonDrawer
-                  workout={workout}
-                  className="px-2"
-                  size={5}
-                />
-
-                {workout.status === "current" ? (
-                  <Link
-                    href={`/workout-to-do?id=${workout.id}`}
-                    scroll={false}
-                    className="flex rounded-lg bg-gradient-to-r from-violet-400 to-violet-500 px-4 py-1 font-bold text-white shadow-md transition active:scale-90 active:from-violet-300 active:to-violet-400 dark:from-violet-500 dark:to-violet-600 dark:active:from-violet-700 dark:active:to-violet-800"
-                  >
-                    Start
-                    <p className="sr-only">Start this workout</p>
-                  </Link>
-                ) : (
-                  <form
-                    action={async () => {
-                      const res = await unarchiveWorkout(
-                        workout.id,
-                        workout.title,
-                      );
-
-                      if (res.status === "success-redirect") {
-                        showToast(
-                          res.message,
-                          "/home?q=current",
-                          "See current",
-                        );
-                      } else {
-                        showToast(res.message);
-                      }
-                    }}
-                  >
-                    <button
-                      type="submit"
-                      className="flex rounded-lg bg-gradient-to-r from-blue-400 to-blue-500 px-4 py-1 font-bold text-white shadow-md transition active:scale-90 active:from-blue-300 active:to-blue-400 dark:from-blue-500 dark:to-blue-600 dark:active:from-blue-700 dark:active:to-blue-800"
+                  {workout.status === "current" ? (
+                    //Start button on current workouts
+                    <Link
+                      href={`/workout-to-do?id=${workout.id}`}
+                      scroll={false}
+                      className="flex rounded-lg bg-gradient-to-r from-violet-400 to-violet-500 px-4 py-1 font-bold text-white shadow-md transition active:scale-90 active:from-violet-300 active:to-violet-400 dark:from-violet-500 dark:to-violet-600 dark:active:from-violet-700 dark:active:to-violet-800"
                     >
-                      Set as Current
-                    </button>
-                  </form>
-                )}
+                      Start
+                      <p className="sr-only">Start this workout</p>
+                    </Link>
+                  ) : (
+                    //Button to unarchive an archived workout
+                    <form
+                      action={async () => {
+                        const res = await unarchiveWorkout(
+                          workout.id,
+                          workout.title,
+                        );
 
-                {workout.status === "current" && (
-                  <Link
-                    href={`/edit?id=${workout.id}`}
-                    scroll={false}
-                    className="rounded-full p-1.5 text-green-500 shadow-md ring-1 ring-inset ring-slate-300 active:scale-95 active:bg-slate-200 dark:shadow-slate-900 dark:ring-slate-600 dark:active:bg-slate-700"
-                  >
-                    {EditWorkoutIcon}
-                    <span className="sr-only">Edit workout</span>
-                  </Link>
-                )}
+                        if (res.status === "success-redirect") {
+                          showToast(
+                            res.message,
+                            "/home?q=current",
+                            "See current",
+                          );
+                        } else {
+                          showToast(res.message);
+                        }
+                      }}
+                    >
+                      <button
+                        type="submit"
+                        className="flex rounded-lg bg-gradient-to-r from-blue-400 to-blue-500 px-3 py-1.5 text-sm font-bold text-white shadow-md transition active:scale-90 active:from-blue-300 active:to-blue-400 dark:from-blue-500 dark:to-blue-600 dark:active:from-blue-700 dark:active:to-blue-800"
+                      >
+                        Unarchive
+                      </button>
+                    </form>
+                  )}
+                </div>
+              </SwipeAction.Trigger>
 
-                <button
-                  type="button"
-                  onClick={async () => {
-                    await new Promise((resolve) => setTimeout(resolve, 100));
-
+              {workout.status === "current" && (
+                //Current workouts swipe actions - flex row design
+                <CurrentWorkoutsSwipeActions
+                  openPreviewDrawer={() => setOpenPreviewModal(true)}
+                  openRemoveModal={() => setOpenRemoveModal(true)}
+                  workoutToEditId={workout.id}
+                  workoutToPreview={() => setWorkoutToPreview(workout)}
+                  workoutToRemove={() =>
                     setWorkoutToRemove({
                       id: workout.id,
                       title: workout.title,
                       status: workout.status,
-                    });
-                    setOpenRemoveModal(true);
-                  }}
-                  className="rounded-full p-1.5 text-red-500 shadow-md ring-1 ring-inset ring-slate-300 active:scale-95 active:bg-slate-200 dark:text-red-400 dark:shadow-slate-900 dark:ring-slate-600 dark:active:bg-slate-700"
-                >
-                  <TrashBinIcon className="size-5" strokeWidth={1.2} />
-                  <span className="sr-only">Remove workout</span>
-                </button>
-              </div>
-            </div>
+                    })
+                  }
+                />
+              )}
+
+              {workout.status === "archived" && (
+                //Archived workouts swipe actions - flex column design
+                <ArchivedWorkoutsSwipeActions
+                  openPreviewDrawer={() => setOpenPreviewModal(true)}
+                  openRemoveModal={() => setOpenRemoveModal(true)}
+                  workoutToPreview={() => setWorkoutToPreview(workout)}
+                  workoutToRemove={() =>
+                    setWorkoutToRemove({
+                      id: workout.id,
+                      title: workout.title,
+                      status: workout.status,
+                    })
+                  }
+                />
+              )}
+            </SwipeAction.Root>
           ))}
         </main>
       )}
@@ -152,31 +193,142 @@ const EmptyPage = ({ status }: { status: WorkoutStatusType }) => {
         {EmptyIcon}
       </div>
 
-      <div className="space-y-4 text-center">
-        {status === "archived" ? (
+      {status === "archived" ? (
+        <div className="max-w-[80%] space-y-4 text-center">
           <h3>Nothing in archive</h3>
-        ) : (
-          <h3>Seems like you haven&apos;t created any workout yet</h3>
-        )}
-
-        {status === "archived" ? (
-          <p className="px-2 font-semibold italic text-slate-400/85">
+          <p className="font-semibold italic text-slate-400/85">
             Tap the{" "}
-            <span className="font-bold uppercase text-slate-800 dark:text-white">
+            <span className="font-bold uppercase text-slate-500 dark:text-white">
               C
             </span>{" "}
             button to load current workouts
           </p>
-        ) : (
+        </div>
+      ) : (
+        <div className="max-w-[90%] space-y-4 text-center">
+          <h3>Seems like you haven&apos;t created any workout yet</h3>
           <p className="font-semibold italic text-slate-400/85">
             Tap the{" "}
-            <span className="font-bold uppercase text-slate-800 dark:text-white">
+            <span className="font-bold uppercase text-slate-500 dark:text-white">
               plus
             </span>{" "}
             button to create one
           </p>
-        )}
-      </div>
+        </div>
+      )}
     </div>
+  );
+};
+
+const CurrentWorkoutsSwipeActions = ({
+  openPreviewDrawer,
+  openRemoveModal,
+  workoutToEditId,
+  workoutToPreview,
+  workoutToRemove,
+}: WorkoutSwipeActions) => {
+  const router = useRouter();
+
+  return (
+    <SwipeAction.Actions
+      wrapperClassName="bg-red-200 dark:bg-red-600 rounded-xl"
+      className="w-2/3"
+    >
+      <SwipeAction.Action
+        onClick={async () => {
+          await timeout(SWIPE_AND_DRAWER_TIMEOUT);
+
+          workoutToRemove();
+          openRemoveModal();
+        }}
+        className="flex w-1/3 flex-col items-center justify-center gap-1 bg-red-200 text-red-600 dark:bg-red-600 dark:text-white"
+      >
+        <SolidTrashBinIcon
+          fill="currentColor"
+          stroke="transparent"
+          strokeWidth={0}
+          className="size-7"
+        />
+        <p className="text-[11px] font-semibold uppercase">Remove</p>
+        <span className="sr-only">Remove workout</span>
+      </SwipeAction.Action>
+
+      <SwipeAction.Action
+        onClick={async () => {
+          await timeout(SWIPE_AND_DRAWER_TIMEOUT);
+
+          router.push(`/edit-workout?id=${workoutToEditId}`);
+        }}
+        className="flex w-1/3 flex-col items-center justify-center gap-1 bg-green-200 text-green-600 dark:bg-green-600 dark:text-white"
+      >
+        <SolidEditIcon
+          fill="currentColor"
+          stroke="transparent"
+          strokeWidth={0}
+          className="size-7"
+        />
+        <p className="text-[11px] font-semibold uppercase">Edit</p>
+        <span className="sr-only">Edit workout</span>
+      </SwipeAction.Action>
+
+      <SwipeAction.Action
+        onClick={async () => {
+          await timeout(BUTTON_TIMEOUT);
+
+          workoutToPreview();
+          openPreviewDrawer();
+        }}
+        className="flex w-1/3 flex-col items-center justify-center gap-1 bg-violet-200 text-violet-600 dark:bg-violet-600 dark:text-white"
+      >
+        <SolidShowIcon
+          fill="currentColor"
+          stroke="transparent"
+          strokeWidth={0}
+          className="size-7"
+        />
+        <p className="text-[11px] font-semibold uppercase">Preview</p>
+        <p className="sr-only">Preview workout</p>
+      </SwipeAction.Action>
+    </SwipeAction.Actions>
+  );
+};
+const ArchivedWorkoutsSwipeActions = ({
+  openPreviewDrawer,
+  openRemoveModal,
+  workoutToPreview,
+  workoutToRemove,
+}: Omit<WorkoutSwipeActions, "workoutToEditId">) => {
+  return (
+    <SwipeAction.Actions
+      wrapperClassName="bg-violet-100 dark:bg-violet-500 rounded-xl"
+      className="flex-col"
+    >
+      <SwipeAction.Action
+        onClick={async () => {
+          await timeout(BUTTON_TIMEOUT);
+
+          workoutToPreview();
+          openPreviewDrawer();
+        }}
+        className="flex grow items-center justify-center border-b border-violet-500 px-4 text-violet-500 dark:border-white dark:text-white"
+      >
+        <ShowIcon strokeWidth={1.5} className="size-7" />
+        <p className="sr-only">Preview workout</p>
+      </SwipeAction.Action>
+
+      <SwipeAction.Action
+        onClick={async () => {
+          await timeout(SWIPE_AND_DRAWER_TIMEOUT);
+
+          workoutToRemove();
+          openRemoveModal();
+        }}
+        className="flex grow items-center justify-center px-4 text-red-500 dark:text-white"
+      >
+        <TrashBinIcon strokeWidth={1.5} className="size-7" />
+
+        <span className="sr-only">Remove workout</span>
+      </SwipeAction.Action>
+    </SwipeAction.Actions>
   );
 };
