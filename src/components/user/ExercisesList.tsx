a56794b746689b3938ac608013d3fd75bd06dde2
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-15 00:16:41 +0100

    MODIFY: adds swipe actions to exercises, improves UI for more consistent look

--- src/components/user/ExercisesList.tsx
+++ src/components/user/ExercisesList.tsx
@@ -22,13 +22,19 @@ import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 import { EditExerciseDrawer } from "./EditExerciseDrawer";
 import { RemoveExerciseModal } from "./RemoveExerciseModal";
 import { ErrorComponent } from "../ErrorComponent";
-import {
-  DragExerciseIcon,
-  EditExerciseIcon,
-  RemoveExerciseIcon,
-} from "../icons/user/modify";
+import { DragExerciseIcon, EditIcon, TrashBinIcon } from "../icons/user/modify";
 
 import type { ExerciseType, CreateWorkoutType } from "@/util/types";
+import { SwipeAction } from "../swipe/SwipeAction";
+
+type ExerciseListProps = {
+  workout: CreateWorkoutType;
+  exercisesError: string[] | undefined;
+  editForm?: boolean;
+  setWorkout: (workout: CreateWorkoutType) => void;
+  editExercises: (editedExercise: ExerciseType) => void;
+  removeExercise: (id: string) => void;
+};
 
 export const ExercisesList = ({
   workout,
@@ -37,14 +43,7 @@ export const ExercisesList = ({
   setWorkout,
   editExercises,
   removeExercise,
-}: {
-  workout: CreateWorkoutType;
-  exercisesError: string[] | undefined;
-  editForm?: boolean;
-  setWorkout: (workout: CreateWorkoutType) => void;
-  editExercises: (editedExercise: ExerciseType) => void;
-  removeExercise: (id: string) => void;
-}) => {
+}: ExerciseListProps) => {
   const [openEditDrawer, setOpenEditDrawer] = useState(false);
   const [openRemoveModal, setOpenRemoveModal] = useState(false);
   const [currentExercise, setCurrentExercise] = useState<ExerciseType>({
@@ -184,103 +183,58 @@ const ExerciseCard = ({
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      className="flex w-full flex-col gap-3 rounded-lg bg-white p-4 shadow-md ring-1 ring-slate-400/40 dark:bg-slate-800 dark:ring-slate-600"
-    >
-      <div className="flex items-center gap-2 border-b border-slate-200 pb-3 dark:border-slate-700 [&>*:nth-child(2)]:mr-auto">
-        <button
-          {...attributes}
-          {...listeners}
-          type="button"
-          className="cursor-move touch-none rounded-lg px-2 py-1 active:bg-slate-200 active:dark:bg-slate-600"
-        >
-          {DragExerciseIcon}
-        </button>
-
-        <p className="font-bold uppercase dark:text-slate-200">
-          {exercise.name}
-        </p>
-
-        <button
-          type="button"
-          onClick={() => {
-            setCurrentExercise({ ...exercise });
-            openEditDrawer();
-          }}
-          className="rounded-full p-1.5 text-green-500 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:shadow-slate-900 dark:ring-slate-600"
-        >
-          {EditExerciseIcon}
-          <span className="sr-only">Edit exercise</span>
-        </button>
-
-        <button
-          type="button"
-          onClick={() => {
-            setCurrentExercise({ ...exercise });
-            openRemoveModal();
-          }}
-          className="rounded-full p-1.5 text-red-500 shadow-md ring-1 ring-inset ring-slate-300 transition focus:outline-none active:scale-95 dark:text-red-400 dark:shadow-slate-900 dark:ring-slate-600"
-        >
-          {RemoveExerciseIcon}
-          <span className="sr-only">Remove exercise</span>
-        </button>
-      </div>
-
-      <div className="flex flex-col gap-6">
-        {exercise.sets.some((set) => set.warmup) && (
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-xs font-semibold uppercase text-slate-400/90 dark:text-slate-400">
-              Warmup sets
+    <SwipeAction.Root>
+      <div ref={setNodeRef} style={style}>
+        <SwipeAction.Trigger className="flex items-center gap-3 rounded-lg bg-white p-6 shadow-md ring-1 ring-slate-400/40 dark:bg-slate-800 dark:ring-slate-600">
+          <div className="flex flex-1 flex-col gap-3">
+            <p className="pl-1 font-bold uppercase dark:text-slate-200">
+              {exercise.name}
             </p>
 
-            {exercise.sets.map((set) =>
-              set.warmup ? (
-                <div
-                  key={set.id}
-                  className="flex w-full justify-center gap-3 text-sm"
-                >
-                  <p className="w-1/5 text-right">{set.reps}</p>
-
-                  <div className="w-[1px] bg-slate-200 dark:bg-slate-700" />
-
-                  <p className="w-1/5 text-left">{set.weight} kg</p>
-                </div>
-              ) : null,
-            )}
-          </div>
-        )}
-
-        {exercise.sets.some((set) => !set.warmup) && (
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-xs font-semibold uppercase text-slate-400/90 dark:text-slate-400">
-              Working sets
+            <p className="border-t border-slate-200 pl-1 pt-3 text-sm italic text-slate-400 dark:border-slate-700 dark:text-slate-500">
+              {exercise.note ? exercise.note : "..."}
             </p>
-
-            {exercise.sets.map((set) =>
-              !set.warmup ? (
-                <div
-                  key={set.id}
-                  className="flex w-full justify-center gap-3 text-sm"
-                >
-                  <p className="w-1/5 text-right">{set.reps}</p>
-
-                  <div className="w-[1px] bg-slate-200 dark:bg-slate-700" />
-
-                  <p className="w-1/5 text-left">{set.weight} kg</p>
-                </div>
-              ) : null,
-            )}
           </div>
-        )}
-      </div>
 
-      {exercise.note && (
-        <p className="border-t border-slate-200 pl-1 pt-3 text-sm italic text-slate-400 dark:border-slate-700 dark:text-slate-500">
-          {exercise.note}
-        </p>
-      )}
-    </div>
+          <button
+            {...attributes}
+            {...listeners}
+            type="button"
+            className="cursor-move touch-none rounded-lg px-2 py-1 active:bg-slate-200 active:dark:bg-slate-600"
+          >
+            {DragExerciseIcon}
+          </button>
+        </SwipeAction.Trigger>
+
+        <SwipeAction.Actions
+          wrapperClassName="bg-violet-100 dark:bg-violet-500 rounded-xl"
+          className="flex-col"
+        >
+          <SwipeAction.Action
+            type="button"
+            onClick={() => {
+              setCurrentExercise({ ...exercise });
+              openEditDrawer();
+            }}
+            className="flex grow items-center justify-center border-b border-violet-500 px-4 text-green-500 dark:border-white dark:text-green-300"
+          >
+            <EditIcon strokeWidth={1.5} className="size-6" />
+            <span className="sr-only">Edit exercise</span>
+          </SwipeAction.Action>
+
+          <SwipeAction.Action
+            type="button"
+            onClick={() => {
+              setCurrentExercise({ ...exercise });
+              openRemoveModal();
+            }}
+            className="flex grow items-center justify-center px-4 text-red-500 dark:text-red-300"
+          >
+            <TrashBinIcon strokeWidth={1.5} className="size-6" />
+            <span className="sr-only">Remove exercise</span>
+          </SwipeAction.Action>
+        </SwipeAction.Actions>
+      </div>
+    </SwipeAction.Root>
   );
 };
