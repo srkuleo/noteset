e534b84916e64438c09a223192e1a7cfd2692f6e
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-26 13:55:53 +0100

    MODIFY: no longer renders only one HomePageContent component, now separates in 2 different components based on the queryParam ensuring always fresh state when switching between archived and current workouts, easier to maintain 2 different components with different swipe actions, WorkoutListShell now reusable between both components

--- src/components/user/home/HomePageDataFetcher.tsx
+++ src/components/user/home/HomePageDataFetcher.tsx
@@ -1,9 +1,10 @@
 import { redirect } from "next/navigation";
 import { getAuthSession } from "@/util/session";
 import { getUserWorkouts } from "@/db/query";
-import { HomePageContent } from "./HomePageContent";
 
 import type { WorkoutStatusType } from "@/util/types";
+import { ArchivedWorkoutsList } from "./ArchivedWorkoutsList";
+import { CurrentWorkoutsList } from "./CurrentWorkoutsList";
 
 export const HomePageDataFetcher = async ({
   queryParam,
@@ -18,5 +19,21 @@ export const HomePageDataFetcher = async ({
 
   const workouts = await getUserWorkouts(queryParam);
 
-  return <HomePageContent workouts={workouts} status={queryParam} />;
+  if (queryParam === "archived") {
+    return (
+      <ArchivedWorkoutsList
+        key={queryParam}
+        workouts={workouts}
+        status={queryParam}
+      />
+    );
+  }
+
+  return (
+    <CurrentWorkoutsList
+      key={queryParam}
+      workouts={workouts}
+      status={queryParam}
+    />
+  );
 };
---
+++ src/components/user/home/WorkoutsListShell.tsx
@@ -0,0 +1,41 @@
+import { EmptyIcon } from "@/components/icons/user/warning";
+
+import type { WorkoutStatusType } from "@/util/types";
+
+export const WorkoutsListShell = ({
+  status,
+}: {
+  status: WorkoutStatusType;
+}) => {
+  return (
+    <div className="flex flex-col items-center gap-8 pb-18">
+      <div className="text-slate-400/60 dark:text-slate-700/80">
+        {EmptyIcon}
+      </div>
+
+      {status === "archived" ? (
+        <div className="max-w-[80%] space-y-4 text-center">
+          <h3>Nothing in archive</h3>
+          <p className="font-semibold italic text-slate-400/85">
+            Tap the{" "}
+            <span className="font-bold uppercase text-slate-500 dark:text-white">
+              C
+            </span>{" "}
+            button to load current workouts
+          </p>
+        </div>
+      ) : (
+        <div className="max-w-[90%] space-y-4 text-center">
+          <h3>Seems like you haven&apos;t created any workout yet</h3>
+          <p className="font-semibold italic text-slate-400/85">
+            Tap the{" "}
+            <span className="font-bold uppercase text-slate-500 dark:text-white">
+              plus
+            </span>{" "}
+            button to create one
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
