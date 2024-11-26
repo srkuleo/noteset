32fec137712a9cd36f383c14becbe1d948e3408c
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-26 13:32:15 +0100

    MODIFY: Spinner is now resuable component, could be added as pending state on buttons, forms etc.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,20 +2,26 @@ import { ThemeIcon } from "./icons/theme";
 import { TimeIcon } from "./icons/user/profile/time";
 import { AddIcon } from "./icons/user/modify";
 
+export const Spinner = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        d="M10 3C6.13401 3 3 6.13401 3 10C3 10.2761 2.77614 10.5 2.5 10.5C2.22386 10.5 2 10.2761 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.72386 18 9.5 17.7761 9.5 17.5C9.5 17.2239 9.72386 17 10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3Z"
+        fill="#212121"
+      />
+    </svg>
+  );
+};
+
 export const LoadingDataSpinner = ({ message }: { message: string }) => {
   return (
     <div className="flex grow flex-col items-center justify-center gap-2 pb-28 md:pb-36">
-      <svg
-        className="size-8 animate-spin text-slate-300 dark:text-slate-500"
-        stroke="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M10 3C6.13401 3 3 6.13401 3 10C3 10.2761 2.77614 10.5 2.5 10.5C2.22386 10.5 2 10.2761 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.72386 18 9.5 17.7761 9.5 17.5C9.5 17.2239 9.72386 17 10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3Z"
-          fill="#212121"
-        />
-      </svg>
+      <Spinner className="size-8 animate-spin text-slate-300 dark:text-slate-500" />
 
       <p className="italic text-slate-500 dark:text-slate-200">{message}</p>
     </div>
