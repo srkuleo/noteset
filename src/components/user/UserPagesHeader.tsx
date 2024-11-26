8be86d2bd61b975bc1295869d7fd52fdfce593d6
Srdjan <milicevic.srdjan0@gmail.com>
2024-11-26 13:52:09 +0100

    MODIFY: add bigger z index to always be on top of everything, except modals and drawers

--- src/components/user/UserPagesHeader.tsx
+++ src/components/user/UserPagesHeader.tsx
@@ -10,7 +10,7 @@ export const UserPagesHeader = ({
   children?: React.ReactNode;
 }) => {
   return (
-    <header className="fixed inset-x-0 top-0 z-20 bg-white px-4 pt-safe-top dark:bg-slate-900">
+    <header className="fixed inset-x-0 top-0 z-[9990] bg-white px-4 pt-safe-top dark:bg-slate-900">
       <div className="pb-2 pt-2">
         <div className="flex items-center justify-between rounded-[28px] bg-white px-4 py-2 shadow-md ring-1 ring-slate-300/80 dark:bg-slate-800 dark:ring-slate-700">
           {Logo}
@@ -36,7 +36,7 @@ export const UserPagesSubHeadingWrapper = ({
   return (
     <div
       className={twMerge(
-        "fixed inset-x-0 top-[77px] z-20 mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-900",
+        "fixed inset-x-0 top-[77px] z-[9990] mt-safe-top flex items-center justify-between border-b border-slate-300/80 bg-white px-6 pb-4 pt-2 dark:border-slate-800 dark:bg-slate-900",
         className,
       )}
     >
