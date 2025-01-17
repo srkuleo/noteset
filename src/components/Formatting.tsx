4990965901157065bc7ac05fcf3fc7dd5c20de82
Srdjan <milicevic.srdjan0@gmail.com>
2025-01-17 23:51:10 +0100

    MODIFY: FormatWorkoutDuration new font styling and better naming of args

--- src/components/Formatting.tsx
+++ src/components/Formatting.tsx
@@ -1,35 +1,35 @@
 import type { TimeFormatType } from "@/util/types";
 
 export const FormatWorkoutDuration = ({
-  timeFormat,
   duration,
+  selectedFormat,
 }: {
-  timeFormat: TimeFormatType;
   duration: number;
+  selectedFormat: TimeFormatType;
 }) => {
-  if (timeFormat === "Hours and minutes" || timeFormat === "default") {
+  if (selectedFormat === "Hours and minutes" || selectedFormat === "default") {
     const minutes = Math.floor(duration % 60);
     const hours = Math.floor(duration / 60);
 
     if (hours === 0) {
       return (
         <p>
-          <span className="text-lg font-bold">{minutes}</span> min
+          <span className="font-bold">{minutes}</span> min
         </p>
       );
     }
 
     return (
       <p>
-        <span className="text-lg font-bold">{hours}</span> h{" "}
-        <span className="text-lg font-bold">{minutes}</span> min
+        <span className="font-bold">{hours}</span> h{" "}
+        <span className="font-bold">{minutes}</span> min
       </p>
     );
   }
 
   return (
     <p>
-      <span className="text-lg font-bold">{duration}</span> min
+      <span className="font-bold">{duration}</span> min
     </p>
   );
 };
