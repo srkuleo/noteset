ce2d7a1f57bc0df8a86d68b2cd402101b7d44436
Srdjan <milicevic.srdjan0@gmail.com>
2025-06-09 10:37:29 +0200

    ADD: separate routes for current and archived workouts with separate suspense barriers triggered on every request, resulting in faster response and better UI between switching routes

---
+++ src/app/(user)/(main pages)/(workouts)/archived-workouts/page.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from "react";
+import { HomePageSkeleton } from "@/components/Loading";
+import { ArchivedWorkoutPageDataFetcher } from "@/components/user/home/ArchivedWorkoutPageDataFetcher";
+
+export default async function ArchivedWorkoutsPage() {
+  return (
+    <Suspense fallback={<HomePageSkeleton />}>
+      <ArchivedWorkoutPageDataFetcher />
+    </Suspense>
+  );
+}
---
+++ src/app/(user)/(main pages)/(workouts)/home/page.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from "react";
+import { HomePageSkeleton } from "@/components/Loading";
+import { CurrentWorkoutPageDataFetcher } from "@/components/user/home/CurrentWorkoutPageDataFetcher";
+
+export default async function HomePage() {
+  return (
+    <Suspense fallback={<HomePageSkeleton />}>
+      <CurrentWorkoutPageDataFetcher />
+    </Suspense>
+  );
+}
