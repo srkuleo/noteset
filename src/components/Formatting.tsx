99747254f53c7f080db4e12e27f76a519e0adc3d
Srdjan <milicevic.srdjan0@gmail.com>
2025-08-10 12:31:15 +0200

    MODIFY: extract formatDate util function to util.ts and improves readability of FormatDate component

--- src/components/Formatting.tsx
+++ src/components/Formatting.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge";
+import { formatDate } from "@/util/utils";
 
 import type { TimeFormatType } from "@/util/types";
 
@@ -61,25 +62,13 @@ export const FormatDate = ({
   withDayOfTheWeek,
   className,
 }: {
-  date: Date;
+  date: Date | null;
   withDayOfTheWeek?: boolean;
   className: string;
 }) => {
-  const formatDate = (date: Date, withDayOfTheWeek?: boolean) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-
-    if (withDayOfTheWeek) {
-      const dayOfTheWeek = date
-        .toLocaleString("en", { weekday: "short" })
-        .split(" ")[0];
-
-      return `${dayOfTheWeek}, ${day}-${month}-${year}`;
-    }
-
-    return `${day}-${month}-${year}`;
-  };
+  if (!date) {
+    return <p className={className}>unknown</p>;
+  }
 
   if (withDayOfTheWeek) {
     return <p className={className}>{formatDate(date, withDayOfTheWeek)}</p>;
