89af74abe35573c4b9646f3328ad6ef81cb6fe6f
Srdjan <milicevic.srdjan0@gmail.com>
2025-08-10 12:15:45 +0200

    MODIFY: adds bunch of dev helping tools - experimental

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+    browserDebugInfoInTerminal: true,
+    devtoolNewPanelUI: true,
+    clientSegmentCache: true,
   },
   turbopack: {},
 };
