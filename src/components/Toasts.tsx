9e4158612d54025d997a54951d216f71014e40ce
Srdjan <milicevic.srdjan0@gmail.com>
2025-12-19 14:44:57 +0100

    FIX: path is now typed as Route to align with new typedRoutes rule in Next 15

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
-import debounce from "lodash.debounce";
-import { toast } from "sonner";
+import debounce from "lodash.debounce"
+import type { Route } from "next"
+import Link from "next/link"
+import { toast } from "sonner"
 
 // Accepts 1-3 args
 // - message to be rendered inside toast
@@ -9,23 +10,18 @@ import { toast } from "sonner";
 // - linkText - text to be rendered as the redirect button
 
 export const showToast = debounce(
-  (message: string, path?: string, linkText?: string) =>
+  (message: string, path?: Route, linkText?: string) =>
     toast.custom(
       (t) => (
-        <div className="select-none pb-8">
-          <div
-            onClick={() => toast.dismiss(t)}
-            className="flex items-center justify-between rounded-lg bg-white p-4 text-sm shadow-lg ring-[1.5px] ring-slate-400/55 dark:bg-slate-900 dark:shadow-black dark:ring-slate-800"
-          >
+        <div className="pb-8">
+          <div className="flex items-center justify-between rounded-lg bg-white p-4 text-sm shadow-lg ring-[1.5px] ring-slate-400/55 dark:bg-slate-900 dark:shadow-black dark:ring-slate-800">
             <div className="flex max-w-[75%] items-center gap-1.5">
-              <p className="font-manrope text-slate-600 dark:text-slate-200">
-                {message}
-              </p>
+              <p className="font-manrope text-slate-600 dark:text-slate-200">{message}</p>
             </div>
 
             {path && linkText ? (
               <Link
-                href={path}
+                href={`${path}`}
                 onClick={() => toast.dismiss(t)}
                 className="text-center font-semibold text-green-500"
               >
@@ -33,6 +29,7 @@ export const showToast = debounce(
               </Link>
             ) : (
               <button
+                type="button"
                 onClick={() => toast.dismiss(t)}
                 className="text-center font-semibold text-green-500"
               >
@@ -42,7 +39,7 @@ export const showToast = debounce(
           </div>
         </div>
       ),
-      { unstyled: true },
+      { unstyled: true }
     ),
-  250,
-);
+  250
+)
