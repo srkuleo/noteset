4296b3ec0089e6256b69eadbbf1b47ede8169187
Srdjan <milicevic.srdjan0@gmail.com>
2025-12-19 14:47:11 +0100

    FIX: use string literal instead of string concatenation, useEffect no longer has deps - removes weird bug with inconsistent rerendering

--- src/components/workout-to-do/NoteInputField.tsx
+++ src/components/workout-to-do/NoteInputField.tsx
@@ -1,6 +1,6 @@
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react"
 
-import type { ExerciseType } from "@/util/types";
+import type { ExerciseType } from "@/util/types"
 
 export const NoteInputField = ({
   exercise,
@@ -8,24 +8,20 @@ export const NoteInputField = ({
   handleNoteInput,
   resetNoteInput,
 }: {
-  exercise: ExerciseType;
+  exercise: ExerciseType
 
-  handleNoteInput: (
-    event: React.ChangeEvent<HTMLTextAreaElement>,
-    exerciseId: string,
-  ) => void;
-  resetNoteInput: (exerciseId: string) => void;
+  handleNoteInput: (event: React.ChangeEvent<HTMLTextAreaElement>, exerciseId: string) => void
+  resetNoteInput: (exerciseId: string) => void
 }) => {
-  const [isFocused, setIsFocused] = useState(false);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [isFocused, setIsFocused] = useState(false)
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
   useEffect(() => {
     if (textAreaRef.current !== null) {
-      textAreaRef.current.style.height = "auto";
-      textAreaRef.current.style.height =
-        textAreaRef.current.scrollHeight + "px";
+      textAreaRef.current.style.height = "auto"
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
     }
-  }, [exercise.note]);
+  })
 
   return (
     <div
@@ -39,17 +35,17 @@ export const NoteInputField = ({
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         onChange={(e) => handleNoteInput(e, exercise.id)}
-        className="w-full rounded-none bg-transparent pr-6 font-semibold text-slate-500/80 placeholder-slate-400 caret-violet-500 no-scrollbar placeholder:text-sm placeholder:italic focus:text-slate-500 focus:placeholder-slate-300 focus:outline-none dark:text-slate-400 dark:placeholder-slate-500/75 dark:caret-green-600 dark:focus:text-white dark:focus:placeholder-slate-600/80"
+        className="no-scrollbar w-full rounded-none bg-transparent pr-6 font-semibold text-slate-500/80 placeholder-slate-400 caret-violet-500 placeholder:text-sm placeholder:italic focus:text-slate-500 focus:placeholder-slate-300 focus:outline-none dark:text-slate-400 dark:placeholder-slate-500/75 dark:caret-green-600 dark:focus:text-white dark:focus:placeholder-slate-600/80"
       />
 
       {exercise.note && isFocused && (
         <button
           type="button"
           onMouseDown={(e) => {
-            e.preventDefault();
-            resetNoteInput(exercise.id);
+            e.preventDefault()
+            resetNoteInput(exercise.id)
           }}
-          className="absolute right-2 top-2 text-slate-400 dark:text-slate-500"
+          className="absolute top-2 right-2 text-slate-400 dark:text-slate-500"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -59,14 +55,10 @@ export const NoteInputField = ({
             stroke="currentColor"
             className="size-6"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
           </svg>
         </button>
       )}
     </div>
-  );
-};
+  )
+}
