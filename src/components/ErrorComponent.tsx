79de56f2d80b4f9a2671a735a8bead61fd98f577
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-05 14:05:16 +0100

    MODIFY: now better handles all the error cases

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,67 +1,31 @@
-import { twMerge } from "tailwind-merge";
-import { ErrorTriangleIcon } from "./icons/user/warning";
+import { twMerge } from "tailwind-merge"
+import { ErrorTriangleIcon } from "./icons/user/warning"
 
 type ErrorComponentProps = React.HTMLAttributes<HTMLParagraphElement> & {
-  errorArr?: string[];
-  message?: string;
-};
+  errorArr?: string[]
+  message?: string
+}
 
-export const ErrorComponent = ({
-  errorArr,
-  message,
-  className,
-}: ErrorComponentProps) => {
+export const ErrorComponent = ({ errorArr, message, className }: ErrorComponentProps) => {
   if (message) {
     return (
-      <div
-        className={twMerge(
-          "flex items-center gap-2.5 pt-2 text-red-500",
-          className,
-        )}
-      >
+      <div className={twMerge("flex items-center gap-2.5 pt-2 text-red-500", className)}>
         <ErrorTriangleIcon strokeWidth={1.5} className="size-7" />
 
-        <p className="font-semibold leading-tight dark:text-slate-100">
-          {message}
-        </p>
+        <p className="font-semibold leading-7 dark:text-slate-100">{message}</p>
       </div>
-    );
+    )
   }
 
-  if (!errorArr) {
-    return null;
-  }
-
-  if (errorArr.length > 1) {
-    const uniqueErrors = [...new Set(errorArr)];
-
-    return (
-      <div className={twMerge("space-y-2", className)}>
-        {uniqueErrors.map((error, i) => (
-          <div key={i} className="flex items-center gap-1.5 pt-1 text-red-500">
-            <ErrorTriangleIcon strokeWidth={1.3} className="size-5" />
-
-            <p className="text-sm font-semibold leading-tight dark:text-white">
-              {error}
-            </p>
-          </div>
-        ))}
-      </div>
-    );
+  if (!errorArr?.length) {
+    return null
   }
 
   return (
-    <div
-      className={twMerge(
-        "flex items-center gap-1.5 pt-1 text-red-500",
-        className,
-      )}
-    >
-      <ErrorTriangleIcon strokeWidth={1.3} className="size-5" />
+    <div className={twMerge("flex gap-1.5 text-red-500", className)}>
+      <ErrorTriangleIcon strokeWidth={1.5} className="size-4" />
 
-      <p className="text-sm font-semibold leading-tight dark:text-white">
-        {errorArr[0]}
-      </p>
+      <p className="block font-semibold text-[14px] leading-4 dark:text-white">{errorArr[0]}</p>
     </div>
-  );
-};
+  )
+}
