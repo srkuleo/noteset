8b68b468da4c9277ed3954ee63a6830eb52f0d9d
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-05 14:29:38 +0100

    MODIFY: improves typing, try catch flow and error handling

--- src/util/actions/workout.ts
+++ src/util/actions/workout.ts
@@ -1,119 +1,71 @@
-"use server";
+"use server"
 
-import { unauthorized } from "next/navigation";
-import { revalidatePath } from "next/cache";
-import { and, eq, ilike, ne } from "drizzle-orm";
-import { db } from "@/db";
-import { workouts } from "@/db/schema";
-import { getAuthSession } from "@/util/session";
-
+import { and, eq, ilike, inArray, ne } from "drizzle-orm"
+import { revalidatePath } from "next/cache"
+import { unauthorized } from "next/navigation"
+import z from "zod"
+import { db } from "@/db"
+import { type CurrentWorkoutForFormsType, workouts } from "@/db/schema"
+import { getAuthSession } from "@/util/session"
 import {
-  CreateWorkoutSchema,
-  WorkoutToDoSchema,
-  type WorkoutActionResponse,
-  type CreateWorkoutType,
-  type WorkoutToDoType,
   type ActionResponse,
-} from "../types";
+  CreateWorkoutSchema,
+  type WorkoutActionResponse,
+  type WorkoutToDoClientType,
+  WorkoutToDoServerSchema,
+} from "../types"
+import { FORM_TIMEOUT, timeout } from "../utils"
 
-export async function createWorkout(
-  workout: CreateWorkoutType,
-): Promise<WorkoutActionResponse> {
-  const { user } = await getAuthSession();
-
-  if (user === null) {
-    unauthorized();
-  }
-
-  const isValidWorkout = CreateWorkoutSchema.safeParse(workout);
-
-  if (!isValidWorkout.success) {
-    return {
-      status: "error",
-      errors: isValidWorkout.error.flatten().fieldErrors,
-      message: "Invalid workout data",
-    };
-  }
-
-  const { title, description, exercises } = isValidWorkout.data;
-
-  try {
-    const existingWorkout = await db.query.workouts.findFirst({
-      where: and(
-        ilike(workouts.title, title),
-        eq(workouts.userId, user.id),
-        eq(workouts.status, "current"),
-      ),
-    });
-
-    if (existingWorkout) {
-      console.log("Workout already exists.");
-
-      return {
-        status: "error",
-        errors: {
-          title: ["Workout with this title already exists."],
-        },
-        message: `${title} workout already exists`,
-      };
-    }
-
-    await db.insert(workouts).values({
-      userId: user.id,
-      title: title,
-      description: description,
-      exercises: exercises,
-    });
-
-    console.log(`${title} workout created!`);
-
-    revalidatePath("/home");
-
-    return {
-      status: "success-redirect",
-      message: `${title} workout created`,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      status: "error",
-      message: "Failed to create workout...",
-    };
-  }
+export type EditWorkoutArgs = {
+  editedWorkout: CurrentWorkoutForFormsType
+  workoutToEditId: number
 }
 
-export async function editWorkout(
-  editedWorkout: CreateWorkoutType,
-  workoutId: number,
-  initTitle: string,
-): Promise<WorkoutActionResponse> {
-  const { user } = await getAuthSession();
+export type UpdateWorkoutsArgs = {
+  id: number
+  title: string
+  action: "remove" | "archive" | "unarchive"
+}
 
+export type SubmitWorkoutArgs = {
+  doneWorkout: WorkoutToDoClientType
+  duration: number
+}
+
+export async function createWorkout(
+  workout: CurrentWorkoutForFormsType
+): Promise<WorkoutActionResponse> {
+  await timeout(FORM_TIMEOUT)
+
+  const { user } = await getAuthSession()
   if (user === null) {
-    unauthorized();
+    unauthorized()
   }
 
-  const isValidWorkout = CreateWorkoutSchema.safeParse(editedWorkout);
-
+  const isValidWorkout = CreateWorkoutSchema.safeParse(workout)
   if (!isValidWorkout.success) {
+    const tree = z.treeifyError(isValidWorkout.error)
+
     return {
       status: "error",
-      errors: isValidWorkout.error.flatten().fieldErrors,
+      errors: {
+        title: tree.properties?.title?.errors,
+        exercises: tree.properties?.exercises?.errors,
+      },
       message: "Invalid workout data",
-    };
+    }
   }
 
-  const { title, description, exercises } = isValidWorkout.data;
+  const { title, description, exercises } = isValidWorkout.data
 
   try {
     const alreadyExistWorkout = await db.query.workouts.findFirst({
       where: and(
         ilike(workouts.title, title),
         eq(workouts.userId, user.id),
-        eq(workouts.status, "current"),
-        ne(workouts.id, workoutId),
+        inArray(workouts.status, ["current", "archived"])
       ),
-    });
+    })
 
     if (alreadyExistWorkout) {
       return {
@@ -121,227 +73,189 @@ export async function editWorkout(
         errors: {
           title: ["Workout with this title already exists."],
         },
+        message: `${title} workout already exists`,
+      }
+    }
+
+    await db.insert(workouts).values({
+      userId: user.id,
+      title,
+      description,
+      exercises,
+    })
+
+    revalidatePath("/current")
+
+    return {
+      status: "success-redirect",
+      message: `${title} workout created`,
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      status: "error",
+      message: "Failed to create workout...",
+    }
+  }
+}
+
+export async function editWorkout({
+  editedWorkout,
+  workoutToEditId,
+}: EditWorkoutArgs): Promise<WorkoutActionResponse> {
+  await timeout(FORM_TIMEOUT)
+
+  const { user } = await getAuthSession()
+  if (user === null) {
+    unauthorized()
+  }
+
+  const isValidWorkout = CreateWorkoutSchema.safeParse(editedWorkout)
+  if (!isValidWorkout.success) {
+    const tree = z.treeifyError(isValidWorkout.error)
+
+    return {
+      status: "error",
+      errors: {
+        title: tree.properties?.title?.errors,
+        exercises: tree.properties?.exercises?.errors,
+      },
+      message: "Invalid workout data",
+    }
+  }
+
+  const { title, description, exercises } = isValidWorkout.data
+  try {
+    const exsitingWorkout = await db.query.workouts.findFirst({
+      where: and(
+        ilike(workouts.title, title),
+        eq(workouts.userId, user.id),
+        inArray(workouts.status, ["current", "archived"]),
+        ne(workouts.id, workoutToEditId)
+      ),
+    })
+
+    if (exsitingWorkout) {
+      return {
+        status: "error",
+        errors: {
+          title: ["Workout with this title already exists."],
+        },
         message: `Workout already exists`,
-      };
+      }
     }
 
     await db
       .update(workouts)
       .set({
-        title: title,
-        description: description,
-        exercises: exercises,
+        title,
+        description,
+        exercises,
       })
-      .where(eq(workouts.id, workoutId));
+      .where(eq(workouts.id, workoutToEditId))
 
-    console.log(`${initTitle} workout edited.`);
-
-    revalidatePath("/home");
+    revalidatePath("/current")
 
     return {
       status: "success-redirect",
-      message: `"${initTitle}" workout edited`,
-    };
+      message: "Workout edited.",
+    }
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return {
       status: "error",
       message: "Failed to edit a workout...",
-    };
+    }
   }
 }
 
-export async function removeWorkout(
-  workoutId: number,
-  workoutTitle: string,
-): Promise<ActionResponse> {
-  const { session } = await getAuthSession();
-
+export async function updateWorkouts({
+  id,
+  title,
+  action,
+}: UpdateWorkoutsArgs): Promise<ActionResponse> {
+  const { session } = await getAuthSession()
   if (!session) {
-    unauthorized();
+    unauthorized()
   }
 
   try {
-    await db.delete(workouts).where(eq(workouts.id, workoutId));
+    switch (action) {
+      case "remove":
+        await db.delete(workouts).where(eq(workouts.id, id))
 
-    console.log("Workout deleted!");
+        return {
+          status: "success",
+          message: `"${title}" workout removed.`,
+        }
 
-    return {
-      status: "success",
-      message: `"${workoutTitle}" workout removed`,
-    };
+      case "archive":
+        await db.update(workouts).set({ status: "archived" }).where(eq(workouts.id, id))
+
+        return {
+          status: "success-redirect",
+          message: `"${title}" workout archived.`,
+        }
+
+      case "unarchive":
+        await db.update(workouts).set({ status: "current" }).where(eq(workouts.id, id))
+
+        return {
+          status: "success-redirect",
+          message: `"${title}" workout unarchived.`,
+        }
+    }
   } catch (err) {
-    console.error(err);
+    console.error(err)
+
     return {
       status: "error",
-      message: "Failed to remove a workout...",
-    };
+      message: "Failed to update workouts...",
+    }
   }
 }
 
-export async function archiveWorkout(
-  workoutId: number,
-  workoutTitle: string,
-): Promise<ActionResponse> {
-  const { session } = await getAuthSession();
-
-  if (!session) {
-    unauthorized();
-  }
-
-  try {
-    await db
-      .update(workouts)
-      .set({ status: "archived" })
-      .where(eq(workouts.id, workoutId));
-
-    console.log("Workout archived!");
-
-    return {
-      status: "success-redirect",
-      message: `"${workoutTitle}" workout archived`,
-    };
-  } catch (err) {
-    console.error(err);
-    return {
-      status: "error",
-      message: "Failed to archive a workout...",
-    };
-  }
-}
-
-export async function unarchiveWorkout(
-  workoutId: number,
-  workoutTitle: string,
-): Promise<ActionResponse> {
-  const { session } = await getAuthSession();
-
-  if (!session) {
-    unauthorized();
-  }
-
-  try {
-    await db
-      .update(workouts)
-      .set({ status: "current" })
-      .where(eq(workouts.id, workoutId));
-
-    console.log("Workout unarchived!");
-
-    return {
-      status: "success-redirect",
-      message: `"${workoutTitle}" workout unarchived`,
-    };
-  } catch (err) {
-    console.error(err);
-    return {
-      status: "error",
-      message: "Failed to unarchive a workout...",
-    };
-  }
-}
-
-export async function submitDoneWorkout(
-  doneWorkout: WorkoutToDoType,
-  duration: number,
-): Promise<ActionResponse> {
-  const { user } = await getAuthSession();
-
+export async function submitWorkout({
+  doneWorkout,
+  duration,
+}: SubmitWorkoutArgs): Promise<ActionResponse> {
+  const { user } = await getAuthSession()
   if (user === null) {
-    unauthorized();
+    unauthorized()
   }
 
-  const isValidWorkout = WorkoutToDoSchema.safeParse(doneWorkout);
-
+  const isValidWorkout = WorkoutToDoServerSchema.safeParse(doneWorkout)
   if (!isValidWorkout.success) {
     return {
       status: "error",
       message: "Invalid workout data!",
-    };
+    }
   }
 
-  const { title, description, exercises } = isValidWorkout.data;
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const exercisesWithoutDoneProp = exercises.map(({ done, ...exercise }) => ({
-    ...exercise,
-    sets: exercise.sets.map(({ warmup, ...set }) => ({
-      ...set,
-      ...(warmup === undefined ? { warmup: false } : { warmup }),
-    })),
-  }));
+  const { title, description, exercises } = isValidWorkout.data
 
   try {
     await db.insert(workouts).values({
       userId: user.id,
-      title: title,
-      description: description,
-      exercises: exercisesWithoutDoneProp,
+      title,
+      description,
+      exercises,
       status: "done",
-      duration: duration,
+      duration,
       doneAt: new Date(),
-    });
+    })
 
-    console.log("Workout submitted!");
-
-    revalidatePath("/logs");
+    revalidatePath("/logs")
 
     return {
       status: "success-redirect",
       message: "Workout submitted",
-    };
+    }
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return {
       status: "error",
       message: "Failed to submit a workout...",
-    };
-  }
-}
-
-export async function updateCurrentWorkout(
-  updatedCurrentWorkout: CreateWorkoutType,
-  workoutId: number,
-): Promise<WorkoutActionResponse> {
-  const { user } = await getAuthSession();
-
-  if (user === null) {
-    unauthorized();
-  }
-
-  const isValidWorkout = CreateWorkoutSchema.safeParse(updatedCurrentWorkout);
-
-  if (!isValidWorkout.success) {
-    return {
-      status: "error",
-      errors: isValidWorkout.error.flatten().fieldErrors,
-      message: "Invalid workout data!",
-    };
-  }
-
-  try {
-    await db
-      .update(workouts)
-      .set({
-        title: updatedCurrentWorkout.title,
-        description: updatedCurrentWorkout.description,
-        exercises: [...updatedCurrentWorkout.exercises],
-      })
-      .where(and(eq(workouts.id, workoutId), eq(workouts.status, "current")));
-
-    console.log("Workout updated.");
-
-    revalidatePath("/home");
-
-    return {
-      status: "success-redirect",
-      message: `${updatedCurrentWorkout.title} workout updated`,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      status: "error",
-      message: "Failed to update a workout...",
-    };
+    }
   }
 }
