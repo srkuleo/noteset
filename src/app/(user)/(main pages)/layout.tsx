29304d77a2111cd50decc012d46cb92d0c91d7c5
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-05 13:11:57 +0100

    MODIFY: now renders user profile icon

--- src/app/(user)/(main pages)/layout.tsx
+++ src/app/(user)/(main pages)/layout.tsx
@@ -1,10 +1,18 @@
-import { MainPagesFooter } from "@/components/user/MainPagesFooter"
+import { Suspense } from "react"
+import { ProfileButtonSkeleton } from "@/components/Loading"
+import { MainPagesFooter } from "@/components/user/main_pages/MainPagesFooter"
+import { ProfileButton } from "@/components/user/main_pages/profile/ProfileButton"
 
 export default async function MainPagesLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <MainPagesFooter />
+
+      <MainPagesFooter>
+        <Suspense fallback={<ProfileButtonSkeleton />}>
+          <ProfileButton />
+        </Suspense>
+      </MainPagesFooter>
     </>
   )
 }
