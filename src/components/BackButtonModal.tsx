a598001c684d6bd6c6ff49ce940189b4171ab509
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-05 13:15:36 +0100

    MODIFY: format changes, now leads to /current

--- src/components/BackButtonModal.tsx
+++ src/components/BackButtonModal.tsx
@@ -1,26 +1,21 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { useState } from "react";
-import { Drawer } from "vaul";
-import { BUTTON_TIMEOUT, timeout } from "@/util/utils";
-import { ArrowLeftIcon } from "./icons/arrows";
+import Link from "next/link"
+import { useState } from "react"
+import { Drawer } from "vaul"
+import { BUTTON_TIMEOUT, timeout } from "@/util/utils"
+import { ArrowLeftIcon } from "./icons/arrows"
 
 export const BackButtonModal = ({ className }: { className: string }) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
   return (
-    <Drawer.Root
-      open={open}
-      onOpenChange={setOpen}
-      noBodyStyles
-      disablePreventScroll
-    >
+    <Drawer.Root open={open} onOpenChange={setOpen} noBodyStyles disablePreventScroll>
       <button
         type="button"
         onClick={async () => {
-          await timeout(BUTTON_TIMEOUT);
-          setOpen(true);
+          await timeout(BUTTON_TIMEOUT)
+          setOpen(true)
         }}
         className={className}
       >
@@ -37,31 +32,31 @@ export const BackButtonModal = ({ className }: { className: string }) => {
           data-vaul-no-drag
           className="fixed inset-x-0 bottom-0 z-[9999] select-none space-y-4 px-4 pb-12 focus:outline-none"
         >
-          <div className="flex flex-col gap-3 rounded-modal bg-white/90 text-center dark:bg-slate-700/70">
-            <Drawer.Title className="px-2 pb-2 pt-5 text-sm font-semibold">
+          <div className="flex flex-col gap-3 rounded-lg+ bg-white/90 text-center dark:bg-slate-700/70">
+            <Drawer.Title className="px-2 pt-5 pb-2 font-semibold text-sm">
               Are you sure you want to leave?
             </Drawer.Title>
 
             <Link
-              href="/home"
-              className="w-full rounded-b-modal border-t border-slate-400/40 p-3 font-manrope text-lg font-semibold text-green-500 focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 active:dark:bg-slate-600/90 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800"
+              href="/current"
+              className="w-full rounded-b-lg+ border-slate-400/40 border-t p-3 font-manrope font-semibold text-green-500 text-lg focus:outline-none active:bg-slate-200 disabled:bg-slate-300/55 disabled:text-green-500/75 dark:border-slate-600 disabled:dark:bg-slate-900/75 disabled:dark:text-green-800 active:dark:bg-slate-600/90"
             >
-              Go to Home page
+              Back to workouts
             </Link>
           </div>
 
           <button
             type="button"
             onClick={async () => {
-              await timeout(BUTTON_TIMEOUT);
-              setOpen(false);
+              await timeout(BUTTON_TIMEOUT)
+              setOpen(false)
             }}
-            className="w-full rounded-modal bg-white p-3 text-xl font-bold text-violet-500 focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
+            className="w-full rounded-lg+ bg-white p-3 font-bold text-violet-500 text-xl focus:outline-none active:bg-slate-200 dark:bg-slate-700 dark:text-violet-400 active:dark:bg-slate-600/90"
           >
             Cancel
           </button>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
-  );
-};
+  )
+}
