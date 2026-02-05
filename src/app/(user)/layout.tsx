ede5384c61973166c2100a6b9f4d9e8af889ce36
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-05 13:10:45 +0100

    MODIFY: no longer renders user profile icon

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,16 +1,10 @@
-import { Suspense } from "react"
-import { ProfileButtonSkeleton } from "@/components/Loading"
-import { ProfileButton } from "@/components/user/profile/ProfileButton"
 import { UserPagesHeader } from "@/components/user/UserPagesHeader"
 
 export default async function UserPagesLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <UserPagesHeader>
-        <Suspense fallback={<ProfileButtonSkeleton />}>
-          <ProfileButton />
-        </Suspense>
-      </UserPagesHeader>
+      <UserPagesHeader />
+
       {children}
     </>
   )
