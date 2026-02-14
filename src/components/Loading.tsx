16f1c2359459c7126c8b4a0cdcaeb17535cd3263
Srdjan <milicevic.srdjan0@gmail.com>
2026-02-14 01:59:41 +0100

    MODIFY: updates UI for skeleton on both Workouts and Logs Page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -50,20 +50,20 @@ export const WorkoutsPageSkeleton = () => {
       {[1, 2, 3, 4, 5, 6].map((skeleton) => (
         <div
           key={skeleton}
-          className="flex animate-pulse items-center justify-between rounded-xl bg-white p-6 shadow-md dark:bg-slate-800"
+          className="flex animate-pulse flex-col rounded-xl bg-white shadow-md dark:bg-slate-800"
         >
-          <div className="w-3/5 space-y-2">
-            <div className="h-7 w-24 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-950/80" />
-            <div className="h-4 w-32 animate-pulse rounded-lg bg-slate-100 dark:bg-slate-950/50" />
+          <div className="flex items-center gap-2 p-6">
+            <div className="w-[calc(100%-86px)] space-y-1">
+              <div className="h-6 w-1/3 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-950/80" />
+              <div className="h-3.5 w-2/5 animate-pulse rounded-lg bg-slate-100 dark:bg-slate-950/50" />
+            </div>
+
+            <div className="h-[28px] w-[78px] animate-pulse rounded-full bg-slate-200 dark:bg-slate-950/80" />
           </div>
 
-          <div className="flex flex-col items-center gap-1.5">
-            <div className="h-[28px] w-20 animate-pulse rounded-full bg-slate-200 dark:bg-slate-950/80" />
-
-            <div className="flex gap-1.5">
-              <div className="size-10 animate-pulse rounded-lg+ bg-slate-200 dark:bg-slate-950/80" />
-              <div className="size-10 animate-pulse rounded-lg+ bg-slate-200 dark:bg-slate-950/80" />
-            </div>
+          <div className="flex h-11 animate-pulse divide-x divide-slate-300 rounded-b-xl bg-slate-100 ring-1 ring-slate-300 ring-inset dark:divide-slate-700 dark:bg-slate-900 dark:ring-slate-700">
+            <div className="flex-1 rounded-bl-xl" />
+            <div className="flex-1 rounded-br-xl" />
           </div>
         </div>
       ))}
@@ -87,16 +87,20 @@ export const LogsPageSkeleton = () => {
         {[1, 2, 3, 4, 5, 6].map((skeleton) => (
           <div
             key={skeleton}
-            className="flex animate-pulse items-center justify-between rounded-xl bg-white p-6 shadow-md dark:bg-slate-800"
+            className="flex animate-pulse flex-col rounded-xl bg-white shadow-md dark:bg-slate-800"
           >
-            <div className="w-3/5 space-y-2">
-              <div className="h-7 w-24 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-950/80" />
-              <div className="h-4 w-32 animate-pulse rounded-lg bg-slate-100 dark:bg-slate-950/50" />
+            <div className="flex items-center gap-2 p-6">
+              <div className="w-[calc(100%-71px)] space-y-1">
+                <div className="h-6 w-1/3 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-950/80" />
+                <div className="h-3.5 w-2/5 animate-pulse rounded-lg bg-slate-100 dark:bg-slate-950/50" />
+              </div>
+
+              <div className="h-[28px] w-[63px] animate-pulse rounded-full bg-slate-200 dark:bg-slate-950/80" />
             </div>
 
-            <div className="flex flex-col items-center gap-3.5">
-              <div className="h-5 w-14 animate-pulse rounded-full bg-slate-200 dark:bg-slate-950/80" />
-              <div className="h-10 w-18 animate-pulse rounded-lg bg-slate-200 dark:bg-slate-950/80" />
+            <div className="flex h-11 animate-pulse divide-x divide-slate-300 rounded-b-xl bg-slate-100 ring-1 ring-slate-300 ring-inset dark:divide-slate-700 dark:bg-slate-900 dark:ring-slate-700">
+              <div className="flex-1 rounded-bl-xl" />
+              <div className="w-[124px] rounded-br-xl" />
             </div>
           </div>
         ))}
